1edbbd314959fb68bb6dd3b31392fb54a2fb3e5c
NicolasClairis <nicolas.clairis@protonmail.com>
2020-11-13 19:32:29 +0100

    detail loop variables renamed

--- Experiment Motivation Original/Matlab Code/saveDataExcel.m
+++ Experiment Motivation Original/Matlab Code/saveDataExcel.m
@@ -1,24 +1,30 @@
 function saveDataExcel(all,nbTrial,file_nm)
 % saveDataExcel(all,nbTrial,file_nm)
+% converts matlab structure of the LGC motivation task with all the relevant
+% informations into an excel file
+% 
+% INPUTS
+%
+% See also LGCM_main
 
 
 % Ugly big function to put the data in the correct order to save. Prepare 4 excel sheet, one for each block
 for iBlock = 1:4
     
     % for each trial they did
-    for j = 1:nbTrial(iBlock)
+    for jBlock = 1:nbTrial(iBlock)
         
         % extract the data from the cells to put them back in the order excel wants
-        thesignal = all.signals{iBlock,j}; % Extract second cell.
-        theWin(j) = all.win{iBlock,j};
-        theVCmax(j) = all.VCmax{iBlock,j};
-        theTrialLength(j) = all.trialLength{iBlock,j};
-        theIncentive(j) = all.incentive{iBlock,j};
-        theFirstT2(j) = all.firstT2{iBlock,j};
+        thesignal = all.signals{iBlock,jBlock}; % Extract second cell.
+        theWin(jBlock) = all.win{iBlock,jBlock};
+        theVCmax(jBlock) = all.VCmax{iBlock,jBlock};
+        theTrialLength(jBlock) = all.trialLength{iBlock,jBlock};
+        theIncentive(jBlock) = all.incentive{iBlock,jBlock};
+        theFirstT2(jBlock) = all.firstT2{iBlock,jBlock};
         
         % as this is the only vector to save, do a for loop on it
-        for row = 1 : size(thesignal, 2)
-            tableData{row,j} = thesignal(row);
+        for iRow = 1 : size(thesignal, 2)
+            tableData{iRow,jBlock} = thesignal(iRow);
         end
         
     end
@@ -27,13 +33,13 @@ for iBlock = 1:4
     theUsedMVC = all.usedMVC{iBlock,1};
     
     % Prepare the data in right order for cell conversion to table
-    for row = 1: nbTrial(iBlock)
-        tableData{row,nbTrial(iBlock)+1} = theWin(row);
-        tableData{row,nbTrial(iBlock)+2} = theVCmax(row);
+    for iRow = 1: nbTrial(iBlock)
+        tableData{iRow,nbTrial(iBlock)+1} = theWin(iRow);
+        tableData{iRow,nbTrial(iBlock)+2} = theVCmax(iRow);
         tableData{1,nbTrial(iBlock)+3} = theUsedMVC;
-        tableData{row,nbTrial(iBlock)+4} = theTrialLength(row);
-        tableData{row,nbTrial(iBlock)+5} = theIncentive(row);
-        tableData{row,nbTrial(iBlock)+6} = theFirstT2(row);
+        tableData{iRow,nbTrial(iBlock)+4} = theTrialLength(iRow);
+        tableData{iRow,nbTrial(iBlock)+5} = theIncentive(iRow);
+        tableData{iRow,nbTrial(iBlock)+6} = theFirstT2(iRow);
     end
     
     % transform data into table
@@ -41,8 +47,8 @@ for iBlock = 1:4
     
     % prepare table names for columns
     trial_name = {};
-    for trial_i = 1 :nbTrial(iBlock)
-        trial_name{trial_i} = strcat('trial_signal_',num2str(trial_i));
+    for iTtrial = 1 :nbTrial(iBlock)
+        trial_name{iTtrial} = strcat('trial_signal_',num2str(iTtrial));
     end
     trial_name{nbTrial(iBlock)+1} = 'doWin';
     trial_name{nbTrial(iBlock)+2} = 'VCmax';
