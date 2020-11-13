c5bf7a3bc48b642dc255eb294996adf731b2f465
NicolasClairis <nicolas.clairis@protonmail.com>
2020-11-13 11:35:14 +0100

    detail update labels (CENIR = Paris MRI center)

--- Matlab_DIY_functions/ScreenConfiguration.m
+++ Matlab_DIY_functions/ScreenConfiguration.m
@@ -22,7 +22,7 @@ screens = Screen('Screens');
 if IRM == 0
     whichScreen = max(screens);
 elseif IRM == 1
-    if testing_script == 0 % CENIR
+    if testing_script == 0 % MRI
         whichScreen = 1; % 1 if 2 screens, 0 if one screen
     elseif testing_script == 1 % my own computer
         whichScreen = max(screens);
@@ -30,16 +30,16 @@ elseif IRM == 1
 end
 
 Screen('Preference','VisualDebugLevel', 1); % avoid initial Psychtoolbox window
-if testing_script == 0 % CENIR
+if testing_script == 0 % MRI
     Screen('Preference', 'SkipSyncTests', 0); % needs all other processes shut off
     window = Screen('OpenWindow',whichScreen,[0 0 0]);
 elseif testing_script == 1 % my own computer
     Screen('Preference', 'SkipSyncTests', 1); % can work even if other softwares are on but displays an ugly red triangle at start
-    % when testing, set resolution equal to CENIR computer for testing
-%     LCenirScreen = 1024;
-%     HCenirScreen = 768;
-%     Screen('Resolution', whichScreen, LCenirScreen, HCenirScreen); % sets a new whichScreen resolution for PTB
-%     window = Screen('OpenWindow',whichScreen,[0 0 0], [1, 1, LCenirScreen, HCenirScreen]); % default window is full whichScreen screen
+    % when testing, set resolution equal to MRI computer for testing
+%     L_MRI_Screen = 1024;
+%     H_MRI_Screen = 768;
+%     Screen('Resolution', whichScreen, L_MRI_Screen, H_MRI_Screen); % sets a new whichScreen resolution for PTB
+%     window = Screen('OpenWindow',whichScreen,[0 0 0], [1, 1, L_MRI_Screen, H_MRI_Screen]); % default window is full whichScreen screen
 
 % window = Screen('OpenWindow', whichScreen, [], [1, 1, 800, 600]); % for debugging
     window = Screen('OpenWindow',whichScreen,[0 0 0]);
@@ -49,11 +49,11 @@ HideCursor()
 baselineTextSize = 40;
 Screen('TextSize', window, baselineTextSize);
 Screen('TextFont', window, 'arial');
-% if testing_script == 0 % CENIR
+% if testing_script == 0 % MRI
 [L, H] = Screen('WindowSize',whichScreen);
 % elseif testing_script == 1
-%        L = LCenirScreen;
-%        H = HCenirScreen;
+%        L = L_MRI_Screen;
+%        H = H_MRI_Screen;
 % end
 x = L/2;
 y = H/2;
