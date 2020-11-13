51f7e1bf50f92eae3bb2fa6aa9488a74dd6f65fa
NicolasClairis <nicolas.clairis@protonmail.com>
2020-11-13 19:10:14 +0100

    detail
    - switched block_i to iBlock
    - switched subjectCodeName to file_nm

--- Experiment Motivation Original/Matlab Code/saveDataExcel.m
+++ Experiment Motivation Original/Matlab Code/saveDataExcel.m
@@ -1,18 +1,20 @@
-function saveDataExcel(all,nbTrial,subjectCodeName)
+function saveDataExcel(all,nbTrial,file_nm)
+% saveDataExcel(all,nbTrial,file_nm)
+
 
 % Ugly big function to put the data in the correct order to save. Prepare 4 excel sheet, one for each block
-for block_i = 1:4
+for iBlock = 1:4
     
     % for each trial they did
-    for j = 1:nbTrial(block_i)
+    for j = 1:nbTrial(iBlock)
         
         % extract the data from the cells to put them back in the order excel wants
-        thesignal = all.signals{block_i,j}; % Extract second cell.
-        theWin(j) = all.win{block_i,j};
-        theVCmax(j) = all.VCmax{block_i,j};
-        theTrialLength(j) = all.trialLength{block_i,j};
-        theIncentive(j) = all.incentive{block_i,j};
-        theFirstT2(j) = all.firstT2{block_i,j};
+        thesignal = all.signals{iBlock,j}; % Extract second cell.
+        theWin(j) = all.win{iBlock,j};
+        theVCmax(j) = all.VCmax{iBlock,j};
+        theTrialLength(j) = all.trialLength{iBlock,j};
+        theIncentive(j) = all.incentive{iBlock,j};
+        theFirstT2(j) = all.firstT2{iBlock,j};
         
         % as this is the only vector to save, do a for loop on it
         for row = 1 : size(thesignal, 2)
@@ -22,16 +24,16 @@ for block_i = 1:4
     end
     
     % Unique value, so we save it appart
-    theUsedMVC = all.usedMVC{block_i,1};
+    theUsedMVC = all.usedMVC{iBlock,1};
     
     % Prepare the data in right order for cell conversion to table
-    for row = 1: nbTrial(block_i)
-        tableData{row,nbTrial(block_i)+1} = theWin(row);
-        tableData{row,nbTrial(block_i)+2} = theVCmax(row);
-        tableData{1,nbTrial(block_i)+3} = theUsedMVC;
-        tableData{row,nbTrial(block_i)+4} = theTrialLength(row);
-        tableData{row,nbTrial(block_i)+5} = theIncentive(row);
-        tableData{row,nbTrial(block_i)+6} = theFirstT2(row);
+    for row = 1: nbTrial(iBlock)
+        tableData{row,nbTrial(iBlock)+1} = theWin(row);
+        tableData{row,nbTrial(iBlock)+2} = theVCmax(row);
+        tableData{1,nbTrial(iBlock)+3} = theUsedMVC;
+        tableData{row,nbTrial(iBlock)+4} = theTrialLength(row);
+        tableData{row,nbTrial(iBlock)+5} = theIncentive(row);
+        tableData{row,nbTrial(iBlock)+6} = theFirstT2(row);
     end
     
     % transform data into table
@@ -39,20 +41,20 @@ for block_i = 1:4
     
     % prepare table names for columns
     trial_name = {};
-    for trial_i = 1 :nbTrial(block_i)
+    for trial_i = 1 :nbTrial(iBlock)
         trial_name{trial_i} = strcat('trial_signal_',num2str(trial_i));
     end
-    trial_name{nbTrial(block_i)+1} = 'doWin';
-    trial_name{nbTrial(block_i)+2} = 'VCmax';
-    trial_name{nbTrial(block_i)+3} = 'usedVCmax';
-    trial_name{nbTrial(block_i)+4} = 'duration_Nbframe';
-    trial_name{nbTrial(block_i)+5} = 'incentive_value';
-    trial_name{nbTrial(block_i)+6} = 'FirstT2_sec';
+    trial_name{nbTrial(iBlock)+1} = 'doWin';
+    trial_name{nbTrial(iBlock)+2} = 'VCmax';
+    trial_name{nbTrial(iBlock)+3} = 'usedVCmax';
+    trial_name{nbTrial(iBlock)+4} = 'duration_Nbframe';
+    trial_name{nbTrial(iBlock)+5} = 'incentive_value';
+    trial_name{nbTrial(iBlock)+6} = 'FirstT2_sec';
     tableData.Properties.VariableNames = trial_name;
     
     % put the table into the sheet and clear the table to prepare it for a new sheet (due to cells being annoying)
-    writetable(tableData, [subjectCodeName '_data.xlsx'],'Sheet',block_i)
-    clear tableData
+    writetable(tableData, [file_nm,'.xlsx'],'Sheet',iBlock)
+    clear tableData;
 end
 
 % Move the data in the correct file
