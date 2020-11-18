8e4f8b1c4878b1f0f2b438755d48b09bcd6aa595
NicolasClairis <nicolas.clairis@protonmail.com>
2020-11-18 20:23:42 +0100

    adding fixation cross

--- LGC_Motiv_task/LGCM_stim_initialize.m
+++ LGC_Motiv_task/LGCM_stim_initialize.m
@@ -14,18 +14,19 @@ function[stim] = LGCM_stim_initialize(scr)
 window          = scr.window;
 xScreenCenter   = scr.xCenter;
 yScreenCenter   = scr.yCenter;
+% screenXpixels   = xScreenCenter*2;
+screenYpixels   = yScreenCenter*2;
 
 %% Enable alpha blending for anti-aliasing of all our textures
 Screen('BlendFunction', window, 'GL_SRC_ALPHA', 'GL_ONE_MINUS_SRC_ALPHA');
 
-%%
-stim.VSsignal = 0;
-% The color used to represent the signal
+%% color used to represent the signal
 stim.signalColor = [0 0.75 0.3];
 
 %% Money variables
 moneySize = 214;
 stim.money = [0 0 moneySize moneySize];
+stim.moneySize      = moneySize;
 
 %% thresholds for reaching target
 % 30% of MVC defined to this size so that 70 can be at 500 pixels
@@ -35,24 +36,17 @@ threshold_2 = [0 0 500 500];
 
 %% missed position
 %
-missTarget     = [0 0 moneySize moneySize];
+missTargetRect     = [0 0 moneySize moneySize];
 % 60% of MVC
-missThreshold = [0 0 429 429];
-
-% First appearance of the stimulus to wait and let the participant see the value of the coin
-stim.first_appearence = false;
-
-% Is it going to be reward or punishment first
-isPositiveStimuli = [randperm(numel([1 0]))-1 2];
-stim.isPositiveStimuli = isPositiveStimuli(1);
+missThresholdRect = [0 0 429 429];
 
 %% Position of each ring/coin/signal on the screen
 % force thresholds
 stim.centeredthreshold_1 = CenterRectOnPointd(threshold_1, xScreenCenter, yScreenCenter);
 stim.centeredthreshold_2 = CenterRectOnPointd(threshold_2, xScreenCenter, yScreenCenter);
 % miss position on the screen
-missThreshold = CenterRectOnPointd(missThreshold, xScreenCenter, yScreenCenter);
-missTarget = CenterRectOnPointd(missTarget, xScreenCenter*2 - moneySize/2, yScreenCenter);
+stim.missThreshold = CenterRectOnPointd(missThresholdRect, xScreenCenter, yScreenCenter);
+stim.missTarget = CenterRectOnPointd(missTargetRect, xScreenCenter*2 - moneySize/2, yScreenCenter);
 
 %% Import the images
 [image_20Cent, ~, alpha_20Cent]   = imread([pics_folder 'SMT_Coin_20RP.png']);
@@ -81,11 +75,16 @@ stim.imageTextures = [stim.imageTexture_1FR,...
     stim.imageTexture_20Cent,...
     stim.imageTexture_0FR];
 
-%% store all info in output structure
-stim.moneySize      = moneySize;
-stim.missTarget     = missTarget;
-stim.missThreshold  = missThreshold;
-stim.threshold_1    = threshold_1;
-stim.threshold_2    = threshold_2;
+%% fixation cross coordinates on the screen (code relative to screen Y size)
+cross_length    = screenYpixels/5;
+cross_thickness = 0.2*cross_length;
+stim.cross_verticalLine = [xScreenCenter - (cross_thickness/2),...
+    yScreenCenter - (cross_length/2),...
+    xScreenCenter + (cross_thickness/2),...
+    yScreenCenter + (cross_length/2)];
+stim.cross_horizontalLine = [xScreenCenter - (cross_length/2),...
+    yScreenCenter - (cross_thickness/2),...
+    xScreenCenter + (cross_length/2),...
+    yScreenCenter + (cross_thickness/2)];
 
 end % function
