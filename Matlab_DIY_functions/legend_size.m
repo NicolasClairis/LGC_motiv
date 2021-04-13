1c1a8a796ea34016447320cfa265c94a1301e08c
NicolasClairis <nicolas.clairis@protonmail.com>
2021-04-13 11:13:56 +0200

    minor update

--- Matlab_DIY_functions/legend_size.m
+++ Matlab_DIY_functions/legend_size.m
@@ -6,8 +6,8 @@ function [  ] = legend_size( pSize )
 % pSize: size of the text you want to use
 % Originally written by N.Clairis - 31/01/2020
 
+% set(gca,'FontName','Century gothic');
 set(gca,'fontsize',pSize,'FontWeight','bold');
 set(findall(gcf,'type','text'),'FontSize',pSize,'fontWeight','bold');
 
-end
-
+end % function
