e3688a43173fd76a8cdf503f4325f526c40ce22d
NicolasClairis <nicolas.clairis@protonmail.com>
2021-05-06 17:12:18 +0200

    updates for physical effort task

--- LGC_Motiv_task/physical_effort_perf.m
+++ LGC_Motiv_task/physical_effort_perf.m
@@ -58,6 +58,11 @@ function[physicalE_perf, trial_success, onsets] = physical_effort_perf(scr, stim
 % See also choice_task_main.m
 
 %% initialize the variables of interest
+% screen and PTB parameters
+window = scr.window;
+ifi = scr.ifi;
+nFrames = 6;
+
 trial_success = 0;
 % initialize indicators whether force threshold has been reached or not
 has_F_threshold_ever_been_reached = 0;
@@ -68,8 +73,14 @@ endAngle = stim.difficulty.arcEndAngle;
 totalAngleDistance = endAngle - startAngle;
 
 % effort time to keep
-onsets.effort_starts = [];
+[onsets.effort.start, onsets.effort.stop] = deal([]);
 t_effort_to_keep = E_time_levels.(['level_',num2str(E_chosen)]);
+
+%% start acquiring the data in the background (if you don't use this
+% function, everytime you call the read function, it will take a
+% long time to process)
+start(dq,"continuous");
+% will need data = read(dq) function only to read the signal
 
 %% display all relevant variables on the screen for the effort period
 
@@ -85,34 +96,130 @@ Screen('FillArc', window,...
     startAngle,...
     endAngle - startAngle);
 
-[~,onsetEffortPhase] = Screen('Flip',window);
+[lastFrameTime, onsetEffortPhase] = Screen('Flip',window);
 onsets.effort_phase = onsetEffortPhase;
 timeNow = onsetEffortPhase;
 
 %% effort
-force_levels = [];
+[force_levels,...
+    dispInfos.time,...
+    dispInfos.currentAngle,...
+    dispInfos.forceLevel] = deal([]);
+[timeEffortStart_tmp, timeEffortStop_tmp] = deal(0);
+stateSqueezeON = false;
+
+% initialize read
+timeCheck = GetSecs;
+F_now_Voltage = read(dq,'all','OutputFormat','Matrix');
+% convert force level from Voltage to a percentage of MVC
+F_now = F_now_Voltage/MVC;
+% store force levels in the output
+force_levels = [force_levels;...
+    [F_now, timeNow, F_now_Voltage]]; % store F in % of MVC, time and F in Volts
+% short pause to avoid empty matrix reading by read.m function
+pause(0.1);
+
 while (trial_success == 0) ||...
         ( (time_limit == true) && (timeNow <= (onsetEffortPhase + t_max_effort)) )
     % you either stop if the trial was successful (both learning and actual
     % task) OR if there is a time_limit defined and this time_limit was
     % reached (actual task only)
     
-    % check the performance and update the display of the force
-    % accordingly
-    [timeNow, force_levels, onsets, currentAngle,...
-        has_F_threshold_ever_been_reached] = physical_effort_check_livePerf(scr, dq,...
-        MVC,...
-        totalAngleDistance,...
-        currentAngle, force_levels, has_F_threshold_ever_been_reached,...
-        F_threshold, F_tolerance, t_effort_to_keep,...
-        onsets);
+    %% read current level of force
+    % check timing now when we extract the level of force
+    timeNow = GetSecs;
+    % check force being applied now
+    if timeNow >= timeCheck + 0.1
+        F_now_Voltage = read(dq,'all','OutputFormat','Matrix');
+        timeCheck = timeNow;
+        % convert force level from Voltage to a percentage of MVC
+        F_now = F_now_Voltage/MVC;
+        F_now_interpolate_tmp = linspace(force_levels(end, 1), F_now, nFrames);
+        % store force levels in the output
+        force_levels = [force_levels;...
+            [F_now, timeNow, F_now_Voltage]]; % store F in % of MVC, time and F in Volts
+        % reset frame for visual display
+        iFrame = 1;
+    end
     
-    % check whether performance was or not achieved
+    %% update the center display according to if force above or below the threshold
+    if F_now >= (F_threshold - F_tolerance) % while force > threshold, update the timer
+        
+        if stateSqueezeON == false % the participant was not squeezing above threshold => new start
+            n_starts = length(onsets.effort.start);
+            onsets.effort.start(n_starts + 1) = timeNow;
+            % update state of squeeze
+            stateSqueezeON = true;
+        end
+        
+        % update the angle for the display
+        if ~isempty(onsets.effort.start)
+            timeEffortStart_tmp = onsets.effort.start(end);
+        end
+        if ~isempty(onsets.effort.stop)
+            timeEffortStop_tmp = onsets.effort.stop(end);
+        end
+        
+    else % force below threshold
+        % switch from squeeze above threshold to squeeze below threshold
+        if stateSqueezeON == true % the participant was squeezing above threshold but now he squeezes below threshold
+            n_stops = length(onsets.effort.stop);
+            onsets.effort.stop(n_stops + 1) = timeNow;
+            % update state of squeeze
+            stateSqueezeON = false;
+        end
+        
+        % update the angle for the display
+        if ~isempty(onsets.effort.start)
+            timeEffortStart_tmp = onsets.effort.start(end);
+        end
+        if ~isempty(onsets.effort.stop)
+            timeEffortStop_tmp = onsets.effort.stop(end);
+        end
+    end % Force above or below threshold?
+    
+    % update the angle for display
+    if timeEffortStart_tmp > 0
+        percentageTimeForceAlreadyMaintained = (timeNow - timeEffortStart_tmp + timeEffortStop_tmp)/t_effort_to_keep;
+        if percentageTimeForceAlreadyMaintained >= 0
+            currentAngle = totalAngleDistance*percentageTimeForceAlreadyMaintained;
+        else
+            currentAngle = startAngle;
+        end
+    end
+    
+    %% display on screen accordingly
+    
+    % display real-time force level
+    disp_realtime_force(scr, F_threshold, F_tolerance, F_now_interpolate_tmp(iFrame), 'task');
+    if iFrame < nFrames
+        iFrame = iFrame + 1;
+    end
+    
+    % display performance achieved level on top of the reward as an arc
+    Screen('FillArc', window,...
+        stim.difficulty.currLevelColor,...
+        stim.difficulty.middle_center,...
+        currentAngle,...
+        endAngle - currentAngle);
+    
+%     [~,timeDispNow] = Screen('Flip',window);
+    [lastFrameTime, timeDispNow]  = Screen('Flip', window, lastFrameTime + (0.5*ifi));
+    
+    % record effort display informations (timing and angle + force level
+    dispInfos.time          = [dispInfos.time, timeDispNow];
+    dispInfos.currentAngle  = [dispInfos.currentAngle, currentAngle];
+    dispInfos.forceLevel    = [dispInfos.forceLevel, F_now];
+    
+    %% check whether performance was or not achieved
     if currentAngle >= endAngle
         trial_success = 1;
         onsets.effort_success = timeNow;
     end
 end % time loop
+
+%% stop acquisition of biopac handgrip
+stop(dq);
 
 %% check whether performance was or not achieved in the time course available
 if currentAngle >= endAngle
@@ -129,5 +236,6 @@ physicalE_perf.startAngle = startAngle;
 physicalE_perf.finalAngle = currentAngle;
 physicalE_perf.endAngle = endAngle;
 physicalE_perf.has_F_threshold_ever_been_reached = has_F_threshold_ever_been_reached;
+physicalE_perf.displayInformations = dispInfos;
 
 end % function
