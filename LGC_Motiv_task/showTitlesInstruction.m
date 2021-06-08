17aaf2a6b1c98aca2d2fbc953ba9e766f3567b8c
NicolasClairis <nicolas.clairis@protonmail.com>
2021-06-08 17:10:04 +0200

    fix showTitlesInstruction bugs

--- LGC_Motiv_task/showTitlesInstruction.m
+++ LGC_Motiv_task/showTitlesInstruction.m
@@ -1,4 +1,4 @@
-function showTitlesInstruction(scr,instructionType,isMental)
+function showTitlesInstruction(scr, instructionType, isMental)
 % Function to display information of the next things to do
 %
 % Scr = screen informations
@@ -7,12 +7,21 @@ function showTitlesInstruction(scr,instructionType,isMental)
 % "training" is when you perform the full trial and the results do not matter
 % "task" is when you launch a block of trials, the core of the experiment
 % isMental = define if we are working on physical or mental effort
-% 
-% 
-% 
+%
+%
+%
+
+%% initialize relevant parameters
+window = scr.window;
+yScreenCenter = scr.yCenter;
+baselineTextSize = scr.textSize.baseline;
+
+%% define title settings
+titleSize = 80;
+titleCol = scr.colours.white;
 
 t_wait = 3;
-Screen('TextSize', scr.window, 80);
+Screen('TextSize', window, titleSize);
 if isMental == true
     effortType = 'Mental';
 elseif isMental == false
@@ -24,21 +33,21 @@ switch instructionType
     case 'learning'
         DrawFormattedText(window,...
             ['Apprentissage ',effortType],...
-            'center',yScreenCenter/2,blackCol, wrapat);
+            'center',yScreenCenter/2,titleCol);
         
-    case 'Training'
+    case 'training'
         DrawFormattedText(window,...
             ['Entrainement ',effortType],...
-            'center',yScreenCenter/2,blackCol, wrapat);
-    case 'Task'
+            'center',yScreenCenter/2,titleCol);
+    case 'task'
         DrawFormattedText(window,...
             ['Tâche ',effortType],...
-            'center',yScreenCenter/2,blackCol, wrapat);
+            'center',yScreenCenter/2,titleCol);
 end
 %flip information on the screen
-Screen(scr.window,'Flip');
+Screen(window,'Flip');
 WaitSecs(t_wait);
 % put back baseline textsize value
-Screen('TextSize', window, 40);
+Screen('TextSize', window, baselineTextSize);
 end
 
