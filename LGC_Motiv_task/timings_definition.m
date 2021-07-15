2712044aeeec417f7cec9c8275426bdf8da5cdb0
NicolasClairis <nicolas.clairis@protonmail.com>
2021-07-15 12:16:26 +0200

    reduce calib minimal tolerated time for fMRI

--- LGC_Motiv_task/timings_definition.m
+++ LGC_Motiv_task/timings_definition.m
@@ -41,7 +41,7 @@ end
 calibTimes.instructions = 5;
 switch effort_type % in case you use different numbers for each effort type
     case 'mental'
-        calibTimes.effort_max = 15; % maximal time to perform the task (calibrated time should be shorter)
+        calibTimes.effort_max = 12; % maximal time to perform the task (calibrated time should be shorter)
     case 'physical'
         calibTimes.instructions_bis = 10;
         calibTimes.effort_max = 5;% time to perform the task
