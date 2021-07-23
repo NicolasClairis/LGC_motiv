23b6575a451445e8a5bbb13c0d3224b15808f9c8
NicolasClairis <nicolas.clairis@protonmail.com>
2021-07-23 18:05:26 +0200

    fix modifications made for behavioral pilots to remain ok for them and fMRI

--- LGC_Motiv_task/main_staircase_piloting.m
+++ LGC_Motiv_task/main_staircase_piloting.m
@@ -12,7 +12,7 @@ cd ..
 main_folder                 = [pwd filesep]; % you have to be sure that you are in the correct path when you launch the script
 main_task_folder            = [main_folder, 'LGC_Motiv_task' filesep];
 results_folder              = [main_folder, 'LGC_Motiv_results' filesep];
-% BioPac_folder               = [main_fo lder, 'BioPac_functions' filesep];
+% BioPac_folder               = [main_folder, 'BioPac_functions' filesep];
 % pics_folder                 = [main_task_folder, 'Coin_PNG', filesep];
 Matlab_DIY_functions_folder = [main_folder, 'Matlab_DIY_functions', filesep];
 
@@ -59,7 +59,7 @@ switch IRM
     case 0
         taskToPerform.physical.task = 'on';
     case 1 % task will be done in the scanner after the training
-        taskToPerform.physical.task = 'on';
+        taskToPerform.physical.task = 'off';
 end
 taskToPerform.mental.learning = 'on';
 taskToPerform.mental.calib = 'on';
@@ -68,7 +68,7 @@ switch IRM
     case 0
         taskToPerform.mental.task = 'on';
     case 1 % task will be done in the scanner after the training
-        taskToPerform.mental.task = 'on';
+        taskToPerform.mental.task = 'off';
 end
 switch langue
     case 'f'
