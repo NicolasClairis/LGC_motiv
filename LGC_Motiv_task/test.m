b5db5d597d03b2a640e35c42ab310fd71191ee2e
NicolasClairis <nicolas.clairis@protonmail.com>
2021-10-22 17:47:08 +0200

    changed the test function to test mental display

--- LGC_Motiv_task/test.m
+++ LGC_Motiv_task/test.m
@@ -1,24 +1,26 @@
+[scr, xScreenCenter, yScreenCenter, window, baselineTextSize] = ScreenConfiguration(0, 1);
+ShowCursor;
+[R_money] = R_amounts(3, 'yes');
+langage = 'fr';
+n_E_levels = 3;
+[stim] = stim_initialize(scr, n_E_levels, langage, R_money);
+[key] = relevant_key_definition('mental', 0, 2);
+[mentalE_prm] = mental_effort_parameters();
+mentalE_prm.startAngle = 0;
+sideQuestion = mentalE_prm.sideQuestion;
+taskTypeDisplay = 1;
+numberVector_calib = 1;
+mental_n_col = mentalE_prm.mental_n_col;
+calib_errorLimits_Em.useOfErrorMapping = false;
+calib_errorLimits_Em.useOfErrorThreshold = true;
+calib_errorLimits_Em.errorThreshold = 3;
 
-      
-a = 0;
-timelimit = false;
-t_max = 10;
-keySpace = 65;
+startAngle = 0;
+endAngle = 360;
+maxPerfUntilNowAngle = 270;
 
-onset = GetSecs;
-timeNow = onset;
 
-KbReleaseWait();
-while (a < 5) &&...
-        ( ( (timelimit == true) && (timeNow < onset + t_max) ) || (timelimit == false) )
-    
-    timeNow = GetSecs;
-    
-    [keyisdown, timeAnswer, keyCode] = KbCheck();
-    if keyisdown == 1 && keyCode(keySpace) == 1
-        a = a + 1;
-        disp(num2str(a));
-    end
-    %     WaitSecs(0.001);
-    KbReleaseWait;
-end % loop
+[onset_stim] = mental_display_stim(scr, stim,...
+    startAngle, endAngle,...
+    sideQuestion, taskTypeDisplay, numberVector_calib, mental_n_col,...
+    'noInstructions', maxPerfUntilNowAngle);
