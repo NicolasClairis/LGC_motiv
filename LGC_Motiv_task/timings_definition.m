64a5af4cada21bfaa9390b6aa039128b8027afe2
NicolasClairis <nicolas.clairis@protonmail.com>
2021-10-22 17:46:53 +0200

    Update timings_definition.m
    - choice time now 5 seconds
    - effort time now 8 seconds
    - also reduced time at the end of the scanning to reduce a bit the total waiting time

--- LGC_Motiv_task/timings_definition.m
+++ LGC_Motiv_task/timings_definition.m
@@ -81,12 +81,12 @@ jitterRdmPerm_effortCross = randperm(nTrials);
 t_effortCross = jitters_effortCross(jitterRdmPerm_effortCross);
 
 t_finalCross = 10;
-t_choice = 15;
+t_choice = 5;
 t_dispChoice = 2;
 switch effort_type
     case 'physical' % in case you use different numbers for each effort type
-        t_max_effort = 6; % time to perform the task
-        taskTimes.max_effort = t_max_effort;
+        t_max_physicalEffort = 6; % time to perform the task
+        taskTimes.max_effort = t_max_physicalEffort;
         
         % store frame rate for physical effort task
         % query the frame duration (inter-frame interval)
@@ -97,7 +97,8 @@ switch effort_type
         % too much in the display
         taskTimes.physicalReadWait = t_readWait; % Arthur manual definition
     case 'mental'
-        taskTimes.t_min_scalingFactor = 150/100; % multiply calibrated minimal time by this value
+        t_max_mentalEffort = 8;
+        taskTimes.max_effort = t_max_mentalEffort;
 end
 t_fbk = 1; % feedback display
 t_fail_and_repeat_fbk = 3; % feedback after a failure => repeat the effort after that
@@ -133,7 +134,7 @@ end
 switch effort_type
     case 'physical' % in case you use different numbers for each effort type
         % max effort time
-        trainingTimes.max_effort = t_max_effort;
+        trainingTimes.max_effort = t_max_physicalEffort;
         
         % store frame rate for physical effort task
         % query the frame duration (inter-frame interval)
@@ -144,7 +145,7 @@ switch effort_type
         % too much in the display
         trainingTimes.physicalReadWait = t_readWait; % Arthur manual definition
     case 'mental'
-        trainingTimes.t_min_scalingFactor = 150/100; % multiply calibrated minimal time by this value
+        trainingTimes.max_effort = t_max_mentalEffort;
 end
 trainingTimes.choice                = t_choice;
 trainingTimes.dispChoice            = t_dispChoice;
@@ -152,7 +153,7 @@ trainingTimes.feedback              = t_fbk;
 trainingTimes.fail_and_repeat_fbk   = t_fail_and_repeat_fbk;
 
 %% time feedback end of a block
-mainTimes.endfMRI = 20; % to get end of fMRI response and avoid artifacts
+mainTimes.endfMRI = 15; % to get end of fMRI response and avoid artifacts
 mainTimes.endSession = 3; % display of the total amount of money obtained
 
 end % function
