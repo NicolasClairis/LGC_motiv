be870b6db435c400926674b086323658baf77d58
EvilTuts <43995428+EvilTuts@users.noreply.github.com>
2021-10-25 11:33:20 +0200

    staircase physique run mtn (en mode debug)

--- LGC_Motiv_task/stim_initialize.m
+++ LGC_Motiv_task/stim_initialize.m
@@ -611,9 +611,9 @@ stim.mentalCalibEnd.colour = white;
 
 switch langage
     case 'fr'
-        stim.staircase.text = 'Bravo! Vous allez maintenant jouer pour de l''argent reel';
+        stim.staircase.text = 'Bravo! Vous allez maintenant jouer pour de l''argent reel.';
     case 'engl'
-        stim.staircase.text = 'Congratulations! You will now play for real money';
+        stim.staircase.text = 'Congratulations! You will now play for real money.';
 end
 [~,~,textSizeStaircaseInfo] = DrawFormattedText(window,stim.staircase.text,...
     'center','center',white, wrapat);
