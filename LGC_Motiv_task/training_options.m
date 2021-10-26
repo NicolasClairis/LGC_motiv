3406ee72e77faad14e56001ed319a288596e4db0
NicolasClairis <nicolas.clairis@protonmail.com>
2021-10-26 16:39:01 +0200

    redefining training options

--- LGC_Motiv_task/training_options.m
+++ LGC_Motiv_task/training_options.m
@@ -1,5 +1,5 @@
-function [trainingChoiceOptions, nTrainingTrials] = training_options(taskTrainingCond, n_R_levels, n_E_levels, R_money)
-% [trainingChoiceOptions, nTrainingTrials] = training_options(taskTrainingCond, n_R_levels, n_E_levels)
+function [trainingChoiceOptions] = training_options(taskTrainingCond, n_R_levels, n_E_levels, R_money, nTrainingTrials)
+% [trainingChoiceOptions] = training_options(taskTrainingCond, n_R_levels, n_E_levels, R_money, nTrainingTrials)
 % design of the reward, effort and punishment options for the learning
 % phase
 %
@@ -16,83 +16,54 @@ function [trainingChoiceOptions, nTrainingTrials] = training_options(taskTrainin
 % R_money: structure with monetary amount corresponding to each reward and
 % punishment level
 %
+% nTrainingTrials: number of training trials
+%
 % OUTPUTS
 % trainingChoiceOptions: structure with reward and effort level for each
 % training trial + reward or punishment trial
-%
-% nTrainingTrials: number of training trials
 
 %% define options and reward or punishment trials
-if n_R_levels == 3 && n_E_levels == 3
+if n_R_levels == 4 && n_E_levels == 4
     switch taskTrainingCond
         case 'R'
-            trainingChoiceOptions.R.left    = [1 3 2];
-            trainingChoiceOptions.R.right   = [3 1 1];
-            trainingChoiceOptions.E.left    = [1 1 3];
-            trainingChoiceOptions.E.right   = [3 2 2];
+            R_or_P = repmat({'R'},1,nTrainingTrials);
         case 'P'
-            % would be good to map reward and punishment so that there is
-            % no discrepancy in the average reward and effort levels
-            % between the two training sessions
-            trainingChoiceOptions.R.left    = [1 2 2];
-            trainingChoiceOptions.R.right   = [3 1 3];
-            trainingChoiceOptions.E.left    = [3 1 1];
-            trainingChoiceOptions.E.right   = [1 2 3];
+            R_or_P = repmat({'P'},1,nTrainingTrials);
         case 'RP'
-            trainingChoiceOptions.R.left    = [1 3 2 1 2 2];
-            trainingChoiceOptions.R.right   = [3 1 1 3 1 3];
-            trainingChoiceOptions.E.left    = [1 1 3 3 1 1];
-            trainingChoiceOptions.E.right   = [3 2 2 1 2 3];
+            switch nTrainingTrials
+                case 4
+                    trainingChoiceOptions.R.left    = [0 1 2 0];
+                    trainingChoiceOptions.R.right   = [1 0 0 3];
+                    trainingChoiceOptions.E.left    = [0 3 2 0];
+                    trainingChoiceOptions.E.right   = [1 0 0 2];
+                    R_or_P = {'R','P','P','R'};
+                case 8
+                    trainingChoiceOptions.R.left    = [0 2 0 0 0 1 2 1];
+                    trainingChoiceOptions.R.right   = [1 0 1 3 3 0 0 0];
+                    trainingChoiceOptions.E.left    = [0 2 0 0 0 3 2 3];
+                    trainingChoiceOptions.E.right   = [1 0 1 2 2 0 0 0];
+                    trainingChoiceOptions.R_or_P = {'R','R','P','R','P','P','P','R'};
+                otherwise
+                    error(['case where ',num2str(nTrainingTrials),' training trials not ready yet. Please define design matrix.'])
+            end
+        otherwise
+            error(['Please determine a training sequence for when there are ',num2str(n_R_levels),' reward levels ',...
+                ' and ', num2str(n_E_levels),' effort levels']);
     end
 else
     switch taskTrainingCond
         case 'R'
-    error(['Please determine a training sequence for when there are ',num2str(n_R_levels),' reward levels ',...
-        ' and ', num2str(n_E_levels),' effort levels']);
+            error(['Please determine a training sequence for when there are ',num2str(n_R_levels),' reward levels ',...
+                ' and ', num2str(n_E_levels),' effort levels']);
         case 'P'
-    error(['Please determine a training sequence for when there are ',num2str(n_R_levels),' reward levels ',...
-        ' and ', num2str(n_E_levels),' effort levels']);
+            error(['Please determine a training sequence for when there are ',num2str(n_R_levels),' reward levels ',...
+                ' and ', num2str(n_E_levels),' effort levels']);
         case 'RP'
-            trainingChoiceOptions.R.left    = [1 4 1 4];
-            trainingChoiceOptions.R.right   = [3 1 2 1];
-            trainingChoiceOptions.E.left    = [1 3 1 3];
-            trainingChoiceOptions.E.right   = [2 1 4 1];
+            error(['Please determine a training sequence for when there are ',num2str(n_R_levels),' reward levels ',...
+                ' and ', num2str(n_E_levels),' effort levels']);
     end
 
 end
-
-%% number of trials
-nTrainingTrials = size(trainingChoiceOptions.E.left, 2);
-
-%% reward or punishment
-switch taskTrainingCond
-    case 'R'
-        R_or_P = repmat({'R'},1,nTrainingTrials);
-    case 'P'
-        R_or_P = repmat({'P'},1,nTrainingTrials);
-    case 'RP'
-        R_or_P = [repmat({'R'},1,nTrainingTrials/2), repmat({'P'},1,nTrainingTrials/2)];
-        % use the same sequence for all participants
-        if n_R_levels == 3 && n_E_levels == 3 && nTrainingTrials == 12
-            rand_RP = [4 9 2 6 5 11 10 12 1 7 8 3];
-        elseif n_R_levels == 3 && n_E_levels == 3 && nTrainingTrials == 6
-            rand_RP = [2 3 4 5 1 6];
-        elseif n_R_levels == 4 && n_E_levels == 4 && nTrainingTrials == 4
-            rand_RP = [1 4 3 2];
-        else
-            error(['Please define a pre-determined training sequence for reward and punishment for when ',num2str(n_R_levels),' reward levels ',...
-        ' and ', num2str(n_E_levels),' effort levels and ',num2str(nTrainingTrials),' trials']);
-        end
-        R_or_P = R_or_P(rand_RP);
-        
-        % re-order options based on RP order randomisation
-        trainingChoiceOptions.R.left = trainingChoiceOptions.R.left(rand_RP);
-        trainingChoiceOptions.E.left = trainingChoiceOptions.E.left(rand_RP);
-        trainingChoiceOptions.R.right = trainingChoiceOptions.R.right(rand_RP);
-        trainingChoiceOptions.E.right = trainingChoiceOptions.E.right(rand_RP);
-end
-% store reward/punishment condition for each trial
-trainingChoiceOptions.R_or_P = R_or_P;
 
 %% convert data in monetary amounts
 if exist('R_money','var') && ~isempty(R_money) % you also need this function for the preparation of the timings, no need to compute this in this case
