76a3e6ea2422d51b51bf51ca68228ba781e564f3
NicolasClairis <nicolas.clairis@protonmail.com>
2021-12-03 11:56:53 +0100

    increase smoothing parameter

--- LGC_Motiv_analysis/fMRI/preprocessing_batch.m
+++ LGC_Motiv_analysis/fMRI/preprocessing_batch.m
@@ -65,7 +65,7 @@ nb_preprocessingSteps = 6;
  % 6) smoothing functional
  
  %% smoothing kernel
- smKernel = 6;
+ smKernel = 8;
  
  %% initialize batch
  matlabbatch = cell(nb_preprocessingSteps*NS,1);
