9033b13e42d7fec978f41200948045ffdd2d0b0e
NicolasClairis <nicolas.clairis@protonmail.com>
2021-12-06 21:36:24 +0100

    Create logitfit_choices.m

---
+++ LGC_Motiv_analysis/behavior/logitfit_choices.m
@@ -0,0 +1,15 @@
+function[] = logitfit_choices()
+% 
+% logitfit_choices will perform a logistic regression on the choices
+% performed by the participants
+
+
+deltaR_Ep = ;
+deltaEp = ;
+deltaP_Ep = ;
+
+deltaR_Em = ;
+deltaEm = ;
+deltaP_Em = ;
+
+end % function
