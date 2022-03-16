bb8ab5361479e5dd6ec07c5178795eb8b0feeda1
NicolasClairis <nicolas.clairis@protonmail.com>
2022-03-16 18:12:08 +0100

    adding Gln/Glu

--- LGC_Motiv_analysis/metabolite_load.m
+++ LGC_Motiv_analysis/metabolite_load.m
@@ -37,6 +37,7 @@ for iROI = 1:nROIs
     for iMet = 1:n_metabolites
         metabolites.(ROI_nm).(all_metabolites{iMet}) = NaN(1,NS);
     end % metabolite loop
+    metabolites.(ROI_nm).Gln_div_Glu = NaN(1,NS);
     
     %% load the data
     switch ROI_nm
@@ -49,14 +50,17 @@ for iROI = 1:nROIs
         'Sheet',sheet_nm);
     
     %% extract the data for each subject
-    for iMet = 1:n_metabolites
-        met_nm = all_metabolites{iMet};
-        for iS = 1:NS
-            sub_nm = subject_id{iS};
-            subj_line = strcmp(excelRead_tmp.CID, sub_nm);
+    for iS = 1:NS
+        sub_nm = subject_id{iS};
+        subj_line = strcmp(excelRead_tmp.CID, sub_nm);
+        for iMet = 1:n_metabolites
+            met_nm = all_metabolites{iMet};
             metabolites.(ROI_nm).(met_nm)(iS) = excelRead_tmp.(met_nm)(subj_line);
-        end % subject loop
-    end % metabolites
+        end % metabolites
+        
+        % perform also division Glu/Gln
+        metabolites.(ROI_nm).Gln_div_Glu(iS) = metabolites.(ROI_nm).Gln(iS)./metabolites.(ROI_nm).Glu(iS);
+    end % subject loop
 end % ROI loop
 
 end % function
