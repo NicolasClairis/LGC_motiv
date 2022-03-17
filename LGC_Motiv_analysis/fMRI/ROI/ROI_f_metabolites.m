6ca74247685692abca1a89e1ba0d1a91c39e86a0
NicolasClairis <nicolas.clairis@protonmail.com>
2022-03-17 11:28:02 +0100

    roi update

--- LGC_Motiv_analysis/fMRI/ROI/ROI_f_metabolites.m
+++ LGC_Motiv_analysis/fMRI/ROI/ROI_f_metabolites.m
@@ -1,6 +1,11 @@
-% function aimed at extracting ROI in a given GLM based on the level of a
+% script aimed at extracting ROI in a given GLM based on the level of a
 % metabolite of interest (to select) in one of the two brain areas of study
-% 1 that can be checked (dmPFC/aIns)
+% that can be checked (dmPFC/aIns). Then, you can select some specific
+% contrast of interest and look at the difference in BOLD activity between
+% the two groups
+
+%% figure display?
+fig_disp = 1;
 
 %% define metabolite and ROI you want to focus on
 % ROI
@@ -52,11 +57,40 @@ for iCon = 1:n_cons
         con_sem_lowMet(iCon),...
         con_std_lowMet(iCon)] = mean_sem_sd(con_vec_all(iCon, low_met_subs),2);
     [con_avg_highMet(iCon),...
-        con_sem_lowMet(iCon),...
-        con_std_lowMet(iCon)] = mean_sem_sd(con_vec_all(iCon, high_met_subs),2);
+        con_sem_highMet(iCon),...
+        con_std_highMet(iCon)] = mean_sem_sd(con_vec_all(iCon, high_met_subs),2);
     [~,ttest_pval_lowMet_vs_highMet(iCon),~,stats_tmp] = ttest2(con_vec_all(iCon, low_met_subs), con_vec_all(iCon, high_met_subs));
     ttest_tval_lowMet_vs_highMet(iCon) = stats_tmp.tstat;
 end % contrast loop
 
-%% figure
+%% how many figures do you want to plot
+if fig_disp == 1
+    n_figs = spm_input('How many figures?',1,'e');
 
+    % prepare contrast names for question
+    list_con_question = con_names{1};
+    for iCon = 2:n_cons
+        list_con_question = [list_con_question,' | ',con_names{iCon}];
+    end
+
+    %% select contrast of interest (ie those you would like to display in each figure)
+    for iFig = 1:n_figs
+        % select contrasts to display
+        which_con = zeros(n_figs, n_cons); % column with 0/1 to see which contrasts to display at the end
+        % select which contrasts you want to display for each figure
+        selectedContrast = spm_input(['What contrast for fig.',num2str(iFig),' ?'],1,'m',...
+            list_con_question, ...
+            1:n_cons, 0);
+        which_con(iFig,selectedContrast) = 1; % 1 for selected contrasts
+        % give name to the contrast for the figure
+        figConName= spm_input([con_names{figCon_idx(iCon)},...
+            ' short name:'],1,'s');
+
+        %% figure
+        [roi_fig] = roi_graph2(selectedCon,...
+            con_avg_lowMet, con_sem_lowMet,...
+            con_avg_highMet, con_sem_highMet,...
+            figConName, ttest_pval_lowMet_vs_highMet,...
+            ['low ',metabolite_nm],['high',metabolite_nm]);
+    end % figure loop
+end % figure display
