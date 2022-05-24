7c22da8ca7ed1e1c0e8f0fca03f943079d5d76eb
NicolasClairis <nicolas.clairis@protonmail.com>
2022-05-24 10:44:53 +0200

    update script for eNAMPT correlations
    more variables checked + some statistics have been added

--- LGC_Motiv_analysis/physio/check_eNAMPT_correlates.m
+++ LGC_Motiv_analysis/physio/check_eNAMPT_correlates.m
@@ -1,3 +1,11 @@
+%% check_eNAMPT_correlates is a script to look at correlations between 
+% several factors of interest (BMI, age, date of acquisition, gender, etc.)
+% and the eNAMPT concentrations in the plasma in study 1.
+
+%% load data
+load('M:\Nicolas_Clairis\study1\subs_physio_variables.mat',...
+    'BMI','age','gender','eNAMPT','datesExp','weight','height');
+
 %% look at correlation between BMI and eNAMPT concentrations
 
 figure;
@@ -9,9 +17,9 @@ hold on;
 
 % add thresholds for anorexia/obesity
 xThresholds = [18.5, 25, 30];
-for iWeight = 1:length(xThresholds)
-    jWeight = xThresholds(iWeight);
-    hdl = line([jWeight jWeight],[0 30]);
+for iBMI = 1:length(xThresholds)
+    jBMI = xThresholds(iBMI);
+    hdl = line([jBMI jBMI],[0 30]);
     hdl.LineWidth = 3; hdl.Color = [0 0 0];
 end
 
@@ -20,6 +28,45 @@ xlabel('BMI');
 ylabel('eNAMPT (ng/mL)');
 legend_size(30);
 
+%% look at correlation between weight and eNAMPT concentrations
+
+figure;
+curveHdl = scatter(weight,eNAMPT);
+curveHdl.MarkerEdgeColor = [0 0 0];
+curveHdl.LineWidth = 2;
+curveHdl.SizeData = 100;
+
+% add legends
+xlabel('weight (kg)');
+ylabel('eNAMPT (ng/mL)');
+ylim([0 30])
+legend_size(30);
+
+% stats
+goodSubs_weight = (~isnan(weight) & ~isnan(eNAMPT));
+% do the stats
+weight_corrcoeff = corr2(weight(goodSubs_weight), eNAMPT(goodSubs_weight));
+[weight_betas, ~, weight_stats] = glmfit(weight(goodSubs_weight), eNAMPT(goodSubs_weight),'normal');
+
+%% look at correlation between height and eNAMPT concentrations
+
+figure;
+curveHdl = scatter(height,eNAMPT);
+curveHdl.MarkerEdgeColor = [0 0 0];
+curveHdl.LineWidth = 2;
+curveHdl.SizeData = 100;
+
+% add legends
+xlabel('height (m)');
+ylabel('eNAMPT (ng/mL)');
+ylim([0 30])
+legend_size(30);
+
+% stats
+goodSubs_weight = (~isnan(weight) & ~isnan(eNAMPT));
+% do the stats
+weight_corrcoeff = corr2(weight(goodSubs_weight), eNAMPT(goodSubs_weight));
+[weight_betas, ~, weight_stats] = glmfit(weight(goodSubs_weight), eNAMPT(goodSubs_weight),'normal');
 
 %% look at correlation between gender and eNAMPT
 figure;
@@ -47,11 +94,15 @@ legend_size(30);
 ylim([0 30]);
 
 %% look correlation between date of experiment and eNAMPT
-datesExp = NaN(1,length(eNAMPT));
-NS = length(eNAMPT);
-for iS = 1:NS
-    datesExp(iS) = datenum(dates{iS},'DD.mm.yyyy');
-end
+
+% convert dates of experiment into seconds
+% datesExp = NaN(1,length(eNAMPT));
+% NS = length(eNAMPT);
+% for iS = 1:NS
+%     datesExp(iS) = datenum(dates{iS},'DD.mm.yyyy');
+% end
+
+% display results
 figure;
 curveHdl = scatter(datesExp, eNAMPT);
 curveHdl.MarkerEdgeColor = [0 0 0];
@@ -74,7 +125,6 @@ for iCTQ = 1:n_ctq_scores
     ctq_field = ctq_fields{iCTQ};
     subplot(2,3,iCTQ);
     CTQ_var = CTQ.(ctq_field);
-    CTQ_eNAMPT_corr.(ctq_field) = corrcoef(CTQ_var,eNAMPT);
     curveHdl = scatter(CTQ_var, eNAMPT);
     curveHdl.MarkerEdgeColor = [0 0 0];
     curveHdl.LineWidth = 2;
@@ -88,4 +138,18 @@ for iCTQ = 1:n_ctq_scores
     if ~strcmp(ctq_field, 'md')
         xlim([5 25]);
     end
+    
+    % do some stats
+    % 1) remove NaN
+    goodSubs = (~isnan(CTQ_var) & ~isnan(eNAMPT));
+    % do the stats
+    CTQ_vs_eNAMPT_stats.(ctq_field).corrcoeff = corr2(CTQ_var(goodSubs), eNAMPT(goodSubs));
+    [CTQ_vs_eNAMPT_stats.(ctq_field).betas,...
+        ~,...
+        CTQ_vs_eNAMPT_stats.(ctq_field).stats] = glmfit(CTQ_var(goodSubs),...
+        eNAMPT(goodSubs),'normal');
+%     [CTQ_vs_eNAMPT_stats.(ctq_field).betas,...
+%         ~,...
+%         CTQ_vs_eNAMPT_stats.(ctq_field).stats] = glmfit(CTQ_var(goodSubs),...
+%         eNAMPT(goodSubs),'normal','constant','off');
 end
