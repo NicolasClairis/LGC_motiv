afb0ce6d866794e4e47a7313691da49aa54046db
NicolasClairis <nicolas.clairis@protonmail.com>
2022-06-08 18:21:06 +0200

    adding GLM 52

--- LGC_Motiv_analysis/fMRI/which_GLM.m
+++ LGC_Motiv_analysis/fMRI/which_GLM.m
@@ -1540,6 +1540,33 @@ switch GLM
             GLMprm.model_onset.(Epm_nm).fbk = 'stick';
             GLMprm.fbk.(Epm_nm).RP.money_obtained = 1;
         end % physical/mental  loop
+    case 52 % same as GLM 50 but with behavioral model 3 instead of model 4
+        % general parameters
+        GLMprm.gal.orth_vars = 0;
+        GLMprm.gal.zPerRun = 1;
+        % loop per task
+        for iEpm = 1:length(Epm)
+            Epm_nm = Epm{iEpm};
+            % initial cross
+            GLMprm.model_onset.(Epm_nm).preChoiceCross = 'stick';
+            % choice
+            GLMprm.model_onset.(Epm_nm).choice = 'stick';
+            GLMprm.choice.(Epm_nm).RP.NV_varOption = 1;
+            GLMprm.choice.(Epm_nm).RP.NV_mdl = 'mdl_3';
+            GLMprm.choice.(Epm_nm).RP.confidence = 2;
+            GLMprm.choice.(Epm_nm).RP.conf_mdl = 'mdl_3';
+            GLMprm.choice.(Epm_nm).RP.RT = 1;
+            % chosen
+            GLMprm.model_onset.(Epm_nm).chosen = 'stick';
+            GLMprm.chosen.(Epm_nm).RP.E_chosen = 1;
+            % pre-effort cross
+            GLMprm.model_onset.(Epm_nm).preEffortCross = 'stick';
+            % effort perf (effort execution)
+            GLMprm.model_onset.(Epm_nm).Eperf = 'stick';
+            % feedback
+            GLMprm.model_onset.(Epm_nm).fbk = 'stick';
+            GLMprm.fbk.(Epm_nm).RP.money_obtained = 1;
+        end % physical/mental  loop
 end % GLM number
 %% warnings: check compatibility of the GLM parameters entered
 isGLMokCheck(GLMprm);
