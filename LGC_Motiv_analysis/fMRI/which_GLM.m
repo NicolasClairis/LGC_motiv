7c18225bfed8197b074e3e751e2a47a791d91c10
NicolasClairis <nicolas.clairis@protonmail.com>
2022-06-08 16:58:49 +0200

    adding GLM 51

--- LGC_Motiv_analysis/fMRI/which_GLM.m
+++ LGC_Motiv_analysis/fMRI/which_GLM.m
@@ -1514,6 +1514,32 @@ switch GLM
             GLMprm.model_onset.(Epm_nm).fbk = 'stick';
             GLMprm.fbk.(Epm_nm).RP.money_obtained = 1;
         end % physical/mental  loop
+    case 51 % same as GLM 50 but decomposing net value in Rnd/End/trialN*End
+        % general parameters
+        GLMprm.gal.orth_vars = 0;
+        GLMprm.gal.zPerRun = 1;
+        % loop per task
+        for iEpm = 1:length(Epm)
+            Epm_nm = Epm{iEpm};
+            % initial cross
+            GLMprm.model_onset.(Epm_nm).preChoiceCross = 'stick';
+            % choice
+            GLMprm.model_onset.(Epm_nm).choice = 'stick';
+            GLMprm.choice.(Epm_nm).RP.money_varOption = 1;
+            GLMprm.choice.(Epm_nm).RP.E_varOption = 1;
+            GLMprm.choice.(Epm_nm).RP.trialN = 4;
+            GLMprm.choice.(Epm_nm).RP.RT = 1;
+            % chosen
+            GLMprm.model_onset.(Epm_nm).chosen = 'stick';
+            GLMprm.chosen.(Epm_nm).RP.E_chosen = 1;
+            % pre-effort cross
+            GLMprm.model_onset.(Epm_nm).preEffortCross = 'stick';
+            % effort perf (effort execution)
+            GLMprm.model_onset.(Epm_nm).Eperf = 'stick';
+            % feedback
+            GLMprm.model_onset.(Epm_nm).fbk = 'stick';
+            GLMprm.fbk.(Epm_nm).RP.money_obtained = 1;
+        end % physical/mental  loop
 end % GLM number
 %% warnings: check compatibility of the GLM parameters entered
 isGLMokCheck(GLMprm);
