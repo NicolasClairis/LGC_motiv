90fb0cf1af755b58f8f85e77569fe2b567df1f13
NicolasClairis <nicolas.clairis@protonmail.com>
2022-06-29 11:37:34 +0200

    adding bayesian model data

--- LGC_Motiv_analysis/behavior/parameters_f_metabolites.m
+++ LGC_Motiv_analysis/behavior/parameters_f_metabolites.m
@@ -29,37 +29,67 @@ condition = subject_condition();
 metabolite_allSubs = metabolites.(ROI_nm).(metabolite_nm);
 
 %% extract behavioral parameters
-%% perform behavioral model
-figDispGroup = 0;
-dispMoneyOrLevels = 'levels';
-[betas_fullList, pvalues_fullList] = logitfit_choices_group(figDispGroup, dispMoneyOrLevels);
+listPossibleConditions = {'bayesian','simple'};
+mdlType_idx = listdlg('promptstring','Which model type?','ListString',listPossibleConditions);
+mdlType = listPossibleConditions{mdlType_idx};
 
-[prm.kRp, prm.kPp, prm.kEp, prm.kFp,...
-    prm.kRm, prm.kPm, prm.kEm, prm.kFm] = deal(NaN(1,NS));
-mdl_nm = 'mdl_3';
-for iS = 1:NS
-    sub_nm = subject_id{iS};
-    % extract physical task parameters
-    sub_idx = strcmp(betas_fullList.subList,sub_nm);
-    prm.kRp(iS) = betas_fullList.Ep.(mdl_nm).kR(sub_idx);
-    prm.kPp(iS) = betas_fullList.Ep.(mdl_nm).kP(sub_idx);
-    prm.kEp(iS) = betas_fullList.Ep.(mdl_nm).kEffort(sub_idx);
-    prm.kFp(iS) = betas_fullList.Ep.(mdl_nm).kFatigue(sub_idx);
-    % extract mental task parameters
-    prm.kRm(iS) = betas_fullList.Em.(mdl_nm).kR(sub_idx);
-    prm.kPm(iS) = betas_fullList.Em.(mdl_nm).kP(sub_idx);
-    prm.kEm(iS) = betas_fullList.Em.(mdl_nm).kEffort(sub_idx);
-    prm.kFm(iS) = betas_fullList.Em.(mdl_nm).kFatigue(sub_idx);
-end % subject list
+
+switch mdlType
+    case 'bayesian'
+        bayesian_mdl = getfield(load('behavioral_prm_tmp.mat','bayesian_mdl3'),...
+            'bayesian_mdl3');
+        [prm.kR, prm.kP,...
+            prm.kEp, prm.kFp,...
+            prm.kEm, prm.kFm] = deal(NaN(1,NS));
+        parameters = {'kR','kP',...
+            'kEp','kFp',...
+            'kEm','kFm'};
+        for iS = 1:NS
+            sub_nm = subject_id{iS};
+            % extract parameters
+            sub_idx = strcmp(bayesian_mdl.subject_id, sub_nm);
+            if sum(sub_idx == 1)
+                prm.kR(iS) = bayesian_mdl.kR(sub_idx);
+                prm.kP(iS) = bayesian_mdl.kP(sub_idx);
+                prm.kEp(iS) = bayesian_mdl.kEp(sub_idx);
+                prm.kFp(iS) = bayesian_mdl.kFp(sub_idx);
+                prm.kEm(iS) = bayesian_mdl.kEm(sub_idx);
+                prm.kFm(iS) = bayesian_mdl.kFm(sub_idx);
+            end % filter if subject extracted by Arthur
+        end % subject list
+    case 'simple'
+        %% perform behavioral model
+        figDispGroup = 0;
+        dispMoneyOrLevels = 'levels';
+        [betas_fullList, pvalues_fullList] = logitfit_choices_group(figDispGroup, dispMoneyOrLevels);
+
+        [prm.kRp, prm.kPp, prm.kEp, prm.kFp,...
+            prm.kRm, prm.kPm, prm.kEm, prm.kFm] = deal(NaN(1,NS));
+        parameters = {'kRp','kPp','kEp','kFp',...
+            'kRm','kPm','kEm','kFm'};
+        mdl_nm = 'mdl_3';
+        for iS = 1:NS
+            sub_nm = subject_id{iS};
+            % extract physical task parameters
+            sub_idx = strcmp(betas_fullList.subList,sub_nm);
+            prm.kRp(iS) = betas_fullList.Ep.(mdl_nm).kR(sub_idx);
+            prm.kPp(iS) = betas_fullList.Ep.(mdl_nm).kP(sub_idx);
+            prm.kEp(iS) = betas_fullList.Ep.(mdl_nm).kEffort(sub_idx);
+            prm.kFp(iS) = betas_fullList.Ep.(mdl_nm).kFatigue(sub_idx);
+            % extract mental task parameters
+            prm.kRm(iS) = betas_fullList.Em.(mdl_nm).kR(sub_idx);
+            prm.kPm(iS) = betas_fullList.Em.(mdl_nm).kP(sub_idx);
+            prm.kEm(iS) = betas_fullList.Em.(mdl_nm).kEffort(sub_idx);
+            prm.kFm(iS) = betas_fullList.Em.(mdl_nm).kFatigue(sub_idx);
+        end % subject list
+end % bayesian/behavioral
 
 %% perform GLM and correlation between behavioral parameters and spectroscopy
-parameters = {'kRp','kPp','kEp','kFp',...
-    'kRm','kPm','kEm','kFm'};
 n_prm = length(parameters);
 % remove NaN subjects
-goodSubs = ~isnan(metabolite_allSubs);
 for iPrm = 1:n_prm
     prm_nm = parameters{iPrm};
+    goodSubs = ~isnan(metabolite_allSubs).*~isnan(prm.(prm_nm));
     var_nm = [prm_nm,'_f_',metabolite_nm];
     [betas.(var_nm), ~,stats_tmp] = glmfit(metabolite_allSubs(goodSubs), prm.(prm_nm)(goodSubs),'normal');
     pval.(var_nm) = stats_tmp.p;
