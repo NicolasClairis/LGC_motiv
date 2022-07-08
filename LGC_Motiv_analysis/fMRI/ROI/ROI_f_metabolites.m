cacbc5a4865cc3acf3306714f1ec0949516d7e04
NicolasClairis <nicolas.clairis@protonmail.com>
2022-07-08 10:07:12 +0200

    Update ROI_f_metabolites.m

--- LGC_Motiv_analysis/fMRI/ROI/ROI_f_metabolites.m
+++ LGC_Motiv_analysis/fMRI/ROI/ROI_f_metabolites.m
@@ -47,6 +47,8 @@ n_ROIs = size(con_vec_all,3);
 
 %% how many figures do you want to plot
 if fig_disp == 1
+    pSize = 50;
+    lSize  = 3;
     n_figs = spm_input('How many figures?',1,'e');
 
     % prepare contrast names for question
@@ -73,22 +75,21 @@ if fig_disp == 1
             ROI_BOLD_nm = ROI_coords.ROI_nm.(['ROI_',num2str(iROI),'_shortName']);
             
             %% perform the correlation
-            beta_values = con_vec_all(selectedContrast,:,iROI);
-            [betas_tmp, ~, stats_tmp] = glmfit(metabolite_allSubs, beta_values, 'normal');
+            ROI_beta_values = NaN(1,NS);
+            ROI_beta_values(:) = con_vec_all(selectedContrast,:,iROI);
+            [betas_tmp, ~, stats_tmp] = glmfit(metabolite_allSubs, ROI_beta_values, 'normal');
             betas.(['MRS_',ROI_nm,'-',metabolite_nm]).(['fMRI_',ROI_BOLD_nm]) = betas_tmp;
             pval.(['MRS_',ROI_nm,'-',metabolite_nm]).(['fMRI_',ROI_BOLD_nm]) = stats_tmp.p;
+            fitted_ROIbetas_tmp = glmval(betas_tmp, metabolite_allSubs, 'identity');
             
-            % first figure: simple bar graph
+            % display figure with correlation data
             fig;
-            scatter(metabolite_allSubs, beta_values);
+            hold on;
+            scatter(metabolite_allSubs, ROI_beta_values,...
+                'LineWidth',3);
+            plot(metabolite_allSubs, fitted_ROIbetas_tmp,...
+                'LineStyle','--','LineWidth',lSize,'Color','k');
             legend_size(pSize);
-            % second figure: same but with violin plots
-            [roi_fig2] = roi_graph3(selectedContrast,...
-                con_vec_all(:,:, iROI),...
-                low_met_subs, high_met_subs,...
-                figConName, ttest_pval_lowMet_vs_highMet(iROI,:),...
-                ['low ',metabolite_nm],['high ',metabolite_nm],...
-                ROI_BOLD_nm);
         end % ROI loop
     end % figure loop
 end % figure display
