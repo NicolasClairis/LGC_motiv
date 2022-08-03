303411db32026be9179ee182084fce1421a5cb52
NicolasClairis <nicolas.clairis@protonmail.com>
2022-08-03 19:01:41 +0200

    Create load_questionnaires_data.m

---
+++ LGC_Motiv_analysis/behavior/load_questionnaires_data.m
@@ -0,0 +1,22 @@
+function[excelReadQuestionnairesFile] = load_questionnaires_data()
+% [excelReadQuestionnairesFile] = load_questionnaires_data()
+%load_questionnaires_data will load Questionnaires-scores3.xlsx data in matlab as a big table.
+
+%% define path
+list_pcs = {'Lab','Home'};
+which_pc_idx = listdlg('PromptString',{'Lab or home pc?'},...
+    'SelectionMode','single','ListString',list_pcs);
+switch list_pcs{which_pc_idx}
+    case 'Lab'
+        hardDisk = 'M:';
+    case 'Home'
+        hardDisk = 'L:';
+end
+pcPath = fullfile(hardDisk,'human_data_private','raw_data_subject','study1');
+cd(pcPath);
+
+%% extract the data
+excelReadQuestionnairesFile = readtable([pcPath,filesep,'Questionnaire-scores3.xlsx'],...
+        'Sheet','Sheet1');
+
+end % function
