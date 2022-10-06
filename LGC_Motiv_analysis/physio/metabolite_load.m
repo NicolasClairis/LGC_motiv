238eb2a1d0e439424610eb717048d4b213abdf28
NicolasClairis <nicolas.clairis@protonmail.com>
2022-10-06 11:01:29 +0200

    adding filter for outliers (mean+3*sd)

--- LGC_Motiv_analysis/physio/metabolite_load.m
+++ LGC_Motiv_analysis/physio/metabolite_load.m
@@ -73,24 +73,38 @@ for iROI = 1:nROIs
         'Sheet',CRLB_sheet_nm);
     
     %% extract the data for each subject
-    for iS = 1:NS
-        sub_nm = subject_id{iS};
-        subj_line = strcmp(excelRead_tmp.CID, sub_nm);
-        subj_CRLB_line = strcmp(CRLB_excelRead_tmp.CID, sub_nm);
-        for iMet = 1:n_metabolites
-            met_nm = all_metabolites{iMet};
+    for iMet = 1:n_metabolites
+        met_nm = all_metabolites{iMet};
+        % extract data for everybody for the current metabolite
+        all_met_data = excelRead_tmp.(met_nm);
+        % compute the mean and SD across participants
+        mean_met_aSubs = mean(all_met_data, 2, 'omitnan');
+        sd_met_aSubs = std(all_met_data, 0, 2, 'omitnan');
+        % identify subjects with metabolite values too far from the SD
+        bad_met_subs = (all_met_data > mean_met_aSubs + sd_met_aSubs.*3) | (all_met_data < mean_met_aSubs + sd_met_aSubs.*3);
+        for iS = 1:NS
+            sub_nm = subject_id{iS};
+            subj_line = strcmp(excelRead_tmp.CID, sub_nm);
+            % check Cramer Lower Bound for the subject, ROI and metabolite
+            % currently assessed
+            subj_CRLB_line = strcmp(CRLB_excelRead_tmp.CID, sub_nm);
             CRLB_tmp = CRLB_excelRead_tmp.(met_nm)(subj_CRLB_line);
-            if excelRead_tmp.(met_nm)(subj_line) > 0 && CRLB_tmp <= CRLB_threshold
-                % ignore subjects where data has not been extracted (=0 when not extracted) 
+            % check if the subject is not an outlier
+            is_sub_sd_outlier_tmp  = bad_met_subs(subj_line);
+            if excelRead_tmp.(met_nm)(subj_line) > 0 &&...
+                    CRLB_tmp <= CRLB_threshold &&...
+                    is_sub_sd_outlier_tmp == 0
+                % ignore subjects where data has not been extracted (=0 when not extracted)
                 % and subjects for which the Cramer Lower Bound is too bad
-                % (>50%)
-                metabolites.(ROI_nm).(met_nm)(iS) = excelRead_tmp.(met_nm)(subj_line);
+                % (>50%) and subjects which are clearly outliers because
+                % their values are too far from the others (>/< mean+3*SD)
+                metabolites.(ROI_nm).(met_nm)(iS) = all_met_data(subj_line);
             end
-        end % metabolites
+        end % subject loop
         
         % perform also division Glu/Gln
         metabolites.(ROI_nm).Gln_div_Glu(iS) = metabolites.(ROI_nm).Gln(iS)./metabolites.(ROI_nm).Glu(iS);
-    end % subject loop
+    end % metabolites
 end % ROI loop
 
 %% go back to root
