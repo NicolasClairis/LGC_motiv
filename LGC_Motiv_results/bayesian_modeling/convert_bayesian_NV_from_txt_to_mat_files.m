69174caadf0276e0c6a4eab75d5a9e66e61fabbf
NicolasClairis <nicolas.clairis@protonmail.com>
2022-10-12 18:03:50 +0200

    renamed correctly

--- LGC_Motiv_results/bayesian_modeling/convert_bayesian_NV_from_txt_to_mat_files.m
+++ LGC_Motiv_results/bayesian_modeling/convert_bayesian_NV_from_txt_to_mat_files.m
@@ -22,14 +22,14 @@ nRuns = 4;
 model_NV_raw.mdl1 = load([gitPath,'net_values_biais.txt']);
 sub_id_line1 = model_NV_raw.mdl1(1,:);
 NV_data_mdl1 = model_NV_raw.mdl1(2:end,:);
-[bayesian_NV.mdl1.subject_id] = convert_sub_id_from_num_to_cell(sub_id_line1);
-NS = length(bayesian_NV.mdl1.subject_id);
+[bayesian_deltaNV.mdl1.subject_id] = convert_sub_id_from_num_to_cell(sub_id_line1);
+NS = length(bayesian_deltaNV.mdl1.subject_id);
 for iS = 1:NS
-    sub_nm = bayesian_NV.mdl1.subject_id{iS};
+    sub_nm = bayesian_deltaNV.mdl1.subject_id{iS};
     for iRun = 1:nRuns
         run_nm = ['run',num2str(iRun)];
         trial_idx = (1:nTrialsPerRun) + nTrialsPerRun*(iRun - 1);
-        bayesian_NV.mdl1.(['CID',sub_nm]).(run_nm) = NV_data_mdl1(trial_idx);
+        bayesian_deltaNV.mdl1.(['CID',sub_nm]).(run_nm) = NV_data_mdl1(trial_idx);
     end % run loop
 end % subject loop
 
@@ -37,16 +37,16 @@ end % subject loop
 model_NV_raw.mdl2 = load([gitPath,'net_values_classic.txt']);
 sub_id_line2 = model_NV_raw.mdl2(1,:);
 NV_data_mdl2 = model_NV_raw.mdl2(2:end,:);
-[bayesian_NV.mdl2.subject_id] = convert_sub_id_from_num_to_cell(sub_id_line2);
-NS = length(bayesian_NV.mdl2.subject_id);
+[bayesian_deltaNV.mdl2.subject_id] = convert_sub_id_from_num_to_cell(sub_id_line2);
+NS = length(bayesian_deltaNV.mdl2.subject_id);
 for iS = 1:NS
-    sub_nm = bayesian_NV.mdl2.subject_id{iS};
+    sub_nm = bayesian_deltaNV.mdl2.subject_id{iS};
     for iRun = 1:nRuns
         run_nm = ['run',num2str(iRun)];
         trial_idx = (1:nTrialsPerRun) + nTrialsPerRun*(iRun - 1);
-        bayesian_NV.mdl2.(run_nm) = NV_data_mdl2(trial_idx);
+        bayesian_deltaNV.mdl2.(run_nm) = NV_data_mdl2(trial_idx);
     end % run loop
 end % subject loop
 
 %% make one global save
-save([gitPath, 'bayesian_NV_data.mat'],'bayesian_NV');
+save([gitPath, 'bayesian_deltaNV_data.mat'],'bayesian_deltaNV');
