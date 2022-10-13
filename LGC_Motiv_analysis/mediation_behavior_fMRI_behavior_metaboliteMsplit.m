c9a571949a04886309aedfd33ee26092af58b52c
NicolasClairis <nicolas.clairis@protonmail.com>
2022-10-13 10:52:11 +0200

    Update mediation_behavior_fMRI_behavior_metaboliteMsplit.m

--- LGC_Motiv_analysis/mediation_behavior_fMRI_behavior_metaboliteMsplit.m
+++ LGC_Motiv_analysis/mediation_behavior_fMRI_behavior_metaboliteMsplit.m
@@ -19,24 +19,32 @@ metab_idx.high = metabolite_allSubs > med_metab;
 [pval,...
     a_path_allSubs, b_path_allSubs, c_path_allSubs, c_prime_path_allSubs,...
     subject_id,...
-    input_f_input_bin, ROI_f_input_bin, output_f_input_bin,...
-    ROI_f_ROI_bin, output_f_ROI_bin,...
+    input_f_input_bin_allSubs, ROI_f_input_bin_allSubs, output_f_input_bin_allSubs,...
+    ROI_f_ROI_bin_allSubs, output_f_ROI_bin_allSubs,...
     input_prm_nm, ROI_short_nm, output_prm_nm] = mediation_behavior_fMRI_behavior(study_nm, condition, subject_id);
 
 mSplit_grps = {'low','high'};
 n_grps = length(mSplit_grps);
 for iGrp = 1:n_grps
     grp_nm = mSplit_grps{iGrp};
-    a_path.(grp_nm) = a_path_allSubs(metab_idx.(grp_nm));
-    b_path.(grp_nm) = b_path_allSubs(metab_idx.(grp_nm));
-    c_path.(grp_nm) = c_path_allSubs(metab_idx.(grp_nm));
-    c_prime_path.(grp_nm) = c_prime_path_allSubs(metab_idx.(grp_nm));
+    sub_idx_tmp = metab_idx.(grp_nm);
+    a_path.(grp_nm) = a_path_allSubs(sub_idx_tmp);
+    b_path.(grp_nm) = b_path_allSubs(sub_idx_tmp);
+    c_path.(grp_nm) = c_path_allSubs(sub_idx_tmp);
+    c_prime_path.(grp_nm) = c_prime_path_allSubs(sub_idx_tmp);
     
     % look at how significant is each path for each group
     [~,pval.(grp_nm).a] = ttest(a_path.(grp_nm));
     [~,pval.(grp_nm).b] = ttest(b_path.(grp_nm));
     [~,pval.(grp_nm).c] = ttest(c_path.(grp_nm));
     [~,pval.(grp_nm).c_prime] = ttest(c_prime_path.(grp_nm));
+    
+    % extract data within each group
+    input_f_input_bin.(grp_nm)  = input_f_input_bin(:, sub_idx_tmp);
+    ROI_f_input_bin.(grp_nm)    = ROI_f_input_bin_allSubs(:, sub_idx_tmp);
+    output_f_input_bin.(grp_nm) = output_f_input_bin_allSubs(:, sub_idx_tmp);
+    ROI_f_ROI_bin.(grp_nm)      = ROI_f_ROI_bin_allSubs(:, sub_idx_tmp);
+    output_f_ROI_bin.(grp_nm)   = output_f_ROI_bin_allSubs(:, sub_idx_tmp);
 end
 
 %% compare the paths between the two groups with unpaired t.test
@@ -48,12 +56,86 @@ end
 %% look at average within each group
 for iGrp = 1:n_grps
     grp_nm = mSplit_grps{iGrp};
+    % average mediation paths
     a_path.mean.(grp_nm) = mean(a_path.(grp_nm),2,'omitnan');
     b_path.mean.(grp_nm) = mean(b_path.(grp_nm),2,'omitnan');
     c_path.mean.(grp_nm) = mean(c_path.(grp_nm),2,'omitnan');
     c_prime_path.mean.(grp_nm) = mean(c_prime_path.(grp_nm),2,'omitnan');
+    
+    % average data
+    [m_input_f_input_bin.(grp_nm),...
+        sem_input_f_input_bin.(grp_nm),...
+        sd_input_f_input_bin.(grp_nm)] = mean_sem_sd(input_f_input_bin.(grp_nm), 2);
+    [m_ROI_f_input_bin.(grp_nm),...
+        sem_ROI_f_input_bin.(grp_nm),...
+        sd_ROI_f_input_bin.(grp_nm)] = mean_sem_sd(ROI_f_input_bin.(grp_nm), 2);
+    [m_output_f_input_bin.(grp_nm),...
+        sem_output_f_input_bin.(grp_nm),...
+        sd_output_f_input_bin.(grp_nm)] = mean_sem_sd(output_f_input_bin.(grp_nm), 2);
+    [m_ROI_f_ROI_bin.(grp_nm),...
+        sem_ROI_f_ROI_bin.(grp_nm),...
+        sd_ROI_f_ROI_bin.(grp_nm)] = mean_sem_sd(ROI_f_ROI_bin.(grp_nm), 2);
+    [m_output_f_ROI_bin.(grp_nm),...
+        sem_output_f_ROI_bin.(grp_nm),...
+        sd_output_f_ROI_bin.(grp_nm)] = mean_sem_sd(output_f_ROI_bin.(grp_nm), 2);
 end % loop
 
-%% average data per group
+%% figure showing different bins
+pSize = 50;
+lWidth = 3;
+col.low = [51 153 255]./255;
+col.high = [0 204 102]./255;
 
-%% figure
+% start figure
+fig;
+
+% ROI = f(input)
+subplot(1,3,1);
+for iGrp = 1:n_grps
+    grp_nm = mSplit_grps{iGrp};
+    hdl.(grp_nm) = errorbar(m_input_f_input_bin,...
+        m_ROI_f_input_bin,...
+        sem_ROI_f_input_bin,...
+        'Color',col.(grp_nm));
+    hdl.(grp_nm).LineWidth = lWidth;
+end % metabolite level loop
+legend([hdl.low, hdl.high],...
+    {['low ',metabolite_nm],['high ',metabolite_nm]});
+legend('boxoff');
+xlabel(input_prm_nm);
+ylabel(ROI_short_nm);
+legend_size(pSize);
+
+% output = f(ROI)
+subplot(1,3,2);
+for iGrp = 1:n_grps
+    grp_nm = mSplit_grps{iGrp};
+    hdl.(grp_nm) = errorbar(m_ROI_f_ROI_bin,...
+        m_output_f_ROI_bin,...
+        sem_output_f_ROI_bin,...
+        'Color',col.(grp_nm));
+    hdl.(grp_nm).LineWidth = lWidth;
+end % metabolite level loop
+legend([hdl.low, hdl.high],...
+    {['low ',metabolite_nm],['high ',metabolite_nm]});
+legend('boxoff');
+xlabel(ROI_short_nm);
+ylabel(output_prm_nm);
+legend_size(pSize);
+
+% output = f(input)
+subplot(1,3,3);
+for iGrp = 1:n_grps
+    grp_nm = mSplit_grps{iGrp};
+    hdl.(grp_nm) = errorbar(m_output_f_input_bin,...
+        m_output_f_input_bin,...
+        sem_output_f_input_bin,...
+        'Color',col.(grp_nm));
+    hdl.(grp_nm).LineWidth = lWidth;
+end % metabolite level loop
+legend([hdl.low, hdl.high],...
+    {['low ',metabolite_nm],['high ',metabolite_nm]});
+legend('boxoff');
+xlabel(input_prm_nm);
+ylabel(output_prm_nm);
+legend_size(pSize);
