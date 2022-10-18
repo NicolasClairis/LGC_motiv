e1fc339ebb77e9d462e073bfa2c9a76cf172a080
NicolasClairis <nicolas.clairis@protonmail.com>
2022-10-18 17:49:21 +0200

    initiation

---
+++ LGC_Motiv_analysis/fMRI/ROI/effort_dmPFC_certainty_choices.m
@@ -0,0 +1,9 @@
+%% test whether there is an interaction between certainty, effort level and dmPFC activity
+
+%% load ROI activity
+
+%% load behavior
+
+%% decompose the activity
+
+%% display the result
