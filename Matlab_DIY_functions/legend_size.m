1fb8b0f47fb860075943fc4a2aca1931d2aed07d
NicolasClairis <nicolas.clairis@protonmail.com>
2022-10-26 13:21:03 +0200

    adding magic trick from arthur for matlab font

--- Matlab_DIY_functions/legend_size.m
+++ Matlab_DIY_functions/legend_size.m
@@ -6,8 +6,11 @@ function [  ] = legend_size( pSize )
 % pSize: size of the text you want to use
 % Originally written by N.Clairis - 31/01/2020
 
+% change font
 set(gca,'FontName','Calibri');
 set(gca,'fontsize',pSize,'FontWeight','normal');
 set(findall(gcf,'type','text'),'FontSize',pSize,'fontWeight','normal');
+% change matlab font from ugly grey to white
+set(0,'defaultfigurecolor',[1 1 1]);
 
 end % function
