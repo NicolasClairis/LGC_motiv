c567edf4eb3d48281264d4cdf1df647b079d4928
NicolasClairis <nicolas.clairis@protonmail.com>
2022-10-31 17:32:47 +0100

    new functions

---
+++ LGC_Motiv_analysis/fMRI/ROI/RT_f_ROI_NV_bins.m
@@ -0,0 +1,360 @@
+%% look whether within each input parameter bin, the activity of the selected ROI 
+% determines which choices should be made.
+%
+
+%% general parameters
+% display figures?
+dispFig = true;
+
+%% study by default
+if ~exist('study_nm','var') || isempty(study_nm)
+    study_nm = 'study1';
+end
+
+%% working directories
+computerRoot = LGCM_root_paths;
+studyBehaviorFolder = [computerRoot, filesep, study_nm, filesep];
+
+%% selection of participants
+condition = subject_condition;
+[subject_id, NS] = LGCM_subject_selection(study_nm, condition);
+
+%% extract ROI activity for all subjects
+[ROI_trial_b_trial] = extract_ROI_betas_onsets_only(computerRoot,...
+    study_nm, subject_id, condition);
+% define which ROI, and which time period is of interest to you
+% define ROI
+ROI_names = fieldnames(ROI_trial_b_trial);
+ROI_subList = ROI_trial_b_trial.subject_id;
+ROI_names(strcmp(ROI_names,'subject_id')) = [];
+if length(ROI_names) > 1
+    error(['There should be only 1 ROI selected, not ',num2str(length(ROI_names))])
+else
+    ROI_nm = ROI_names;
+end
+ROI_short_nm = inputdlg('ROI short name?');
+ROI_short_nm = ROI_short_nm{1};
+% define task
+task_names = {'Ep','Em','EpEmPool'};
+which_task = listdlg('PromptString','Which task?','ListString',task_names);
+task_to_look = task_names{which_task};
+% define time period
+timePeriods = fieldnames(ROI_trial_b_trial.(ROI_nm{1}).Ep.run1);
+which_timePeriod = listdlg('PromptString','Which time phase of the trial?',...
+    'listString',timePeriods);
+timePeriod_nm = timePeriods{which_timePeriod};
+
+%% select parameters of interest
+potential_input_prm = {'NV_hE','deltaNV','uncertainty','E_level','money_level',...
+    'deltaMoney_level'};
+which_input = listdlg('PromptString','please select input parameter',...
+    'ListString',potential_input_prm);
+input_prm_nm = potential_input_prm{which_input};
+
+%% number of bins
+switch input_prm_nm
+    case {'E_level','money_level','deltaMoney_level'}
+        nBins = 3;
+    case {'NV_hE','deltaNV','uncertainty'}
+        nBins = 6;
+    otherwise
+        nBins = 6;
+end
+
+%% select which model to use (if relevant)
+if ismember(input_prm_nm,{'NV_hE','deltaNV','uncertainty'})
+    needModeling = true;
+    [mdlType, mdlN] = behavioral_model_selection;
+else
+    needModeling = false;
+end
+
+%% extract the parameters of interest
+nRuns = 4;
+nTrialsPerRun = 54;
+nTrials = nTrialsPerRun*nRuns;
+% input/mediator/output
+[input_prm, ROI_mediator, RT] = deal(NaN(nTrials, NS));
+
+% bin variables
+[input_f_input_bin, ROI_f_input_bin, RT_f_input_bin,...
+    input_f_input_low_ROI, ROI_f_input_low_ROI, RT_f_input_low_ROI,...
+    input_f_input_high_ROI, ROI_f_input_high_ROI, RT_f_input_high_ROI] = deal(NaN(nBins, NS));
+
+%% loop through subjects
+for iS = 1:NS
+    sub_nm = subject_id{iS};
+    subBehaviorFolder = [studyBehaviorFolder, 'CID',sub_nm, filesep, 'behavior',filesep];
+
+    % extract confidence based on the model
+    if needModeling == true
+        switch mdlType
+            case 'simple'
+            [~, dataInferred] = logitfit_choices(computerRoot, study_nm, sub_nm,...
+                0, 'levels', 6, 6);
+        case 'bayesian'
+            error('please update for bayesian model');
+        end
+    end
+
+    % extract runs
+    [runsStruct] = runs_definition(study_nm, sub_nm, 'behavior');
+    okRuns = runsStruct.runsToKeep;
+    taskNames = runsStruct.tasks;
+    jRun = 0;
+    for iRun = 1:length(okRuns)
+        kRun = okRuns(iRun);
+        run_nm = num2str(kRun);
+        jRun = jRun + 1;
+        task_nm_tmp = taskNames{jRun};
+        runTrials_idx = (1:nTrialsPerRun) + nTrialsPerRun*(kRun-1);
+        switch task_nm_tmp
+            case 'Em'
+                task_fullName = 'mental';
+            case 'Ep'
+                task_fullName = 'physical';
+        end
+        % define which task session it is
+        switch kRun
+            case {1,2}
+                run_nm_bis = ['run',num2str(1)];
+            case {3,4}
+                run_nm_bis = ['run',num2str(2)];
+        end
+        
+        %% load the data
+        behaviorStruct_tmp = load([subBehaviorFolder,...
+            'CID',sub_nm,'_session',run_nm,'_',task_fullName,...
+            '_task.mat']);
+        choiceOptions_tmp = behaviorStruct_tmp.choice_opt;
+        switch task_nm_tmp
+            case 'Em'
+                choiceAndPerf_tmp = behaviorStruct_tmp.mentalE_perf;
+            case 'Ep'
+                choiceAndPerf_tmp = behaviorStruct_tmp.physicalPerf;
+        end
+        
+        %% default side
+        defaultSide_tmp = choiceOptions_tmp.default_LR;
+        %% extract R or P
+        RP_var_tmp = strcmp(choiceOptions_tmp.R_or_P,'R');
+        
+        %% effort level
+        E_highE_tmp = (choiceOptions_tmp.E.left).*(defaultSide_tmp == 1) +...
+            (choiceOptions_tmp.E.right).*(defaultSide_tmp == -1);
+        
+        %% high-effort money amount
+        money_hE_tmp = ((choiceOptions_tmp.monetary_amount.left).*(defaultSide_tmp == 1) +...
+            (choiceOptions_tmp.monetary_amount.right).*(defaultSide_tmp == -1)).*((RP_var_tmp == 1) - (RP_var_tmp == 0));
+        money_lE_tmp = ((choiceOptions_tmp.monetary_amount.left).*(defaultSide_tmp == -1) +...
+            (choiceOptions_tmp.monetary_amount.right).*(defaultSide_tmp == 1)).*((RP_var_tmp == 1) - (RP_var_tmp == 0));
+        
+        %% delta between high and low effort options
+        deltaMoney_tmp = money_hE_tmp - money_lE_tmp;
+        
+        %% RT
+        onsets_tmp = behaviorStruct_tmp.onsets;
+        switch task_nm_tmp
+            case 'Ep'
+                onsets_tmp = behaviorStruct_tmp.physicalPerf.onsets;
+                choice_LR_tmp = behaviorStruct_tmp.physicalPerf.choice;
+            case 'Em'
+                onsets_tmp = behaviorStruct_tmp.mentalE_perf.onsets;
+                choice_LR_tmp = behaviorStruct_tmp.mentalE_perf.choice;
+        end
+        RT_tmp = onsets_tmp.choice - onsets_tmp.dispChoiceOptions;
+        
+        %% confidence rating
+        uncertaintyRtg_tmp = NaN(1,length(choice_LR_tmp));
+        uncertaintyRtg_tmp(abs(choice_LR_tmp) == 2) = 0; % high confidence = low uncertainty
+        uncertaintyRtg_tmp(abs(choice_LR_tmp) == 1) = 1; % low confidence = high uncertainty
+        
+        %% net value and confidence inferred by the model
+        if needModeling == 1 
+            switch mdlType
+                case 'simple'
+                    NV_hE_tmp = dataInferred.NV_varOption.(task_nm_tmp).(['mdl_',mdlN]).(run_nm_bis);
+                    trial_idx = (1:nTrialsPerRun) + nTrialsPerRun*(kRun >= 3);
+                    deltaNV_tmp = dataInferred.deltaNV.(['mdl_',mdlN]).(task_nm_tmp)(trial_idx);
+                    uncertainty_tmp = - dataInferred.confidenceFitted.(['mdl_',mdlN]).(run_nm_bis); % revert sign to transform confidence into uncertainty
+                otherwise
+                    error('not ready yet');
+            end
+        end
+        
+        %% extract RT
+        
+        %% extract input behavioral variable
+        switch input_prm_nm
+            case 'uncertainty'
+                input_prm(runTrials_idx, iS) = uncertainty_tmp;
+            case 'E_level'
+                input_prm(runTrials_idx, iS) = E_highE_tmp;
+            case 'money_level'
+                input_prm(runTrials_idx, iS) = money_hE_tmp;
+            case 'deltaMoney_level'
+                input_prm(runTrials_idx, iS) = deltaMoney_tmp;
+            case 'deltaNV'
+                input_prm(runTrials_idx, iS) = deltaNV_tmp;
+            case 'NV_hE'
+                input_prm(runTrials_idx, iS) = NV_hE_tmp;
+            otherwise
+                error(['input = ',input_prm_nm,' not ready yet']);
+        end
+        
+        %% extract fMRI ROI mediator
+        if strcmp(task_to_look,'EpEmPool') ||...
+                (strcmp(task_to_look, task_nm))
+            ROI_mediator(runTrials_idx, iS) = ROI_trial_b_trial.(ROI_nm{1}).(task_nm_tmp).(run_nm_bis).(timePeriod_nm)(:, iS);
+        end
+        %% extract output behavioral variable
+        RT(runTrials_idx, iS) = RT_tmp;
+    end % run loop
+    
+
+    %% extract the bins
+    % 1) split the data according to input parameter bins
+    [ROI_f_input_bin(:,iS), input_f_input_bin(:,iS), bin_idx_tmp] = do_bin2(ROI_mediator(:, iS), input_prm(:,iS), nBins, 0);
+    RT_f_input_bin(:,iS) = do_bin2(RT(:, iS), input_prm(:,iS), nBins, 0);
+
+    % 2) perform a median split according to the ROI activity for each bin
+    for iBin = 1:nBins
+        curr_bin_idx_tmp = bin_idx_tmp == iBin;
+        med_tmp = median(ROI_mediator(curr_bin_idx_tmp, iS), 1,'omitnan');
+        % extract data for the current bin of interest
+        input_bin_tmp   = input_prm(curr_bin_idx_tmp, iS);
+        ROI_bin_tmp     = ROI_mediator(curr_bin_idx_tmp, iS);
+        RT_bin_tmp  = RT(curr_bin_idx_tmp, iS);
+        % extract data for low ROI activity
+        curr_bin_low_ROI_idx = ROI_bin_tmp < med_tmp;
+        input_f_input_low_ROI(iBin, iS) = mean(input_bin_tmp(curr_bin_low_ROI_idx),1,'omitnan');
+        ROI_f_input_low_ROI(iBin, iS) = mean(ROI_bin_tmp(curr_bin_low_ROI_idx),1,'omitnan');
+        RT_f_input_low_ROI(iBin, iS) =  mean(RT_bin_tmp(curr_bin_low_ROI_idx),1,'omitnan');
+        % extract data for high ROI activity
+        curr_bin_high_ROI_idx = ROI_bin_tmp > med_tmp;
+        input_f_input_high_ROI(iBin, iS) = mean(input_bin_tmp(curr_bin_high_ROI_idx),1,'omitnan');
+        ROI_f_input_high_ROI(iBin, iS) = mean(ROI_bin_tmp(curr_bin_high_ROI_idx),1,'omitnan');
+        RT_f_input_high_ROI(iBin, iS) =  mean(RT_bin_tmp(curr_bin_high_ROI_idx),1,'omitnan');
+    end
+    
+end % subject loop
+
+%% average data
+% basic data
+[m_input_f_input,...
+    sem_input_f_input] = mean_sem_sd(input_f_input_bin, 2);
+[m_ROI_f_input,...
+    sem_ROI_f_input] = mean_sem_sd(ROI_f_input_bin, 2);
+[m_RT_f_input,...
+    sem_RT_f_input] = mean_sem_sd(RT_f_input_bin, 2);
+% low ROI median split
+[m_input_f_input_low_ROI,...
+    sem_input_f_input_low_ROI] = mean_sem_sd(input_f_input_low_ROI, 2);
+[m_ROI_f_input_low_ROI,...
+    sem_ROI_f_input_low_ROI] = mean_sem_sd(ROI_f_input_low_ROI, 2);
+[m_RT_f_input_low_ROI,...
+    sem_RT_f_input_low_ROI] = mean_sem_sd(RT_f_input_low_ROI, 2);
+% high ROI median split
+[m_input_f_input_high_ROI,...
+    sem_input_f_input_high_ROI] = mean_sem_sd(input_f_input_high_ROI, 2);
+[m_ROI_f_input_high_ROI,...
+    sem_ROI_f_input_high_ROI] = mean_sem_sd(ROI_f_input_high_ROI, 2);
+[m_RT_f_input_high_ROI,...
+    sem_RT_f_input_high_ROI] = mean_sem_sd(RT_f_input_high_ROI, 2);
+
+%% figures
+if dispFig == true
+    lWidth = 3;
+    black = [0 0 0];
+    purple = [153 142 195]./255;
+    orange = [241 163 64]./255;
+    pSize = 50;
+    input_prm_nm = strrep(input_prm_nm,'_',' ');
+
+    % look at the general figure (choice = f(inputs), ROI=f(inputs)
+    fig;
+    % RT = f(inputs)
+    subplot(1,2,1);
+    hold on;
+    gal_data_hdl = errorbar(m_input_f_input,...
+        m_RT_f_input,...
+        sem_RT_f_input);
+    gal_data_hdl.LineStyle = 'none';
+    gal_data_hdl.LineWidth = lWidth;
+    gal_data_hdl.MarkerEdgeColor = black;
+    xlabel(input_prm_nm);
+    ylabel('RT (s)');
+    legend_size(pSize);
+
+    subplot(1,2,2);
+    % ROI = f(inputs)
+    hold on;
+    ROI_f_input_hdl = errorbar(m_input_f_input,...
+        m_ROI_f_input,...
+        sem_ROI_f_input);
+    ROI_f_input_hdl.LineStyle = 'none';
+    ROI_f_input_hdl.LineWidth = lWidth;
+    ROI_f_input_hdl.MarkerEdgeColor = black;
+    xlabel(input_prm_nm);
+    ylabel([ROI_short_nm,' BOLD']);
+    legend_size(pSize);
+
+    %% sanity check: ROI = f(inputs) + ROI median split (did it work?)
+    fig;
+    hold on;
+    ROI_f_input_low_ROI_hdl = errorbar(m_input_f_input_low_ROI,...
+        m_ROI_f_input_low_ROI,...
+        sem_ROI_f_input_low_ROI);
+    ROI_f_input_high_ROI_hdl = errorbar(m_input_f_input_high_ROI,...
+        m_ROI_f_input_high_ROI,...
+        sem_ROI_f_input_high_ROI);
+    ROI_f_input_low_ROI_hdl.LineStyle = 'none';
+    ROI_f_input_low_ROI_hdl.LineWidth = lWidth;
+    ROI_f_input_low_ROI_hdl.MarkerEdgeColor = purple;
+    ROI_f_input_high_ROI_hdl.LineStyle = 'none';
+    ROI_f_input_high_ROI_hdl.LineWidth = lWidth;
+    ROI_f_input_high_ROI_hdl.MarkerEdgeColor = orange;
+    xlabel(input_prm_nm);
+    ylabel([ROI_short_nm,' BOLD']);
+    legend([ROI_f_input_low_ROI_hdl, ROI_f_input_high_ROI_hdl],...
+        {['low ',ROI_short_nm],['high ',ROI_short_nm]});
+    legend('boxoff');
+    legend_size(pSize);
+
+    %% main figure: RT = f(inputs) + ROI median split
+    fig;
+    hold on;
+    RT_f_inputs_low_ROI_data_hdl = errorbar(m_input_f_input_low_ROI,...
+        m_RT_f_input_low_ROI,...
+        sem_RT_f_input_low_ROI);
+    RT_f_inputs_high_ROI_data_hdl = errorbar(m_input_f_input_high_ROI,...
+        m_RT_f_input_high_ROI,...
+        sem_RT_f_input_high_ROI);
+    RT_f_inputs_low_ROI_data_hdl.LineWidth = lWidth;
+    RT_f_inputs_low_ROI_data_hdl.MarkerEdgeColor = purple;
+    RT_f_inputs_high_ROI_data_hdl.LineWidth = lWidth;
+    RT_f_inputs_high_ROI_data_hdl.MarkerEdgeColor = orange;
+    xlabel(input_prm_nm);
+    ylabel('RT (s)');
+    legend([RT_f_inputs_low_ROI_data_hdl, RT_f_inputs_high_ROI_data_hdl],...
+        {['low ',ROI_short_nm],['high ',ROI_short_nm]});
+    legend('boxoff');
+    legend_size(pSize);
+
+    
+end
+
+%% compare slopes
+
+[slope_low, slope_high,...
+    intercept_low, intercept_high] = deal(NaN(1,NS));
+for iS = 1:NS
+    b1 = glmfit(1:nBins, RT_f_input_low_ROI(:,iS),'normal');
+    intercept_low(iS) = b1(1);
+    slope_low(iS) = b1(2);
+    b2 = glmfit(1:nBins, RT_f_input_high_ROI(:,iS),'normal');
+    intercept_high(iS) = b2(1);
+    slope_high(iS) = b2(2);
+end
+[~,pval.intercept]=ttest(intercept_low,intercept_high);
+[~,pval.slope]=ttest(slope_low,slope_high);
---
+++ LGC_Motiv_analysis/nutrition/questionnaire_f_niacin_Trp.m
@@ -0,0 +1,229 @@
+
+%% test correlation between some questionnaires and nutrition components
+% extracted through the FFQ questionnaire
+
+%% by default display the figures but can be set to 0 if you only want the p.value
+figDisp = 1;
+
+%% working directories
+root = LGCM_root_paths;
+% study
+study_nm = 'study1';
+studyPath = [root, filesep, study_nm, filesep];
+% nutrition data
+switch root
+    case ['E:',filesep]
+        gitPath = fullfile('C:','Users','clairis','Desktop');
+    case {[fullfile('C:','Users','Loco','Downloads'),filesep],...
+            [fullfile('L:','human_data_private','raw_data_subject'),filesep]}
+        gitPath = fullfile('C:','Users','Loco','Downloads');
+    otherwise
+        error('case not ready yet');
+end
+nutritionPath = [fullfile(gitPath,'GitHub','LGC_motiv',...
+    'LGC_Motiv_results','nutrition'),filesep];
+
+%% define subjects
+study_nm = 'study1';
+condition = subject_condition;
+[subject_id, NS] = LGCM_subject_selection(study_nm, condition);
+
+%% load nutrition
+[nutri.niacin, nutri.Trp, nutri.niacinPlusTrp,...
+    nutri.calories,...
+    nutri.niacin_div_totalCal,  nutri.Trp_div_totalCal,...
+    nutri.niacinPlusTrp_div_totalCal] = deal(NaN(1,NS));
+nutri_vars = fieldnames(nutri);
+% extract all calories data
+calories_table = readtable([nutritionPath, 'calories_scoring.xlsx'],...
+    'Sheet','Sheet1');
+% extract all niacin data
+niacineFilePath = [nutritionPath,'niacine_scoring.xlsx'];
+niacin_table = readtable(niacineFilePath,...
+    'Sheet','Sheet1');
+% extract all Tryptophane data
+TrpFilePath = [nutritionPath,'Tryptophan_scoring.xlsx'];
+Trp_table = readtable(TrpFilePath,...
+    'Sheet','Sheet1');
+
+%% load questionnaire
+potentialQuestionnaires = {'JPI_RScore', 'MADRS_SCorrected',...
+    'MPSTEFSPhysicalTraitScore','MPSTEFSMentalTraitScore',...
+    'PunishmentScore','RewardScore',...
+    'IPAQ','IPAQInactivity'};
+selectedCon = listdlg('PromptString','Which questionnaires?',...
+    'ListString',potentialQuestionnaires);
+nQuest = length(selectedCon);
+questToCheck = potentialQuestionnaires(selectedCon);
+[excelReadQuestionnairesFile, sub_CID_list] = load_questionnaires_data();
+for iQuest = 1:nQuest
+    quest_nm = questToCheck{iQuest};
+    quest_data.(quest_nm) = NaN(1,NS);
+end
+
+%% loop through subjects
+for iS = 1:NS
+    sub_nm = subject_id{iS};
+    
+    %% load questionnaire
+    sub_quest_idx = strcmp(sub_CID_list, sub_nm);
+    for iQuest = 1:nQuest
+        quest_nm = questToCheck{iQuest};
+        quest_data.(quest_nm)(iS) = excelReadQuestionnairesFile.(quest_nm)(sub_quest_idx);
+    end % questionnaire loop
+    
+    %% load nutrition score
+    sub_niacin_idx = find(strcmp(niacin_table.CID, sub_nm));
+    sub_Trp_idx = find(strcmp(Trp_table.CID, sub_nm));
+    sub_calories_idx = find(strcmp(calories_table.CID, sub_nm));
+    % extract nutrition intake values
+    % extract niacin values
+    if ~isempty(sub_niacin_idx) && size(sub_niacin_idx,1) == 1
+        nutri.niacin(iS) = niacin_table.NiacineParSemaine_ug_(sub_niacin_idx);
+    end
+    % extract Tryptophan values
+    if ~isempty(sub_Trp_idx) && size(sub_Trp_idx,1) == 1
+        nutri.Trp(iS) = Trp_table.TryptophaneParSemaine_ug_(sub_Trp_idx);
+    end
+    % extract calories values
+    if ~isempty(sub_calories_idx) && size(sub_calories_idx,1) == 1
+        nutri.calories(iS) = calories_table.CaloriesParSemaine_kcal_(sub_calories_idx);
+    end
+    nutri.niacinPlusTrp(iS) = nutri.niacin(iS) + nutri.Trp(iS);
+    
+    % divide by calories
+    if ~isnan(nutri.calories(iS))
+        nutri.niacin_div_totalCal(iS) = nutri.niacin(iS)/nutri.calories(iS);
+        nutri.Trp_div_totalCal(iS) = nutri.Trp(iS)/nutri.calories(iS);
+        nutri.niacinPlusTrp_div_totalCal(iS) = nutri.niacinPlusTrp(iS)/nutri.calories(iS);
+    end
+end % subject loop
+
+%% test correlations
+for iQuest = 1:nQuest
+    quest_nm = questToCheck{iQuest};
+    for iNutri = 1:length(nutri_vars)
+        nutri_nm = nutri_vars{iNutri};
+        % filter bad subjects
+        subs_ok = (~isnan(nutri.(nutri_nm)).*~isnan(quest_data.(quest_nm))) == 1;
+        if sum(subs_ok) > 0
+            % perform glm brain GSH = b0 + b1*nutrition variable
+            [betas.(quest_nm).(['f_',nutri_nm]),~,stats_tmp] = glmfit(nutri.(nutri_nm)(subs_ok), quest_data.(quest_nm)(subs_ok),'normal');
+            % extract p.value
+            pval.(quest_nm).(['f_',nutri_nm]) = stats_tmp.p;
+            % extract the fit
+            nutri_bis.(quest_nm).(nutri_nm) = sort(nutri.(nutri_nm)(subs_ok));
+            fittedData.(quest_nm).(['f_',nutri_nm]) = glmval(betas.(quest_nm).(['f_',nutri_nm]), nutri_bis.(quest_nm).(nutri_nm), 'identity');
+        end
+    end % loop through nutritional intake variables
+end % loop through ROIs
+
+%% figure
+if figDisp == 1
+    
+    lWidth = 3;
+    pSize = 25;
+    rawDataCol = 'k';
+    fitCol = [255 143 143]./255;
+    %% show results
+    for iQuest = 1:nQuest
+        quest_nm = questToCheck{iQuest};
+        % define y.label for questionnaires
+        switch quest_nm
+            case 'JPI_RScore'
+                quest_nm_bis = 'JPI-R';
+            case 'MADRS_SCorrected'
+                quest_nm_bis = 'MADRS-S';
+            case 'MPSTEFSPhysicalTraitScore'
+                quest_nm_bis = 'MPSTEFS physical';
+            case 'MPSTEFSMentalTraitScore'
+                quest_nm_bis = 'MPSTEFS mental';
+            case 'PunishmentScore'
+                quest_nm_bis = 'PANAS P';
+            case 'RewardScore'
+                quest_nm_bis = 'PANAS R';
+            case 'IPAQ'
+                quest_nm_bis = 'IPAQ activity';
+            case 'IPAQInactivity'
+                quest_nm_bis = 'IPAQ inactivity';
+        end
+        
+        % initiliaze figures
+        fig1 = fig;
+        fig2 = fig;
+        for iNutri = 1:3
+            switch iNutri
+                case 1
+                    nutri_nm = 'niacin';
+                    nutri_nm_bis = 'niacin_div_totalCal';
+                case 2
+                    nutri_nm = 'Trp';
+                    nutri_nm_bis = 'Trp_div_totalCal';
+                case 3
+                    nutri_nm = 'niacinPlusTrp';
+                    nutri_nm_bis = 'niacinPlusTrp_div_totalCal';
+            end
+            
+            %% raw metabolite
+            figure(fig1);
+            subplot(1,3,iNutri);
+            hold on;
+            % show raw data
+            curve_hdl = scatter(nutri.(nutri_nm), quest_data.(quest_nm));
+            curve_hdl.LineWidth = lWidth;
+            curve_hdl.MarkerEdgeColor = rawDataCol;
+            % add the fit
+            curve_fit_hdl = plot(nutri_bis.(quest_nm).(nutri_nm),...
+                fittedData.(quest_nm).(['f_',(nutri_nm)]));
+            curve_fit_hdl.LineStyle = '--';
+            curve_fit_hdl.LineWidth = lWidth;
+            curve_fit_hdl.Color = fitCol;
+            switch nutri_nm
+                case {'niacin','Trp'}
+                    xlabel([nutri_nm,' (μg/week)']);
+                case 'niacinPlusTrp'
+                    xlabel('niacin + Trp (μg/week)');
+            end
+            ylabel(quest_nm_bis);
+            legend_size(pSize);
+            
+            %% Nutrition/total calories
+            figure(fig2);
+            subplot(1,3,iNutri);
+            hold on;
+            % show data normalized
+            curve_hdl = scatter(nutri.(nutri_nm_bis), quest_data.(quest_nm));
+            curve_hdl.LineWidth = lWidth;
+            curve_hdl.MarkerEdgeColor = rawDataCol;
+            % add the fit
+            curve_fit_hdl = plot(nutri_bis.(quest_nm).(nutri_nm_bis),...
+                fittedData.(quest_nm).(['f_',(nutri_nm_bis)]));
+            curve_fit_hdl.LineStyle = '--';
+            curve_fit_hdl.LineWidth = lWidth;
+            curve_fit_hdl.Color = fitCol;
+            if ismember(nutri_nm,{'niacin','Trp'})
+                xlabel([nutri_nm,'/calories']);
+            elseif strcmp(nutri_nm,'niacinPlusTrp')
+                xlabel('(niacin + Trp)/calories');
+            end
+            ylabel(quest_nm_bis);
+            legend_size(pSize);
+        end % nutrition metabolite
+        
+        %% add plot with calories only
+        fig3 = fig;
+        % show raw data
+        curve_hdl = scatter(nutri.calories, quest_data.(quest_nm));
+        curve_hdl.LineWidth = lWidth;
+        curve_hdl.MarkerEdgeColor = rawDataCol;
+        % add the fit
+        curve_fit_hdl = plot(nutri_bis.(quest_nm).calories,...
+            fittedData.(quest_nm).f_calories);
+        curve_fit_hdl.LineStyle = '--';
+        curve_fit_hdl.LineWidth = lWidth;
+        curve_fit_hdl.Color = fitCol;
+        xlabel('calories (kcal/week)');
+        ylabel(quest_nm_bis);
+        legend_size(pSize);
+    end % questionnaire loop
+end % figure display
