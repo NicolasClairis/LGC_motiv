a8b15cdfe72911322c0ced70ffe881ff3a0a3a3b
NicolasClairis <nicolas.clairis@protonmail.com>
2022-10-31 17:33:08 +0100

    useless removed

--- LGC_Motiv_analysis/fMRI/ROI/effort_dmPFC_certainty_choices.m
+++
@@ -1,9 +0,0 @@
-%% test whether there is an interaction between certainty, effort level and dmPFC activity
-
-%% load ROI activity
-
-%% load behavior
-
-%% decompose the activity
-
-%% display the result
