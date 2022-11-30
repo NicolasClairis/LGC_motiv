43aa5c13e72fcf5f3e6870d84a6ee1f65a330b43
NicolasClairis <nicolas.clairis@protonmail.com>
2022-11-30 16:36:45 +0100

    update

--- LGC_Motiv_analysis/fMRI/delete_first_level_folders.m
+++ LGC_Motiv_analysis/fMRI/delete_first_level_folders.m
@@ -5,28 +5,33 @@ function[] = delete_first_level_folders(study_nm)
 
 baseline_folder = pwd;
 
-sm_kernel = input(sprintf('What smoothing kernel do you want to delete (4/6/8)? \n'));
+kernels = {'8','6','4'};
+sm_kernel_idx = listdlg('PromptString','What smoothing kernel do you want to delete?',...
+    'ListString',kernels);
+sm_kernel = str2double(kernels{sm_kernel_idx});
 
-GLM = input(sprintf('What First level GLM do you want to delete? \n'));
+%% GLM choice
+GLM_str = inputdlg('What First level GLM do you want to delete?');
+GLM = str2double(GLM_str);
 
 if ~exist('study_nm','var') || isempty(study_nm)
-    study_names = {'fMRI_pilots','study1','study2'};
+    study_names = {'study1','study2','fMRI_pilots'};
     study_nm_idx = listdlg('ListString',study_names);
     study_nm = study_names{study_nm_idx};
 end
 
 
 % check if sure to delete before doing so
-del_check = input(sprintf(['Are you sure you want to delete the first level folders of GLM ',num2str(GLM),' for',study_nm,'?',...
-    '\n If yes, press 1. If no, press whatever else. \n']));
-if del_check ~= 1
+del_check = listdlg('PromptString',...
+    ['Are you sure you want to delete the first level folders of GLM ',num2str(GLM),' for',study_nm,'?'],...
+    'ListString',{'yes','no'},'SelectionMode','single');
+if del_check == 2
     disp('You didn''t want to delete these files finally huh?');
     return;
 end
 
-condition = 'fMRI';
-warning('should add a filter here to select fMRI/fMRI_noMove/etc.');
-
+%% define subject list
+condition = subject_condition;
 [subject_id, NS] = LGCM_subject_selection(study_nm, condition);
 
 %% working directories
@@ -50,14 +55,12 @@ for iS = 1:NS
         'functional',filesep,'preproc_sm_',num2str(sm_kernel),'mm', filesep];
     cd(subj_analysis_folder);
     
-    run_folders = {['GLM',num2str(GLM)]};
-    for iRun = 1:length(run_folders)
-        if exist([subj_analysis_folder, run_folders{iRun}],'dir')
-            rmdir(run_folders{iRun},'s');
-            disp([run_folders{iRun}, ' correctly removed for ',subject_id{iS}]);
-        else
-            disp([run_folders{iRun}, ' not found for ',subject_id{iS}]);
-        end
+    [resultsFolderName, resultsFolderShortName] = fMRI_subFolder(subj_analysis_folder, GLM, condition);
+    if exist(resultsFolderName,'dir')
+        rmdir(resultsFolderShortName,'s');
+        disp([resultsFolderShortName, ' correctly removed for ',subject_id{iS}]);
+    else
+        disp([resultsFolderShortName, ' not found for ',subject_id{iS}]);
     end
 end
 
