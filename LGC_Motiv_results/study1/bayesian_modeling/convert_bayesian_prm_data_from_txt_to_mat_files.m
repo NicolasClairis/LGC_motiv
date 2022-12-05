f2449bceef644799a7e05acda119a68dc860d49d
NicolasClairis <nicolas.clairis@protonmail.com>
2022-12-05 10:28:19 +0100

    updating with last bayesian model data

--- LGC_Motiv_results/study1/bayesian_modeling/convert_bayesian_prm_data_from_txt_to_mat_files.m
+++ LGC_Motiv_results/study1/bayesian_modeling/convert_bayesian_prm_data_from_txt_to_mat_files.m
@@ -8,7 +8,7 @@ bayesian_mdl.mdl1.kEp = model_raw.mdl1(3,:);
 bayesian_mdl.mdl1.kEm = model_raw.mdl1(4,:);
 bayesian_mdl.mdl1.kBiasM = model_raw.mdl1(5,:);
 bayesian_mdl.mdl1.kFp = model_raw.mdl1(6,:);
-bayesian_mdl.mdl1.kFm = model_raw.mdl1(7,:);
+bayesian_mdl.mdl1.kLm = model_raw.mdl1(7,:);
 lSub = 8;
 % model 2 (no bias)
 bayesian_mdl.mdl2.kR = model_raw.mdl2(1,:);
@@ -16,18 +16,18 @@ bayesian_mdl.mdl2.kP = model_raw.mdl2(2,:);
 bayesian_mdl.mdl2.kEp = model_raw.mdl2(3,:);
 bayesian_mdl.mdl2.kEm = model_raw.mdl2(4,:);
 bayesian_mdl.mdl2.kFp = model_raw.mdl2(5,:);
-bayesian_mdl.mdl2.kFm = model_raw.mdl2(6,:);
+bayesian_mdl.mdl2.kLm = model_raw.mdl2(6,:);
 lSub = 7;
 
 % model 3 (bias)
-bayesian_mdl.mdl3.kR = model_raw.mdl3(1,:);
-bayesian_mdl.mdl3.kP = model_raw.mdl3(2,:);
-bayesian_mdl.mdl3.kEp = model_raw.mdl3(3,:);
-bayesian_mdl.mdl3.kEm = model_raw.mdl3(4,:);
-bayesian_mdl.mdl3.kBiasM = model_raw.mdl3(5,:);
-bayesian_mdl.mdl3.kFp = model_raw.mdl3(6,:);
-bayesian_mdl.mdl3.kFm = model_raw.mdl3(7,:);
-lSub = 8;
+bayesian_mdl.mdl3.kR = model_raw.mdl3(2,:);
+bayesian_mdl.mdl3.kP = model_raw.mdl3(3,:);
+bayesian_mdl.mdl3.kEp = model_raw.mdl3(4,:);
+bayesian_mdl.mdl3.kEm = model_raw.mdl3(5,:);
+bayesian_mdl.mdl3.kBiasM = model_raw.mdl3(6,:);
+bayesian_mdl.mdl3.kFp = model_raw.mdl3(7,:);
+bayesian_mdl.mdl3.kLm = model_raw.mdl3(8,:);
+lSub = 1;
 
 lSub = input('Which line for subject_id?');
 % define the model data
