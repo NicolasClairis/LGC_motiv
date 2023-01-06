4785f2a1dd0ca5c4ea2022f2e08bea340917c430
NicolasClairis <nicolas.clairis@protonmail.com>
2023-01-06 15:51:27 +0100

    major fix sign Effort

--- LGC_Motiv_analysis/fMRI/First_level_loadRegressors.m
+++ LGC_Motiv_analysis/fMRI/First_level_loadRegressors.m
@@ -187,8 +187,8 @@ money_level_fixedOption = money_level_left.*(defaultSide == -1) +...
     money_level_right.*(defaultSide == 1);
 abs_money_level_varOption = abs(money_level_varOption);
 % loading effort choice
-E_left = behavioralDataStruct.(task_behavioral_id).choiceOptions.E.left.*RP_var;
-E_right = behavioralDataStruct.(task_behavioral_id).choiceOptions.E.right.*RP_var;
+E_left = behavioralDataStruct.(task_behavioral_id).choiceOptions.E.left;
+E_right = behavioralDataStruct.(task_behavioral_id).choiceOptions.E.right;
 E_sum = E_left + E_right;
 E_varOption = E_left.*(defaultSide == 1) + E_right.*(defaultSide == -1);
 E_fixedOption = E_left.*(defaultSide == -1) + E_right.*(defaultSide == 1);
