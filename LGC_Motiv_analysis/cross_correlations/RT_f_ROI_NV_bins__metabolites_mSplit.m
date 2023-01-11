be1ea38343a4613717a9a4c3cdfd084b58f8c93c
NicolasClairis <nicolas.clairis@protonmail.com>
2023-01-11 11:00:52 +0100

    update + new function
    - adding RT_f_ROI_NV_bins median split per metabolite level
    - adding mention of task when appropriate

--- LGC_Motiv_analysis/cross_correlations/RT_f_ROI_NV_bins__metabolites_mSplit.m
+++ LGC_Motiv_analysis/cross_correlations/RT_f_ROI_NV_bins__metabolites_mSplit.m
@@ -1,7 +1,5 @@
-%% gets data from choice_f_ROI_NV_bins.m and then performs median split based on the metabolites.
-
-%% extract choice in function of ROI and input parameter
-choice_f_ROI_NV_bins;
+%% extract RT in function of ROI and input parameter
+RT_f_ROI_NV_bins;
 
 %% load group indexes depending on the level of metabolites
 [met_subs.low, met_subs.high, metabolite_nm, ROI_nm] = medSplit_metabolites(study_nm, subject_id);
@@ -17,22 +15,22 @@ for iGrp = 1:n_met_grps
         sem_input_f_input.(grp_nm_bis)] = mean_sem_sd(input_f_input_bin(:,met_subs.(grp_nm)), 2);
     [m_ROI_f_input.(grp_nm_bis),...
         sem_ROI_f_input.(grp_nm_bis)] = mean_sem_sd(ROI_f_input_bin(:,met_subs.(grp_nm)), 2);
-    [m_choice_f_input.(grp_nm_bis),...
-        sem_choice_f_input.(grp_nm_bis)] = mean_sem_sd(choice_f_input_bin(:,met_subs.(grp_nm)), 2);
+    [m_RT_f_input.(grp_nm_bis),...
+        sem_RT_f_input.(grp_nm_bis)] = mean_sem_sd(RT_f_input_bin(:,met_subs.(grp_nm)), 2);
     % low ROI median split
     [m_input_f_input_low_ROI.(grp_nm_bis),...
         sem_input_f_input_low_ROI.(grp_nm_bis)] = mean_sem_sd(input_f_input_low_ROI(:,met_subs.(grp_nm)), 2);
     [m_ROI_f_input_low_ROI.(grp_nm_bis),...
         sem_ROI_f_input_low_ROI.(grp_nm_bis)] = mean_sem_sd(ROI_f_input_low_ROI(:,met_subs.(grp_nm)), 2);
-    [m_choice_f_input_low_ROI.(grp_nm_bis),...
-        sem_choice_f_input_low_ROI.(grp_nm_bis)] = mean_sem_sd(choice_f_input_low_ROI(:,met_subs.(grp_nm)), 2);
+    [m_RT_f_input_low_ROI.(grp_nm_bis),...
+        sem_RT_f_input_low_ROI.(grp_nm_bis)] = mean_sem_sd(RT_f_input_low_ROI(:,met_subs.(grp_nm)), 2);
     % high ROI median split
     [m_input_f_input_high_ROI.(grp_nm_bis),...
         sem_input_f_input_high_ROI.(grp_nm_bis)] = mean_sem_sd(input_f_input_high_ROI(:,met_subs.(grp_nm)), 2);
     [m_ROI_f_input_high_ROI.(grp_nm_bis),...
         sem_ROI_f_input_high_ROI.(grp_nm_bis)] = mean_sem_sd(ROI_f_input_high_ROI(:,met_subs.(grp_nm)), 2);
-    [m_choice_f_input_high_ROI.(grp_nm_bis),...
-        sem_choice_f_input_high_ROI.(grp_nm_bis)] = mean_sem_sd(choice_f_input_high_ROI(:,met_subs.(grp_nm)), 2);
+    [m_RT_f_input_high_ROI.(grp_nm_bis),...
+        sem_RT_f_input_high_ROI.(grp_nm_bis)] = mean_sem_sd(RT_f_input_high_ROI(:,met_subs.(grp_nm)), 2);
 end % loop median split
 
 %% figures
@@ -49,17 +47,17 @@ if dispFig == true
     low_met_nm_bis = ['low ',metabolite_nm];
     high_met_nm_bis = ['high ',metabolite_nm];
     
-    % look at the general figure (choice = f(inputs), ROI=f(inputs)
+    % look at the general figure (RT = f(inputs), ROI=f(inputs)
     fig;
-    % choices = f(inputs)
+    % RT = f(inputs)
     subplot(1,2,1);
     hold on;
     gal_data_hdl_low_met = errorbar(m_input_f_input.(low_met_nm),...
-        m_choice_f_input.(low_met_nm),...
-        sem_choice_f_input.(low_met_nm));
+        m_RT_f_input.(low_met_nm),...
+        sem_RT_f_input.(low_met_nm));
     gal_data_hdl_high_met = errorbar(m_input_f_input.(high_met_nm),...
-        m_choice_f_input.(high_met_nm),...
-        sem_choice_f_input.(high_met_nm));
+        m_RT_f_input.(high_met_nm),...
+        sem_RT_f_input.(high_met_nm));
     gal_data_hdl_low_met.LineStyle = '--';
     gal_data_hdl_low_met.LineWidth = lWidth;
     gal_data_hdl_low_met.MarkerEdgeColor = grey;
@@ -70,8 +68,7 @@ if dispFig == true
         {high_met_nm_bis, low_met_nm_bis});
     legend('boxoff');
     xlabel({input_prm_nm; full_bhv_taskName});
-    ylabel({'Choice = high effort (%)';...
-        full_bhv_taskName});
+    ylabel({'RT (s)'; full_bhv_taskName});
     legend_size(pSize);
 
     subplot(1,2,2);
@@ -136,48 +133,45 @@ if dispFig == true
     legend('boxoff');
     legend_size(pSize);
 
-    %% main figure: choices = f(inputs) + ROI median split
+    %% main figure: RT = f(inputs) + ROI median split
     fig;
     hold on;
-    choices_f_inputs_low_ROI_data_low_met_hdl = errorbar(m_input_f_input_low_ROI.(low_met_nm),...
-        m_choice_f_input_low_ROI.(low_met_nm),...
-        sem_choice_f_input_low_ROI.(low_met_nm));
-    choices_f_inputs_high_ROI_data_low_met_hdl = errorbar(m_input_f_input_high_ROI.(low_met_nm),...
-        m_choice_f_input_high_ROI.(low_met_nm),...
-        sem_choice_f_input_high_ROI.(low_met_nm));
-    choices_f_inputs_low_ROI_data_high_met_hdl = errorbar(m_input_f_input_low_ROI.(high_met_nm),...
-        m_choice_f_input_low_ROI.(high_met_nm),...
-        sem_choice_f_input_low_ROI.(high_met_nm));
-    choices_f_inputs_high_ROI_data_high_met_hdl = errorbar(m_input_f_input_high_ROI.(high_met_nm),...
-        m_choice_f_input_high_ROI.(high_met_nm),...
-        sem_choice_f_input_high_ROI.(high_met_nm));
-    choices_f_inputs_low_ROI_data_low_met_hdl.LineStyle = '--';
-    choices_f_inputs_low_ROI_data_low_met_hdl.LineWidth = lWidth;
-    choices_f_inputs_low_ROI_data_low_met_hdl.Color = purple;
-    choices_f_inputs_high_ROI_data_low_met_hdl.LineStyle = '--';
-    choices_f_inputs_high_ROI_data_low_met_hdl.LineWidth = lWidth;
-    choices_f_inputs_high_ROI_data_low_met_hdl.Color = orange;
-    choices_f_inputs_low_ROI_data_high_met_hdl.LineStyle = '-';
-    choices_f_inputs_low_ROI_data_high_met_hdl.LineWidth = lWidth;
-    choices_f_inputs_low_ROI_data_high_met_hdl.Color = purple;
-    choices_f_inputs_high_ROI_data_high_met_hdl.LineStyle = '-';
-    choices_f_inputs_high_ROI_data_high_met_hdl.LineWidth = lWidth;
-    choices_f_inputs_high_ROI_data_high_met_hdl.Color = orange;
-    legend([choices_f_inputs_low_ROI_data_low_met_hdl,...
-        choices_f_inputs_high_ROI_data_low_met_hdl,...
-        choices_f_inputs_low_ROI_data_high_met_hdl,...
-        choices_f_inputs_high_ROI_data_high_met_hdl],...
+    RT_f_inputs_low_ROI_data_low_met_hdl = errorbar(m_input_f_input_low_ROI.(low_met_nm),...
+        m_RT_f_input_low_ROI.(low_met_nm),...
+        sem_RT_f_input_low_ROI.(low_met_nm));
+    RT_f_inputs_high_ROI_data_low_met_hdl = errorbar(m_input_f_input_high_ROI.(low_met_nm),...
+        m_RT_f_input_high_ROI.(low_met_nm),...
+        sem_RT_f_input_high_ROI.(low_met_nm));
+    RT_f_inputs_low_ROI_data_high_met_hdl = errorbar(m_input_f_input_low_ROI.(high_met_nm),...
+        m_RT_f_input_low_ROI.(high_met_nm),...
+        sem_RT_f_input_low_ROI.(high_met_nm));
+    RT_f_inputs_high_ROI_data_high_met_hdl = errorbar(m_input_f_input_high_ROI.(high_met_nm),...
+        m_RT_f_input_high_ROI.(high_met_nm),...
+        sem_RT_f_input_high_ROI.(high_met_nm));
+    RT_f_inputs_low_ROI_data_low_met_hdl.LineStyle = '--';
+    RT_f_inputs_low_ROI_data_low_met_hdl.LineWidth = lWidth;
+    RT_f_inputs_low_ROI_data_low_met_hdl.Color = purple;
+    RT_f_inputs_high_ROI_data_low_met_hdl.LineStyle = '--';
+    RT_f_inputs_high_ROI_data_low_met_hdl.LineWidth = lWidth;
+    RT_f_inputs_high_ROI_data_low_met_hdl.Color = orange;
+    RT_f_inputs_low_ROI_data_high_met_hdl.LineStyle = '-';
+    RT_f_inputs_low_ROI_data_high_met_hdl.LineWidth = lWidth;
+    RT_f_inputs_low_ROI_data_high_met_hdl.Color = purple;
+    RT_f_inputs_high_ROI_data_high_met_hdl.LineStyle = '-';
+    RT_f_inputs_high_ROI_data_high_met_hdl.LineWidth = lWidth;
+    RT_f_inputs_high_ROI_data_high_met_hdl.Color = orange;
+    legend([RT_f_inputs_low_ROI_data_low_met_hdl,...
+        RT_f_inputs_high_ROI_data_low_met_hdl,...
+        RT_f_inputs_low_ROI_data_high_met_hdl,...
+        RT_f_inputs_high_ROI_data_high_met_hdl],...
         {['low ',ROI_short_nm,' - low ',metabolite_nm],...
         ['high ',ROI_short_nm,' - low ',metabolite_nm],...
         ['low ',ROI_short_nm,' - high ',metabolite_nm],...
         ['high ',ROI_short_nm,' - high ',metabolite_nm]});
     legend('boxoff');
     xlabel({input_prm_nm; full_bhv_taskName});
-    ylabel({'Choice = high effort (%)';...
-        full_bhv_taskName});
+    ylabel({'RT (s)'; full_bhv_taskName});
     legend_size(pSize);
-
-    
 end
 
 %% compare slopes: should do an ANOVA or GLM with dmPFC and GSH as factors and look at the interaction(s)
@@ -185,10 +179,10 @@ end
 % [slope_low, slope_high,...
 %     intercept_low, intercept_high] = deal(NaN(1,NS));
 % for iS = 1:NS
-%     b1 = glmfit(1:nBins, choice_f_input_low_ROI(:,iS),'normal');
+%     b1 = glmfit(1:nBins, RT_f_input_low_ROI(:,iS),'normal');
 %     intercept_low(iS) = b1(1);
 %     slope_low(iS) = b1(2);
-%     b2 = glmfit(1:nBins, choice_f_input_high_ROI(:,iS),'normal');
+%     b2 = glmfit(1:nBins, RT_f_input_high_ROI(:,iS),'normal');
 %     intercept_high(iS) = b2(1);
 %     slope_high(iS) = b2(2);
 % end
