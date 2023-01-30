47b174720d8137a33e79d9378c72552fa62ec6d5
NicolasClairis <nicolas.clairis@protonmail.com>
2023-01-30 17:55:11 +0100

    Create extract_RP.m

---
+++ LGC_Motiv_analysis/behavior/extract_RP.m
@@ -0,0 +1,25 @@
+function[RP_trial] = extract_RP(subBehaviorFolder, sub_nm, run_nm, task_fullName)
+% [RP_trial] = extract_money_chosen(subBehaviorFolder, sub_nm, run_nm, task_fullName)
+%
+% INPUTS
+% subBehaviorFolder: folder where data is stored
+%
+% sub_nm: string with subject CID name
+%
+% run_nm: string with run name
+%
+% task_fullName: task full name 'mental'/'physical'
+%
+% OUTPUTS
+% RP_trial: simple vector indicating whether trial was Reward (+1) or
+% Punishment (-1)
+
+%% load data
+behaviorStruct = load([subBehaviorFolder,...
+    'CID',sub_nm,'_session',run_nm,'_',task_fullName,...
+    '_task.mat']);
+choiceOptions = behaviorStruct.choice_opt;
+%% extract R/P
+RP_trial = strcmp(choiceOptions.R_or_P,'R') - strcmp(choiceOptions.R_or_P,'P');
+
+end % function
