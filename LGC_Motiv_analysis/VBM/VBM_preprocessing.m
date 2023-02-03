3e63736a7dd5f6a3971d9d66810ca2d4025d2176
NicolasClairis <nicolas.clairis@protonmail.com>
2023-02-03 10:18:41 +0100

    starting scripts for VBM preprocessing

---
+++ LGC_Motiv_analysis/VBM/VBM_preprocessing.m
@@ -0,0 +1,74 @@
+
+
+%% iniate spm
+spm('defaults','fmri');
+spm_jobman('initcfg');
+
+%% checking = 0 if you want to run directly or = 1 if you want to look at the batch first
+checking = 1;
+
+%% define working directory
+
+unil_TPM_priorsPath = 'D:\Matlab extensions\spm12\tpm\enhanced_TPM.nii';
+%% subject selection
+
+%% initialize size of matlabbatch
+nBatchPerSub = 3;
+matlabbatch = cell(1,NS*nBatchPerSub);
+nativeOnly = [0 0];
+nativeAndDartel = [1 1];
+
+%% loop through subjects
+for iS = 1:NS
+    %% segmentation
+    jSeg = 1 + nBatchPerSub*(iS - 1);
+    matlabbatch{jSeg}.spm.spatial.preproc.channel.vols = '<UNDEFINED>';
+    matlabbatch{jSeg}.spm.spatial.preproc.channel.biasreg = 0.001;
+    matlabbatch{jSeg}.spm.spatial.preproc.channel.biasfwhm = 60;
+    matlabbatch{jSeg}.spm.spatial.preproc.channel.write = [0 0];
+    for iTissue = 1:6
+        matlabbatch{jSeg}.spm.spatial.preproc.tissue(iTissue).tpm = {[unil_TPM_priorsPath,',',num2str(iTissue)]};
+        matlabbatch{jSeg}.spm.spatial.preproc.tissue(iTissue).ngaus = 1;
+        switch iTissue
+            case {1,2,3}
+        matlabbatch{jSeg}.spm.spatial.preproc.tissue(iTissue).native = nativeAndDartel;
+            case {4,5,6}
+                matlabbatch{jSeg}.spm.spatial.preproc.tissue(iTissue).native = nativeOnly;
+        end
+        matlabbatch{jSeg}.spm.spatial.preproc.tissue(iTissue).warped = [0 0];
+    end
+    matlabbatch{jSeg}.spm.spatial.preproc.warp.mrf = 1;
+    matlabbatch{jSeg}.spm.spatial.preproc.warp.cleanup = 1;
+    matlabbatch{jSeg}.spm.spatial.preproc.warp.reg = [0 0.001 0.5 0.05 0.2];
+    matlabbatch{jSeg}.spm.spatial.preproc.warp.affreg = 'mni';
+    matlabbatch{jSeg}.spm.spatial.preproc.warp.fwhm = 0;
+    matlabbatch{jSeg}.spm.spatial.preproc.warp.samp = 3;
+    matlabbatch{jSeg}.spm.spatial.preproc.warp.write = [0 0];
+    matlabbatch{jSeg}.spm.spatial.preproc.warp.vox = NaN;
+    matlabbatch{jSeg}.spm.spatial.preproc.warp.bb = [NaN NaN NaN
+        NaN NaN NaN];
+    %% geodesic shooting
+    jShoot = 2 + nBatchPerSub*(iS - 1);
+    matlabbatch{jShoot}.spm.tools.shoot.warp.images{1}(1) = cfg_dep('Segment: rc1 Images', substruct('.','val', '{}',{jSeg}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','tiss', '()',{1}, '.','rc', '()',{':'}));
+    matlabbatch{jShoot}.spm.tools.shoot.warp.images{2}(1) = cfg_dep('Segment: rc2 Images', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','tiss', '()',{2}, '.','rc', '()',{':'}));
+    %% normalise in MNI space
+    jMNI = 3 + nBatchPerSub*(iS - 1);
+    matlabbatch{jMNI}.spm.tools.shoot.norm.template(1) = cfg_dep('Run Shooting (create Templates): Template (4)', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','template', '()',{5}));
+    matlabbatch{jMNI}.spm.tools.shoot.norm.data.subjs.deformations(1) = cfg_dep('Run Shooting (create Templates): Deformation Fields', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','def', '()',{':'}));
+    matlabbatch{jMNI}.spm.tools.shoot.norm.data.subjs.images{1}(1) = cfg_dep('Segment: c1 Images', substruct('.','val', '{}',{jSeg}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','tiss', '()',{1}, '.','c', '()',{':'}));
+    matlabbatch{jMNI}.spm.tools.shoot.norm.data.subjs.images{2}(1) = cfg_dep('Segment: c2 Images', substruct('.','val', '{}',{jSeg}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','tiss', '()',{2}, '.','c', '()',{':'}));
+    matlabbatch{jMNI}.spm.tools.shoot.norm.vox = [NaN NaN NaN];
+    matlabbatch{jMNI}.spm.tools.shoot.norm.bb = [NaN NaN NaN
+        NaN NaN NaN];
+    matlabbatch{jMNI}.spm.tools.shoot.norm.preserve = 1;
+    matlabbatch{jMNI}.spm.tools.shoot.norm.fwhm = 6;
+end % subject loop
+
+
+%% display spm batch before running it or run it directly
+if exist('checking','var') && checking == 1
+    spm_jobman('interactive',matlabbatch);
+    %     spm_jobman('run',matlabbatch);
+elseif ~exist('checking','var') || isempty(checking) || checking == 0
+    spm_jobman('run',matlabbatch);
+end
---
+++ LGC_Motiv_analysis/VBM/bogdan1_job.m
@@ -0,0 +1,54 @@
+%-----------------------------------------------------------------------
+% Job saved on 02-Feb-2023 18:00:57 by cfg_util (rev $Rev: 7345 $)
+% spm SPM - SPM12 (7771)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.spatial.preproc.channel.vols = '<UNDEFINED>';
+matlabbatch{1}.spm.spatial.preproc.channel.biasreg = 0.001;
+matlabbatch{1}.spm.spatial.preproc.channel.biasfwhm = 60;
+matlabbatch{1}.spm.spatial.preproc.channel.write = [0 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(1).tpm = {'D:\Matlab extensions\spm12\tpm\enhanced_TPM.nii,1'};
+matlabbatch{1}.spm.spatial.preproc.tissue(1).ngaus = 1;
+matlabbatch{1}.spm.spatial.preproc.tissue(1).native = [1 1];
+matlabbatch{1}.spm.spatial.preproc.tissue(1).warped = [0 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(2).tpm = '<UNDEFINED>';
+matlabbatch{1}.spm.spatial.preproc.tissue(2).ngaus = 1;
+matlabbatch{1}.spm.spatial.preproc.tissue(2).native = [1 1];
+matlabbatch{1}.spm.spatial.preproc.tissue(2).warped = [0 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(3).tpm = '<UNDEFINED>';
+matlabbatch{1}.spm.spatial.preproc.tissue(3).ngaus = 2;
+matlabbatch{1}.spm.spatial.preproc.tissue(3).native = [1 1];
+matlabbatch{1}.spm.spatial.preproc.tissue(3).warped = [0 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(4).tpm = '<UNDEFINED>';
+matlabbatch{1}.spm.spatial.preproc.tissue(4).ngaus = 3;
+matlabbatch{1}.spm.spatial.preproc.tissue(4).native = [0 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(4).warped = [0 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(5).tpm = '<UNDEFINED>';
+matlabbatch{1}.spm.spatial.preproc.tissue(5).ngaus = 4;
+matlabbatch{1}.spm.spatial.preproc.tissue(5).native = [0 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(5).warped = [0 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(6).tpm = '<UNDEFINED>';
+matlabbatch{1}.spm.spatial.preproc.tissue(6).ngaus = 2;
+matlabbatch{1}.spm.spatial.preproc.tissue(6).native = [0 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(6).warped = [0 0];
+matlabbatch{1}.spm.spatial.preproc.warp.mrf = 1;
+matlabbatch{1}.spm.spatial.preproc.warp.cleanup = 1;
+matlabbatch{1}.spm.spatial.preproc.warp.reg = [0 0.001 0.5 0.05 0.2];
+matlabbatch{1}.spm.spatial.preproc.warp.affreg = 'mni';
+matlabbatch{1}.spm.spatial.preproc.warp.fwhm = 0;
+matlabbatch{1}.spm.spatial.preproc.warp.samp = 3;
+matlabbatch{1}.spm.spatial.preproc.warp.write = [0 0];
+matlabbatch{1}.spm.spatial.preproc.warp.vox = NaN;
+matlabbatch{1}.spm.spatial.preproc.warp.bb = [NaN NaN NaN
+                                              NaN NaN NaN];
+matlabbatch{2}.spm.tools.shoot.warp.images{1}(1) = cfg_dep('Segment: rc1 Images', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','tiss', '()',{1}, '.','rc', '()',{':'}));
+matlabbatch{2}.spm.tools.shoot.warp.images{2}(1) = cfg_dep('Segment: rc2 Images', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','tiss', '()',{2}, '.','rc', '()',{':'}));
+matlabbatch{3}.spm.tools.shoot.norm.template(1) = cfg_dep('Run Shooting (create Templates): Template (4)', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','template', '()',{5}));
+matlabbatch{3}.spm.tools.shoot.norm.data.subjs.deformations(1) = cfg_dep('Run Shooting (create Templates): Deformation Fields', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','def', '()',{':'}));
+matlabbatch{3}.spm.tools.shoot.norm.data.subjs.images{1}(1) = cfg_dep('Segment: c1 Images', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','tiss', '()',{1}, '.','c', '()',{':'}));
+matlabbatch{3}.spm.tools.shoot.norm.data.subjs.images{2}(1) = cfg_dep('Segment: c2 Images', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','tiss', '()',{2}, '.','c', '()',{':'}));
+matlabbatch{3}.spm.tools.shoot.norm.vox = [NaN NaN NaN];
+matlabbatch{3}.spm.tools.shoot.norm.bb = [NaN NaN NaN
+                                          NaN NaN NaN];
+matlabbatch{3}.spm.tools.shoot.norm.preserve = 1;
+matlabbatch{3}.spm.tools.shoot.norm.fwhm = 6;
