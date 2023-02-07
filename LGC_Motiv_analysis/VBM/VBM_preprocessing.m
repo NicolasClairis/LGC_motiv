19675c3e00edcdaefd440eeadc9a70bc3580a5fe
NicolasClairis <nicolas.clairis@protonmail.com>
2023-02-07 15:44:29 +0100

    Update VBM_preprocessing.m

--- LGC_Motiv_analysis/VBM/VBM_preprocessing.m
+++ LGC_Motiv_analysis/VBM/VBM_preprocessing.m
@@ -8,9 +8,11 @@ spm_jobman('initcfg');
 checking = 1;
 
 %% define working directory
-
+anaPath = '';
 unil_TPM_priorsPath = 'D:\Matlab extensions\spm12\tpm\enhanced_TPM.nii';
 %% subject selection
+
+NS = 75;
 
 %% initialize size of matlabbatch
 nBatchPerSub = 3;
@@ -22,13 +24,24 @@ nativeAndDartel = [1 1];
 for iS = 1:NS
     %% segmentation
     jSeg = 1 + nBatchPerSub*(iS - 1);
-    matlabbatch{jSeg}.spm.spatial.preproc.channel.vols = '<UNDEFINED>';
+    matlabbatch{jSeg}.spm.spatial.preproc.channel.vols = anaPath;
     matlabbatch{jSeg}.spm.spatial.preproc.channel.biasreg = 0.001;
     matlabbatch{jSeg}.spm.spatial.preproc.channel.biasfwhm = 60;
     matlabbatch{jSeg}.spm.spatial.preproc.channel.write = [0 0];
     for iTissue = 1:6
         matlabbatch{jSeg}.spm.spatial.preproc.tissue(iTissue).tpm = {[unil_TPM_priorsPath,',',num2str(iTissue)]};
+        % gaussian intensity distribution differs depending on tissue type
+        switch iTissue
+            case {1,2}
         matlabbatch{jSeg}.spm.spatial.preproc.tissue(iTissue).ngaus = 1;
+            case {3,6}
+        matlabbatch{jSeg}.spm.spatial.preproc.tissue(iTissue).ngaus = 2;
+            case 4
+        matlabbatch{jSeg}.spm.spatial.preproc.tissue(iTissue).ngaus = 3;
+            case 5
+        matlabbatch{jSeg}.spm.spatial.preproc.tissue(iTissue).ngaus = 4;
+        end
+        % extract DARTEL only for 3 first tissues
         switch iTissue
             case {1,2,3}
         matlabbatch{jSeg}.spm.spatial.preproc.tissue(iTissue).native = nativeAndDartel;
@@ -50,7 +63,7 @@ for iS = 1:NS
     %% geodesic shooting
     jShoot = 2 + nBatchPerSub*(iS - 1);
     matlabbatch{jShoot}.spm.tools.shoot.warp.images{1}(1) = cfg_dep('Segment: rc1 Images', substruct('.','val', '{}',{jSeg}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','tiss', '()',{1}, '.','rc', '()',{':'}));
-    matlabbatch{jShoot}.spm.tools.shoot.warp.images{2}(1) = cfg_dep('Segment: rc2 Images', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','tiss', '()',{2}, '.','rc', '()',{':'}));
+    matlabbatch{jShoot}.spm.tools.shoot.warp.images{2}(1) = cfg_dep('Segment: rc2 Images', substruct('.','val', '{}',{jSeg}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','tiss', '()',{2}, '.','rc', '()',{':'}));
     %% normalise in MNI space
     jMNI = 3 + nBatchPerSub*(iS - 1);
     matlabbatch{jMNI}.spm.tools.shoot.norm.template(1) = cfg_dep('Run Shooting (create Templates): Template (4)', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','template', '()',{5}));
@@ -66,9 +79,9 @@ end % subject loop
 
 
 %% display spm batch before running it or run it directly
-if exist('checking','var') && checking == 1
+if checking == 1
     spm_jobman('interactive',matlabbatch);
     %     spm_jobman('run',matlabbatch);
-elseif ~exist('checking','var') || isempty(checking) || checking == 0
+elseif checking == 0
     spm_jobman('run',matlabbatch);
 end
