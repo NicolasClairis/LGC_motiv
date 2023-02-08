8cfa7fc8b501dc2ed29381e38c34f11552dbeec0
NicolasClairis <nicolas.clairis@protonmail.com>
2023-02-08 13:37:21 +0100

    adding peak force

--- LGC_Motiv_analysis/cross_correlations/grip_overshoot_f_STAI_T_PSS.m
+++ LGC_Motiv_analysis/cross_correlations/grip_overshoot_f_STAI_T_PSS.m
@@ -16,9 +16,11 @@ studyBehaviorFolder = [computerRoot, filesep, study_nm, filesep];
 
 %% extract grip overshoot and STAI-T
 [STAI_T, PSS14,...
-    grip_avg_AUC_overshoot.allTrials] = deal(NaN(1,NS));
+    grip_avg_AUC_overshoot.allTrials,...
+    grip_avg_peakF.allTrials] = deal(NaN(1,NS));
 n_Ech = 4;
-grip_avg_AUC_overshoot.per_Ech = NaN(n_Ech, NS);
+[grip_avg_AUC_overshoot.per_Ech,...
+    grip_avg_peakF.per_Ech] = deal(NaN(n_Ech, NS));
 nGripRuns = 2;
 nTrialsPerRun = 54;
 nGripTrials = nTrialsPerRun*nGripRuns;
@@ -30,8 +32,10 @@ for iS = 1:NS
     STAI_T(iS) = excelReadQuestionnairesFile.STAITraitScore(questionnaire_idx);
     PSS14(iS) = excelReadQuestionnairesFile.PSS_14Score(questionnaire_idx);
     %% extract grip overshoot
-    AUC_overshoot_perSub.allTrials = NaN(nGripTrials, 1);
-    AUC_overshoot_perSub.per_Ech = NaN(n_Ech, nGripRuns);
+    [AUC_overshoot_perSub.allTrials,...
+        peakF_perSub.allTrials] = deal(NaN(nGripTrials, 1));
+    [AUC_overshoot_perSub.per_Ech,...
+        peakF_perSub.per_Ech]  = deal(NaN(n_Ech, nGripRuns));
 % extract runs
     [runsStruct] = runs_definition(study_nm, sub_nm, 'behavior');
     okRuns = runsStruct.runsToKeep;
@@ -59,15 +63,19 @@ for iS = 1:NS
         runTrials_idx = (1:nTrialsPerRun) + nTrialsPerRun*(jGripRun-1);
         switch task_nm_tmp
             case 'Ep'
-                [~, ~, ~, AUC_overshoot_tmp] = extract_grip_force(subBehaviorFolder, sub_nm, run_nm);
+                [~, ~, forcePeak_tmp, AUC_overshoot_tmp] = extract_grip_force(subBehaviorFolder, sub_nm, run_nm);
                 AUC_overshoot_perSub.allTrials(runTrials_idx) = AUC_overshoot_tmp.allTrials;
                 AUC_overshoot_perSub.per_Ech(:,jGripRun) = AUC_overshoot_tmp.per_Ech;
+                peakF_perSub.allTrials(runTrials_idx) = forcePeak_tmp.allTrials;
+                peakF_perSub.per_Ech(:,jGripRun) = forcePeak_tmp.per_Ech;
         end
     end % run loop
 
     %% average data per subject
     grip_avg_AUC_overshoot.allTrials(iS) = mean(AUC_overshoot_perSub.allTrials,1,'omitnan');
     grip_avg_AUC_overshoot.per_Ech(:,iS) = mean(AUC_overshoot_perSub.per_Ech,2,'omitnan');
+    grip_avg_peakF.allTrials(iS) = mean(peakF_perSub.allTrials,1,'omitnan');
+    grip_avg_peakF.per_Ech(:,iS) = mean(peakF_perSub.per_Ech,2,'omitnan');
 end % subject loop
 
 %% test correlation
@@ -75,11 +83,21 @@ end % subject loop
 [b_STAI_avg_AUC_overshoot, ~,stats_STAI_avg_overshoot] = glmfit(STAI_T, grip_avg_AUC_overshoot.allTrials, 'normal');
 STAI_T_fit = sort(STAI_T);
 avg_AUC_overshoot_STAI_T_fit = glmval(b_STAI_avg_AUC_overshoot, STAI_T_fit, 'identity');
+pval.STAI.AUC_overshoot = stats_STAI_avg_overshoot.p;
+
+[b_STAI_avg_peakF, ~,stats_STAI_avg_peakF] = glmfit(STAI_T, grip_avg_peakF.allTrials, 'normal');
+avg_peakF_STAI_T_fit = glmval(b_STAI_avg_peakF, STAI_T_fit, 'identity');
+pval.STAI.peakF = stats_STAI_avg_peakF.p;
 
 % PSS-14
 [b_PSS14_avg_AUC_overshoot, ~,stats_PSS14_avg_overshoot] = glmfit(PSS14, grip_avg_AUC_overshoot.allTrials, 'normal');
 PSS14_fit = sort(PSS14);
 avg_AUC_overshoot_PSS14_fit = glmval(b_PSS14_avg_AUC_overshoot, PSS14_fit, 'identity');
+pval.PSS14.AUC_overshoot = stats_PSS14_avg_overshoot.p;
+
+[b_PSS14_avg_peakF, ~,stats_PSS14_avg_peakF] = glmfit(PSS14, grip_avg_peakF.allTrials, 'normal');
+avg_peakF_PSS14_fit = glmval(b_PSS14_avg_peakF, PSS14_fit, 'identity');
+pval.PSS14.peakF = stats_PSS14_avg_peakF.p;
 
 %% figure display
 pSize = 30;
@@ -87,6 +105,7 @@ lWidth = 3;
 black = [0 0 0];
 
 %% STAI-T
+% AUC overshoot
 fig;
 hold on;
 scat_hdl = scatter(STAI_T, grip_avg_AUC_overshoot.allTrials);
@@ -105,7 +124,21 @@ legend_size(pSize);
 % ylabel('average AUC overshoot');
 % legend_size(pSize);
 
+% peak force
+fig;
+hold on;
+scat_hdl = scatter(STAI_T, grip_avg_peakF.allTrials);
+fit_hdl = plot(STAI_T_fit, avg_peakF_STAI_T_fit);
+scat_hdl.LineWidth = lWidth;
+scat_hdl.MarkerEdgeColor = black;
+fit_hdl.LineWidth = lWidth;
+fit_hdl.LineStyle = '--';
+xlabel('STAI-T');
+ylabel('peak force (%)');
+legend_size(pSize);
+
 %% PSS-14
+% AUC overshoot
 fig;
 hold on;
 scat_hdl = scatter(PSS14, grip_avg_AUC_overshoot.allTrials);
@@ -116,4 +149,17 @@ fit_hdl.LineWidth = lWidth;
 fit_hdl.LineStyle = '--';
 xlabel('PSS14');
 ylabel('average AUC overshoot');
+legend_size(pSize);
+
+% peak force
+fig;
+hold on;
+scat_hdl = scatter(PSS14, grip_avg_peakF.allTrials);
+fit_hdl = plot(PSS14_fit, avg_peakF_PSS14_fit);
+scat_hdl.LineWidth = lWidth;
+scat_hdl.MarkerEdgeColor = black;
+fit_hdl.LineWidth = lWidth;
+fit_hdl.LineStyle = '--';
+xlabel('PSS14');
+ylabel('peak force (%)');
 legend_size(pSize);
