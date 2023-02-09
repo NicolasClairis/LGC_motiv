63b58b7bba715ad35ef0088c669175e24f39f5b8
NicolasClairis <nicolas.clairis@protonmail.com>
2023-02-09 13:52:46 +0100

    adding mediation

--- LGC_Motiv_analysis/behavior/RT_f_NV_uncertainty.m
+++ LGC_Motiv_analysis/behavior/RT_f_NV_uncertainty.m
@@ -1,5 +1,5 @@
-function[RT_f_pChoice_bin] = RT_f_NV_uncertainty(study_nm, subject_id, condition, figDisp)
-% [RT_f_pChoice_bin] = RT_f_NV_uncertainty(study_nm, subject_id, condition, figDisp)
+function[RT_f_pChoice_bin, mediation_paths, pval] = RT_f_NV_uncertainty(study_nm, subject_id, condition, figDisp)
+% [RT_f_pChoice_bin, mediation_paths, pval] = RT_f_NV_uncertainty(study_nm, subject_id, condition, figDisp)
 % RT_f_NV_uncertainty will look at how reaction times (RT) during choice
 % vary with net value depending on the uncertainty rating and uncertainty
 % derived from the model, looking at all tasks together and each task
@@ -18,6 +18,12 @@ function[RT_f_pChoice_bin] = RT_f_NV_uncertainty(study_nm, subject_id, condition
 %
 % OUTPUTS
 % RT: structure with RT
+%
+% mediation_paths: structure with average (mean) and SEM for each path of
+% the mediation for each task
+%
+% pval: structure with p.value for the t.test against zero of each path of
+% the mediation for each task
 
 %% subject selection
 if ~exist('study_nm','var') || isempty(study_nm) ||...
@@ -45,9 +51,20 @@ gitResultsFolder = [fullfile('C:','Users','clairis','Desktop',...
 %% main parameters
 tasks = {'Ep','Em'};
 nTasks = length(tasks);
+nTrialsPerRun = 54;
 nRunsPerTask = 2;
 mdl_nm = 'mdl_3';
 nBins = 6;
+[RT_perSub.Ep, confMdl_perSub.Ep, conf_rtg_perSub.Ep,...
+    RT_perSub.Em, confMdl_perSub.Em, conf_rtg_perSub.Em] = deal(NaN(nTrialsPerRun*nRunsPerTask,NS));
+[path_a_confMdl_RT.Ep, path_b_RT_confRtg.Ep,...
+    path_c_confMdl_confRtg.Ep, path_cprime_confMdl_confRtg.Ep,...
+    path_a_confMdl_RT.Em, path_b_RT_confRtg.Em,...
+    path_c_confMdl_confRtg.Em, path_cprime_confMdl_confRtg.Em,...
+    path_a_lowConfMdl_RT.Ep, path_b_lowConfMdl_RT_confRtg.Ep,...
+    path_c_lowConfMdl_confRtg.Ep, path_cprime_lowConfMdl_confRtg.Ep,...
+    path_a_lowConfMdl_RT.Em, path_b_lowConfMdl_RT_confRtg.Em,...
+    path_c_lowConfMdl_confRtg.Em, path_cprime_lowConfMdl_confRtg.Em] = deal(NaN(1,NS));
 [RT_f_pChoice_bin.Ep.high_confRtg, pChoice_f_pChoice_bin.Ep.high_confRtg,...
     RT_f_pChoice_bin.Ep.low_confRtg, pChoice_f_pChoice_bin.Ep.low_confRtg,...
     RT_f_pChoice_bin.Em.high_confRtg, pChoice_f_pChoice_bin.Em.high_confRtg,...
@@ -82,12 +99,17 @@ for iS = 1:NS
             jRun = runs.(task_nm).runsToKeep(iRun);
             run_nm = num2str(jRun);
             % extract RT, conf from the model, conf rating and NV
-            [~, ~, modelConf_tmp,pChoice_tmp] = extract_bayesian_mdl(gitResultsFolder, subBehaviorFolder,...
+            [~, ~, modelConf_tmp, pChoice_tmp] = extract_bayesian_mdl(gitResultsFolder, subBehaviorFolder,...
                 sub_nm, run_nm, task_fullName, mdl_nm);
             % RT
             [RT_tmp] = extract_RT(subBehaviorFolder, sub_nm, run_nm, task_fullName);
             % confidence rating
             [conf_rtg_tmp] = extract_confidence_rating(subBehaviorFolder, sub_nm, run_nm, task_fullName);
+            % extract data for mediation
+            run_trial_idx = (1:nTrialsPerRun) + nTrialsPerRun*(iRun - 1);
+            RT_perSub.(task_nm)(run_trial_idx, iS) = RT_tmp;
+            confMdl_perSub.(task_nm)(run_trial_idx, iS) = modelConf_tmp;
+            conf_rtg_perSub.(task_nm)(run_trial_idx, iS) = conf_rtg_tmp;
 
             % split data per confidence  levels
             high_confRtg = conf_rtg_tmp == 1;
@@ -123,6 +145,22 @@ for iS = 1:NS
         pChoice_f_pChoice_bin.(task_nm).high_confMdl(:,iS) = mean(pChoice_f_pChoice_bin_highConfMdl_tmp,2,'omitnan');
         RT_f_pChoice_bin.(task_nm).low_confMdl(:,iS) = mean(RT_f_pChoice_bin_lowConfMdl_tmp,2,'omitnan');
         pChoice_f_pChoice_bin.(task_nm).low_confMdl(:,iS) = mean(pChoice_f_pChoice_bin_lowConfMdl_tmp,2,'omitnan');
+        
+        %% perform mediation
+        % all trials
+        [path_a_confMdl_RT.(task_nm)(iS), path_b_RT_confRtg.(task_nm)(iS),...
+            path_c_confMdl_confRtg.(task_nm)(iS),...
+            path_cprime_confMdl_confRtg.(task_nm)(iS)] = mediation(confMdl_perSub.(task_nm)(:,iS), RT_perSub.(task_nm)(:,iS), conf_rtg_perSub.(task_nm)(:,iS),...
+            'conf model','RT','conf rating',0);
+        % focus on trials with initial low confidence
+        lowConf_trials_tmp = confMdl_perSub.(task_nm)(:, iS) <= 0.5;
+        if ~isempty(lowConf_trials_tmp) && sum(lowConf_trials_tmp) > 1
+            [path_a_lowConfMdl_RT.(task_nm)(iS), path_b_lowConfMdl_RT_confRtg.(task_nm)(iS),...
+                path_c_lowConfMdl_confRtg.(task_nm)(iS),...
+                path_cprime_lowConfMdl_confRtg.(task_nm)(iS)] = mediation(confMdl_perSub.(task_nm)(lowConf_trials_tmp,iS),...
+                RT_perSub.(task_nm)(lowConf_trials_tmp,iS), conf_rtg_perSub.(task_nm)(lowConf_trials_tmp,iS),...
+                'low conf model','RT','conf rating',0);
+        end
     end % task loop
 end % subject loop
 %% average data across subjects
@@ -137,6 +175,35 @@ for iT = 1:nTasks
         [m_pChoice_f_pChoice_bin.(task_nm).(cond_nm),...
             sem_pChoice_f_pChoice_bin.(task_nm).(cond_nm)] = mean_sem_sd(pChoice_f_pChoice_bin.(task_nm).(cond_nm),2);
     end % condition loop
+    
+    % average and test mediation paths
+    [mediation_paths.(task_nm).mean.allTrials.a_confMdl_RT,...
+        mediation_paths.(task_nm).sem.allTrials.a_confMdl_RT] = mean_sem_sd(path_a_confMdl_RT.(task_nm),2);
+    [mediation_paths.(task_nm).mean.allTrials.b_RT_confRtg,...
+        mediation_paths.(task_nm).sem.allTrials.b_RT_confRtg] = mean_sem_sd(path_b_RT_confRtg.(task_nm),2);
+    [mediation_paths.(task_nm).mean.allTrials.c_confMdl_confRtg,...
+        mediation_paths.(task_nm).sem.allTrials.c_confMdl_confRtg] = mean_sem_sd(path_c_confMdl_confRtg.(task_nm),2);
+    [mediation_paths.(task_nm).mean.allTrials.cprime_confMdl_confRtg,...
+        mediation_paths.(task_nm).sem.allTrials.cprime_confMdl_confRtg] = mean_sem_sd(path_cprime_confMdl_confRtg.(task_nm),2);
+    [~,pval.(task_nm).allTrials.a_confMdl_RT] = ttest(path_a_confMdl_RT.(task_nm));
+    [~,pval.(task_nm).allTrials.b_RT_confRtg] = ttest(path_b_RT_confRtg.(task_nm));
+    [~,pval.(task_nm).allTrials.c_confMdl_confRtg] = ttest(path_c_confMdl_confRtg.(task_nm));
+    [~,pval.(task_nm).allTrials.cprime_confMdl_confRtg] = ttest(path_cprime_confMdl_confRtg.(task_nm));
+    
+    % average and test mediation paths when focusing on low initial
+    % confidence trials
+    [mediation_paths.(task_nm).mean.lowConfMdl_trials.a_confMdl_RT,...
+        mediation_paths.(task_nm).sem.lowConfMdl_trials.a_confMdl_RT] = mean_sem_sd(path_a_lowConfMdl_RT.(task_nm),2);
+    [mediation_paths.(task_nm).mean.lowConfMdl_trials.b_RT_confRtg,...
+        mediation_paths.(task_nm).sem.lowConfMdl_trials.b_RT_confRtg] = mean_sem_sd(path_b_lowConfMdl_RT_confRtg.(task_nm),2);
+    [mediation_paths.(task_nm).mean.lowConfMdl_trials.c_confMdl_confRtg,...
+        mediation_paths.(task_nm).sem.lowConfMdl_trials.c_confMdl_confRtg] = mean_sem_sd(path_c_lowConfMdl_confRtg.(task_nm),2);
+    [mediation_paths.(task_nm).mean.lowConfMdl_trials.cprime_confMdl_confRtg,...
+        mediation_paths.(task_nm).sem.lowConfMdl_trials.cprime_confMdl_confRtg] = mean_sem_sd(path_cprime_lowConfMdl_confRtg.(task_nm),2);
+    [~,pval.(task_nm).lowConfMdl_trials.a_confMdl_RT] = ttest(path_a_lowConfMdl_RT.(task_nm));
+    [~,pval.(task_nm).lowConfMdl_trials.b_RT_confRtg] = ttest(path_b_lowConfMdl_RT_confRtg.(task_nm));
+    [~,pval.(task_nm).lowConfMdl_trials.c_confMdl_confRtg] = ttest(path_c_lowConfMdl_confRtg.(task_nm));
+    [~,pval.(task_nm).lowConfMdl_trials.cprime_confMdl_confRtg] = ttest(path_cprime_lowConfMdl_confRtg.(task_nm));
 end % task loop
 
 %% display figure
@@ -150,36 +217,8 @@ if figDisp == 1
         task_nm = tasks{iT};
 
         fig;
-        % RT = f(NV) split per confidence rating
-        subplot(1,2,1);
-        % low confidence
-        lowConfRtg_hdl = errorbar(m_pChoice_f_pChoice_bin.(task_nm).low_confRtg,...
-            m_RT_f_pChoice_bin.(task_nm).low_confRtg,...
-            sem_RT_f_pChoice_bin.(task_nm).low_confRtg,...
-            sem_RT_f_pChoice_bin.(task_nm).low_confRtg,...
-            sem_pChoice_f_pChoice_bin.(task_nm).low_confRtg,...
-            sem_pChoice_f_pChoice_bin.(task_nm).low_confRtg);
-        lowConfRtg_hdl.LineWidth = lWidth;
-        lowConfRtg_hdl.Color = lowConf_col;
-        hold on;
-        % high confidence
-        highConfRtg_hdl = errorbar(m_pChoice_f_pChoice_bin.(task_nm).high_confRtg,...
-            m_RT_f_pChoice_bin.(task_nm).high_confRtg,...
-            sem_RT_f_pChoice_bin.(task_nm).high_confRtg,...
-            sem_RT_f_pChoice_bin.(task_nm).high_confRtg,...
-            sem_pChoice_f_pChoice_bin.(task_nm).high_confRtg,...
-            sem_pChoice_f_pChoice_bin.(task_nm).high_confRtg);
-        highConfRtg_hdl.LineWidth = lWidth;
-        highConfRtg_hdl.Color = highConf_col;
-        legend([highConfRtg_hdl, lowConfRtg_hdl],...
-            {'high confidence rating', 'low confidence rating'});
-        legend('boxoff');
-        xlabel(['p(high effort) - ',task_nm]);
-        ylabel('RT (s)');
-        legend_size(pSize);
-
         % RT = f(NV) split per confidence based on the model
-        subplot(1,2,2);
+        subplot(1,2,1);
         % low confidence
         lowConfMdl_hdl = errorbar(m_pChoice_f_pChoice_bin.(task_nm).low_confMdl,...
             m_RT_f_pChoice_bin.(task_nm).low_confMdl,...
@@ -203,6 +242,34 @@ if figDisp == 1
             {'high model confidence', 'low model confidence'});
         legend('boxoff');
         xlabel(['p(high effort) (',task_nm,')']);
+        ylabel('RT (s)');
+        legend_size(pSize);
+        
+        % RT = f(NV) split per confidence rating
+        subplot(1,2,2);
+        % low confidence
+        lowConfRtg_hdl = errorbar(m_pChoice_f_pChoice_bin.(task_nm).low_confRtg,...
+            m_RT_f_pChoice_bin.(task_nm).low_confRtg,...
+            sem_RT_f_pChoice_bin.(task_nm).low_confRtg,...
+            sem_RT_f_pChoice_bin.(task_nm).low_confRtg,...
+            sem_pChoice_f_pChoice_bin.(task_nm).low_confRtg,...
+            sem_pChoice_f_pChoice_bin.(task_nm).low_confRtg);
+        lowConfRtg_hdl.LineWidth = lWidth;
+        lowConfRtg_hdl.Color = lowConf_col;
+        hold on;
+        % high confidence
+        highConfRtg_hdl = errorbar(m_pChoice_f_pChoice_bin.(task_nm).high_confRtg,...
+            m_RT_f_pChoice_bin.(task_nm).high_confRtg,...
+            sem_RT_f_pChoice_bin.(task_nm).high_confRtg,...
+            sem_RT_f_pChoice_bin.(task_nm).high_confRtg,...
+            sem_pChoice_f_pChoice_bin.(task_nm).high_confRtg,...
+            sem_pChoice_f_pChoice_bin.(task_nm).high_confRtg);
+        highConfRtg_hdl.LineWidth = lWidth;
+        highConfRtg_hdl.Color = highConf_col;
+        legend([highConfRtg_hdl, lowConfRtg_hdl],...
+            {'high confidence rating', 'low confidence rating'});
+        legend('boxoff');
+        xlabel(['p(high effort) - ',task_nm]);
         ylabel('RT (s)');
         legend_size(pSize);
 
