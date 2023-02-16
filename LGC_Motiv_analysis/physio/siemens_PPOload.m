27d1e08ebed6454b44aae6a2e2a2e7dc8476f802
NicolasClairis <nicolas.clairis@protonmail.com>
2023-02-16 18:07:03 +0100

    physio loading

--- LGC_Motiv_analysis/physio/siemens_PPOload.m
+++ LGC_Motiv_analysis/physio/siemens_PPOload.m
@@ -1,7 +1,6 @@
-function [signal, time, startTrigger] = siemens_RESPload2(fname,varargin)
-%function [signal, time, startTrigger] = RESP_load(fname, optname, optval);
+function [signal, time, startTrigger] = siemens_PPOload(fname,varargin)
 %
-% A loader for the Respiration data coming off of the Siemens Trio system.
+% A loader for the PulseOx data coming off of the Siemens Trio system.
 %  
 %   INPUT:
 %   fname = name of text file with respiration information
@@ -13,24 +12,25 @@ function [signal, time, startTrigger] = siemens_RESPload2(fname,varargin)
 %
 %     c) 'iPatOn': whether or not parallel imaging is on (default true)
 %
-%     d) 'EndClip': Lag time to stop the physio recording (1040ms default)
+%     d) 'EndClip': Lag time to stop the physio recording (2040ms default)
 %     
 %   OUTPUT
-%   resp   = vector of the respiration waveform
-%   time   = vector of timestamps (in seconds)
+%   ppo   = vector of the pulse-ox waveform
+%   time  = vector of timestamps (in seconds)
 %
 %  
-% Written by T. Verstynen (November 2007) and J Schlerf (August 2008)
+% Written by T. Verstynen (November 2007);
+% Hacked a bit by J Schlerf (August 2008)
 %
 % Rewritten to optimize loading time taking heavily from code written by V.
 % Deshpande and J. Grinstead, Siemens Medical Solutions (March 2009) 
 %
 % Liscensed under the GPL public license (version 3.0)
 
-% Liscensed under the GPL public license (version 3.0)
 
-if nargin < 1 || isempty(fname)
-  [filename, filepath] = uigetfile('*.resp','Get Respiration File');
+% If no input given, then get the file
+if nargin < 1 | isempty(fname);
+  [filename filepath] = uigetfile('*.puls','Get PPO File');
   fname = fullfile(filepath,filename);
 end
 
@@ -40,18 +40,19 @@ EndClip = 1000; % Always seems to take a second and change to finish
 StartClip = 0; % Some systems have to have a pause between record onset and gradient onset
 Hz = 50;   % Sampling Frequency
 DOSAVE = 0; % Saving Flag
+StartJump = 0; % Number of shift samples to adjust at the beginning of
+               % the series
 
-if nargin > 1 && ~isempty(varargin)
+if nargin > 1 & ~isempty(varargin)
     for i = 1:2:length(varargin)
         optlabel = varargin{i};
         optvalue = varargin{i+1};
         
-        if isnumeric(optvalue); optvalue = num2str(optvalue); end
+        if isnumeric(optvalue); optvalue = num2str(optvalue); end;
         eval(sprintf('%s = %s;',optlabel, optvalue));
         
-    end
-end
-
+    end;
+end;
 
 fclose('all');fid=fopen(fname);
 ignore=textscan(fid,'%s',4); %Ignore first 4 values.
@@ -59,20 +60,19 @@ ignore=textscan(fid,'%s',4); %Ignore first 4 values.
 data   = textscan(fid,'%u16'); %Read data until end of u16 data.
 footer = textscan(fid,'%s');   %Read in remaining data (time stamps and statistics).
 
-
 %Get time stamps from footer:
 for n=1:size(footer{1},1)
     if strcmp(footer{1}(n),'LogStartMDHTime:')  %log start time
-        LogStartTime=str2double(footer{1}{n+1});
+        LogStartTime=str2num(footer{1}{n+1});
     end
     if strcmp(footer{1}(n),'LogStopMDHTime:')   %log stop time
-        LogStopTime=str2double(footer{1}{n+1});
+        LogStopTime=str2num(footer{1}{n+1});
     end
     if strcmp(footer{1}(n),'LogStartMPCUTime:') %scan start time
-        ScanStartTime=str2double(footer{1}{n+1});
+        ScanStartTime=str2num(footer{1}{n+1});
     end
     if strcmp(footer{1}(n),'LogStopMPCUTime:')  %scan stop time
-        ScanStopTime=str2double(footer{1}{n+1});
+        ScanStopTime=str2num(footer{1}{n+1});
     end
 end
 
@@ -80,8 +80,8 @@ end
 t_on  = find(data{1} == 5000);  % System uses identifier 5000 as trigger ON
 t_off = find(data{1} == 5003);  % System uses identifier 5003 as trigger OFF
 
-% extract start trigger
 startTrigger = t_on(1);
+
 
 % Filter the trigger markers from the data
 data_t=transpose(1:length(data{1}));
@@ -91,18 +91,24 @@ time = (1:length(signal))./Hz;
 
 % Now work around the Dummy Scans
 if iPatOn
-  SkipTR = [1+floor(3000/TR)]+1; % extra reference scan included
+  SkipTR = (1+floor(3000/TR))+1; % extra reference scan included
 else
-  SkipTR = [1+floor(3000/TR)];
+  SkipTR = 1+floor(3000/TR);
 end
 
 % Clip the time series to begin and end with the scan.
-start = StartClip*(Hz/1000)+(TR*SkipTR)*(Hz/1000);
-stop = EndClip*(Hz/1000)+mod(length(signal(1:end)),Hz);
+if TR < 1000
+  start = StartClip*(Hz/1000)+(TR*SkipTR)*(Hz/1000);
+  stop  = length(signal) - Hz*floor((length(signal) - EndClip*(Hz/1000))/Hz);
+else
+  start = StartClip*(Hz/1000)+Hz/2+(TR*SkipTR)*(Hz/1000);
+  stop = EndClip*(Hz/1000)+mod(length(signal(1:end)),Hz)+ Hz/2;
+  %stop  = length(signal) - Hz*floor([length(signal) - EndClip*(Hz/1000)]/Hz) + Hz/2;
+end
 
 % Reset vectors;
-signal = double(signal(start+1:end-stop)');
-time = time(start+1:end-stop);
+signal = double(signal(start+1-StartJump:end-stop-StartJump)');
+time = time(start+1-StartJump:end-stop-StartJump);
 time = time(:)-time(1);
 
 % saving, then save
