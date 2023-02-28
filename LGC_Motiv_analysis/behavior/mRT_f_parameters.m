e66141d4edeea2f76ca113afa8ce242e637f3a28
NicolasClairis <nicolas.clairis@protonmail.com>
2023-02-28 14:53:24 +0100

    improvements

--- LGC_Motiv_analysis/behavior/mRT_f_parameters.m
+++ LGC_Motiv_analysis/behavior/mRT_f_parameters.m
@@ -88,30 +88,27 @@ for iS = 1:NS
                 jRun = jRun + 1;
                 runToInclude = 1;
             end
+            run_nm = num2str(iRun);
             
             if runToInclude == 1
                 runTrials_idx = (1:nTrialsPerRun) + nTrialsPerRun*(jRun-1);
-                
                 %% load data
                 behaviorStruct_tmp = load([subBehaviorFolder,...
-                    'CID',sub_nm,'_session',num2str(iRun),'_',task_fullName,...
+                    'CID',sub_nm,'_session',run_nm,'_',task_fullName,...
                     '_task.mat']);
                 % load relevant data
-                onsets_tmp = behaviorStruct_tmp.onsets;
                 choiceOptions_tmp = behaviorStruct_tmp.choice_opt;
                 switch task_id
                     case 'Ep'
-                        onsets_tmp = behaviorStruct_tmp.physicalPerf.onsets;
                         choice_LR_tmp = behaviorStruct_tmp.physicalPerf.choice;
                     case 'Em'
-                        onsets_tmp = behaviorStruct_tmp.mentalE_perf.onsets;
                         choice_LR_tmp = behaviorStruct_tmp.mentalE_perf.choice;
                 end
                 % remove confidence information from choice
                 choice_LR_tmp(choice_LR_tmp == 2) = 1;
                 choice_LR_tmp(choice_LR_tmp == -2) = -1;
                 % extract RT
-                RT_tmp = onsets_tmp.choice - onsets_tmp.dispChoiceOptions;
+                [RT_tmp] = extract_RT(subBehaviorFolder, sub_nm, run_nm, task_fullName);
                 RT_perTrial_tmp.(task_id)(runTrials_idx) = RT_tmp;
                 % extract choice made
                 defaultSide_tmp = choiceOptions_tmp.default_LR;
@@ -139,7 +136,7 @@ for iS = 1:NS
         % average the RT within each subject
         mean_RT.(task_id)(iS) = mean(raw_z_log(RT_perTrial_tmp.(task_id)),1,'omitnan');
         median_RT.(task_id)(iS) = median(raw_z_log(RT_perTrial_tmp.(task_id)),1,'omitnan');
-        sd_RT.(task_id)(iS) = std(raw_z_log(RT_perTrial_tmp.(task_id)),1,'omitnan');
+        sd_RT.(task_id)(iS) = std(raw_z_log(RT_perTrial_tmp.(task_id)),[],1,'omitnan');
     end % physical/mental loop
 end % subject loop
 
