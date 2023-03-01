46dcfe76905e8ade61607497dd44b614aae14a38
NicolasClairis <nicolas.clairis@protonmail.com>
2023-03-01 18:50:24 +0100

    keep on developing new function

--- LGC_Motiv_analysis/cross_correlations/choice_f_ROI.m
+++ LGC_Motiv_analysis/cross_correlations/choice_f_ROI.m
@@ -21,6 +21,15 @@ condition = subject_condition;
 if ~exist('nBins','var') || isempty(nBins)
     nBins = 9;
 end
+tasks = {'Ep','Em'};
+nTasks = length(tasks);
+nTrialsPerRun = 54;
+nRunsPerTask = 2;
+nTrialsPerTask = nTrialsPerRun*nRunsPerTask;
+[fMRI_allTrials.Ep, choice_hE_allTrials.Ep,...
+    fMRI_allTrials.Em, choice_hE_allTrials.Em] = deal(NaN(nTrialsPerTask, NS));
+[fMRI_bins.Ep, choice_hE_bins.Ep,...
+    fMRI_bins.Em, choice_hE_bins.Em] = deal(NaN(nBins, NS));
 
 %% extract ROI activity for all subjects
 [ROI_trial_b_trial] = extract_ROI_betas_onsets_only(computerRoot,...
@@ -33,10 +42,43 @@ end
 
 %% loop through subjects
 for iS = 1:NS
+    sub_nm = subject_id{iS};
+    subBehaviorFolder = [studyBehaviorFolder, 'CID',sub_nm, filesep, 'behavior',filesep];
+    
+    % extract runs
+    [runsStruct] = runs_definition(study_nm, sub_nm, 'behavior');
+    okRuns = runsStruct.runsToKeep;
+    taskNames = runsStruct.tasks;
+    jRun = 0;
+    for iRun = 1:length(okRuns)
+        kRun = okRuns(iRun);
+        run_nm = num2str(kRun);
+        jRun = jRun + 1;
+        task_nm_tmp = taskNames{jRun};
+        switch task_nm_tmp
+            case 'Em'
+                task_fullName = 'mental';
+            case 'Ep'
+                task_fullName = 'physical';
+        end
+        % define which task session it is
+        switch kRun
+            case {1,2}
+                taskRun_idx = 1;
+            case {3,4}
+                taskRun_idx = 2;
+        end
+        run_nm_bis = ['run',num2str(taskRun_idx)];
+        runTrials_idx = (1:nTrialsPerRun) + nTrialsPerRun*(taskRun_idx-1);
+        
+        % extract fMRI ROI as well
+    end % run loop
+    
+    %% extract the bins
     
 end % subject loop
 
-%% average 
+%% average data across subjects
 
 %% figure
 
