a1e5945135739d4e6a5e247e311b6abd614e7a3a
NicolasClairis <nicolas.clairis@protonmail.com>
2023-07-31 11:40:43 +0200

    Create performance_avg.m

---
+++ LGC_Motiv_analysis/behavior/performance_avg.m
@@ -0,0 +1,16 @@
+function[avg_perf] = performance_avg(study_nm, subject_id, condition)
+%[avg_perf] = performance_avg(study_nm, subject_id, condition)
+% performance_avg will look at the average performance for each task of
+% each subject
+%
+% INPUTS
+% study_nm: study name
+%
+% subject_id: list of subjects
+%
+% condition: condition
+%
+% OUTPUTS
+% avg_perf: average performance
+
+end % function
