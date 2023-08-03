e54db0a14802330a4601fe949fc98a653d5a0635
NicolasClairis <nicolas.clairis@protonmail.com>
2023-08-03 17:54:37 +0200

    adding colours

--- Matlab_DIY_functions/general_fig_prm.m
+++ Matlab_DIY_functions/general_fig_prm.m
@@ -23,5 +23,11 @@ mSize = 20;
 col.white = [1 1 1];
 col.black = [0 0 0];
 col.grey = [174 174 174]./255;
+% 4 colours compatible with daltonian perception
+% based on https://colorbrewer2.org/#type=diverging&scheme=Spectral&n=4
+col.red = [215 25 28]./255;
+col.orange = [253 174 97]./255;
+col.green = [171 221 164]./255;
+col.blue = [43 131 186]./255;
 
 end % function
