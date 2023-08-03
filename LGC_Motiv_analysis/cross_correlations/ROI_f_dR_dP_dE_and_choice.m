39c610c592c3c490468f0e0a56227bb00e3d12fc
NicolasClairis <nicolas.clairis@protonmail.com>
2023-08-03 17:54:53 +0200

    finish 1st version of script

--- LGC_Motiv_analysis/cross_correlations/ROI_f_dR_dP_dE_and_choice.m
+++ LGC_Motiv_analysis/cross_correlations/ROI_f_dR_dP_dE_and_choice.m
@@ -31,6 +31,7 @@ tasks = {'Ep','Em'};
 nTasks = length(tasks);
 
 for iT = 1:nTasks
+    task_nm = tasks{iT};
     % data for all trials
     [fMRI_ROI.(task_nm).allTrials, choice_hE.(task_nm).allTrials,...
         RP.(task_nm).allTrials,...
@@ -103,9 +104,177 @@ for iS = 1:NS
     for iT = 1:nTasks
         task_nm = tasks{iT};
         
+        %% extract data per high effort option features
         % extract data per reward level
         for iR = 1:n_hR_levels
-            
+            R_idx = hR_level.(task_nm).allTrials(:,iS) == iR;
+            fMRI_ROI.(task_nm).hR_level(:,iS) = mean(fMRI_ROI.(task_nm_tmp).allTrials(R_idx, iS),1,'omitnan');
+            choice_hE.(task_nm).hR_level(:,iS) = mean(choice_hE.(task_nm_tmp).allTrials(R_idx, iS),1,'omitnan');
         end % reward
+
+        % extract data per punishment level
+        for iP = 1:n_hP_levels
+            P_idx = hP_level.(task_nm).allTrials(:,iS) == iP;
+            fMRI_ROI.(task_nm).hP_level(:,iS) = mean(fMRI_ROI.(task_nm_tmp).allTrials(P_idx, iS),1,'omitnan');
+            choice_hE.(task_nm).hP_level(:,iS) = mean(choice_hE.(task_nm_tmp).allTrials(P_idx, iS),1,'omitnan');
+        end % punishment
+
+        % extract data per effort level
+        for iE = 1:n_hE_levels
+            E_idx = hE_level.(task_nm).allTrials(:,iS) == iE;
+            fMRI_ROI.(task_nm).hE_level(:,iS) = mean(fMRI_ROI.(task_nm_tmp).allTrials(E_idx, iS),1,'omitnan');
+            choice_hE.(task_nm).hE_level(:,iS) = mean(choice_hE.(task_nm_tmp).allTrials(E_idx, iS),1,'omitnan');
+        end % effort
+
+        %% extract data depending on choice (high/low) and high effort option features
+        choice_highE_idx_tmp    = choice_hE.(task_nm_tmp).allTrials(:, iS) == 1;
+
+        % reward
+        for iR = 1:n_hR_levels
+            R_hE_idx = (hR_level.(task_nm).allTrials(:,iS) == iR).*(choice_highE_idx_tmp == 1) == 1;
+            R_lE_idx = (hR_level.(task_nm).allTrials(:,iS) == iR).*(choice_highE_idx_tmp == 0) == 1;
+            fMRI_ROI.(task_nm).hEch.hR_level(:,iS) = mean(fMRI_ROI.(task_nm_tmp).allTrials(R_hE_idx, iS),1,'omitnan');
+            choice_hE.(task_nm).lEch.hR_level(:,iS) = mean(choice_hE.(task_nm_tmp).allTrials(R_lE_idx, iS),1,'omitnan');
+        end
+
+        % punishment
+        for iP = 1:n_hP_levels
+            P_hE_idx = (hP_level.(task_nm).allTrials(:,iS) == iP).*(choice_highE_idx_tmp == 1) == 1;
+            P_lE_idx = (hP_level.(task_nm).allTrials(:,iS) == iP).*(choice_highE_idx_tmp == 0) == 1;
+            fMRI_ROI.(task_nm).lEch.hP_level(:,iS) = mean(fMRI_ROI.(task_nm_tmp).allTrials(P_hE_idx, iS),1,'omitnan');
+            fMRI_ROI.(task_nm).hEch.hP_level(:,iS) = mean(choice_hE.(task_nm_tmp).allTrials(P_lE_idx, iS),1,'omitnan');
+        end
+
+        % effort
+        for iE = 1:n_hE_levels
+            E_hE_idx = (hP_level.(task_nm).allTrials(:,iS) == iE).*(choice_highE_idx_tmp == 1) == 1;
+            E_lE_idx = (hP_level.(task_nm).allTrials(:,iS) == iE).*(choice_highE_idx_tmp == 0) == 1;
+            fMRI_ROI.(task_nm).lEch.hE_level(:,iS) = mean(fMRI_ROI.(task_nm_tmp).allTrials(E_hE_idx, iS),1,'omitnan');
+            fMRI_ROI.(task_nm).hEch.hE_level(:,iS) = mean(choice_hE.(task_nm_tmp).allTrials(E_lE_idx, iS),1,'omitnan');
+        end
     end % task loop
 end % subject loop
+
+%% figure parameter
+[pSize, lWidth, col, mSize] = general_fig_prm;
+figHighE = fig;
+figChoice_x_highE = fig;
+
+%% loop through tasks for average + figure
+for iT = 1:nTasks
+    task_nm = tasks{iT};
+
+    %% average across subjects
+    % data split by level of incentive proposed for high effort option
+    [mean_fMRI_ROI.(task_nm).hR_level,...
+        sem_fMRI_ROI.(task_nm).hR_level] = mean_sem_sd(fMRI_ROI.(task_nm).hR_level, 2);
+    [mean_choice_hE.(task_nm).hR_level,...
+        sem_choice_hE.(task_nm).hR_level] = mean_sem_sd(choice_hE.(task_nm).hR_level,2);
+    [mean_fMRI_ROI.(task_nm).hP_level,...
+        sem_fMRI_ROI.(task_nm).hP_level] = mean_sem_sd(fMRI_ROI.(task_nm).hP_level,2);
+    [mean_choice_hE.(task_nm).hP_level,...
+        sem_choice_hE.(task_nm).hP_level] = mean_sem_sd(choice_hE.(task_nm).hP_level,2);
+    
+    % data split by level of effort proposed for high effort option
+    [mean_fMRI_ROI.(task_nm).hE_level,...
+        sem_fMRI_ROI.(task_nm).hE_level] = mean_sem_sd(fMRI_ROI.(task_nm).hE_level,2);
+    [mean_choice_hE.(task_nm).hE_level,...
+        sem_choice_hE.(task_nm).hE_level] = mean_sem_sd(choice_hE.(task_nm).hE_level,2);
+    
+    % data split by incentive*choice
+    [mean_fMRI_ROI.(task_nm).lEch.hR_level,...
+        sem_fMRI_ROI.(task_nm).lEch.hR_level] = mean_sem_sd(fMRI_ROI.(task_nm).lEch.hR_level,2);
+    [mean_fMRI_ROI.(task_nm).hEch.hR_level,...
+        sem_fMRI_ROI.(task_nm).hEch.hR_level] = mean_sem_sd(fMRI_ROI.(task_nm).hEch.hR_level,2);
+    [mean_fMRI_ROI.(task_nm).lEch.hP_level,...
+        sem_fMRI_ROI.(task_nm).lEch.hP_level] = mean_sem_sd(fMRI_ROI.(task_nm).lEch.hP_level,2);
+    [mean_fMRI_ROI.(task_nm).hEch.hP_level,...
+        sem_fMRI_ROI.(task_nm).hEch.hP_level] = mean_sem_sd(fMRI_ROI.(task_nm).hEch.hP_level,2);
+
+    % data split by effort*choice
+    [mean_fMRI_ROI.(task_nm).lEch.hE_level,...
+        sem_fMRI_ROI.(task_nm).lEch.hE_level] = mean_sem_sd(fMRI_ROI.(task_nm).lEch.hE_level,2);
+    [mean_fMRI_ROI.(task_nm).hEch.hE_level,...
+        sem_fMRI_ROI.(task_nm).hEch.hE_level] = mean_sem_sd(fMRI_ROI.(task_nm).hEch.hE_level,2);
+
+    %% figures
+
+    %% figure focused on varying option
+    figure(figHighE);
+
+    % reward
+    subplot(1,3,1); hold on;
+    R_er_hdl = errorbar(1:n_hR_levels,...
+        mean_fMRI_ROI.(task_nm).hR_level,...
+        sem_fMRI_ROI.(task_nm).hR_level);
+    errorbar_hdl_upgrade(R_er_hdl);
+    xlabel('Reward');
+    ylabel(ROI_short_nm);
+    legend_size(pSize);
+
+    % punishment
+    subplot(1,3,2); hold on;
+    P_er_hdl = errorbar(1:n_hP_levels,...
+        mean_fMRI_ROI.(task_nm).hP_level,...
+        sem_fMRI_ROI.(task_nm).hP_level);
+    errorbar_hdl_upgrade(P_er_hdl);
+    xlabel('Punishment');
+    ylabel(ROI_short_nm);
+    legend_size(pSize);
+
+    % effort
+    subplot(1,3,3); hold on;
+    E_er_hdl = errorbar(1:n_hE_levels,...
+        mean_fMRI_ROI.(task_nm).hE_level,...
+        sem_fMRI_ROI.(task_nm).hE_level);
+    errorbar_hdl_upgrade(E_er_hdl);
+    xlabel('Effort');
+    ylabel(ROI_short_nm);
+    legend_size(pSize);
+
+
+    %% figure for choice*high effort option
+    figure(figChoice_x_highE);
+
+    % reward
+    subplot(1,3,1); hold on;
+    R_hE_er_hdl = errorbar(1:n_hR_levels,...
+        mean_fMRI_ROI.(task_nm).hEch.hR_level,...
+        sem_fMRI_ROI.(task_nm).hEch.hR_level);
+    R_lE_er_hdl = errorbar(1:n_hR_levels,...
+        mean_fMRI_ROI.(task_nm).lEch.hR_level,...
+        sem_fMRI_ROI.(task_nm).lEch.hR_level);
+    errorbar_hdl_upgrade(R_hE_er_hdl, col.orange);
+    errorbar_hdl_upgrade(R_lE_er_hdl, col.blue);
+    xlabel('Reward');
+    ylabel(ROI_short_nm);
+    legend_size(pSize);
+
+    % punishment
+    subplot(1,3,2); hold on;
+    P_hE_er_hdl = errorbar(1:n_hP_levels,...
+        mean_fMRI_ROI.(task_nm).hEch.hP_level,...
+        sem_fMRI_ROI.(task_nm).hEch.hP_level);
+    P_lE_er_hdl = errorbar(1:n_hP_levels,...
+        mean_fMRI_ROI.(task_nm).lEch.hP_level,...
+        sem_fMRI_ROI.(task_nm).lEch.hP_level);
+    errorbar_hdl_upgrade(P_hE_er_hdl, col.orange);
+    errorbar_hdl_upgrade(P_lE_er_hdl, col.blue);
+    xlabel('Punishment');
+    ylabel(ROI_short_nm);
+    legend_size(pSize);
+
+    % effort
+    subplot(1,3,3); hold on;
+    E_hE_er_hdl = errorbar(1:n_hE_levels,...
+        mean_fMRI_ROI.(task_nm).hEch.hE_level,...
+        sem_fMRI_ROI.(task_nm).hEch.hE_level);
+    E_lE_er_hdl = errorbar(1:n_hE_levels,...
+        mean_fMRI_ROI.(task_nm).lEch.hE_level,...
+        sem_fMRI_ROI.(task_nm).lEch.hE_level);
+    errorbar_hdl_upgrade(E_hE_er_hdl, col.orange);
+    errorbar_hdl_upgrade(E_lE_er_hdl, col.blue);
+    xlabel('Effort');
+    ylabel(ROI_short_nm);
+    legend_size(pSize);
+end % task loop
