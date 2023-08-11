34108cee2ae83950cd1184d490a82fdeaecde27e
NicolasClairis <nicolas.clairis@protonmail.com>
2023-08-11 17:45:20 +0200

    modif allowing to loop on script more easily

--- LGC_Motiv_analysis/fMRI/launch_fMRI_global_analysis.m
+++ LGC_Motiv_analysis/fMRI/launch_fMRI_global_analysis.m
@@ -1,9 +1,13 @@
 
 %% define main parameters
-GLM_str = inputdlg('GLM number?');
-GLM = str2double(GLM_str); % convert from cell to numeric
+if ~exist('GLM','var') || isempty(GLM)
+    GLM_str = inputdlg('GLM number?');
+    GLM = str2double(GLM_str); % convert from cell to numeric
+end
 checking = 0;
-condition = subject_condition;
+if ~exist('condition','var') || isempty(condition)
+    condition = subject_condition;
+end
 study_nm = 'study1';
 
 %% launch first level, contrasts and 2nd level subsequently
