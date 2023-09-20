bc731260e01826597723f6dfba96cbd323264769
NicolasClairis <nicolas.clairis@protonmail.com>
2023-09-20 11:40:07 +0200

    GLM 170

--- LGC_Motiv_analysis/fMRI/which_GLM.m
+++ LGC_Motiv_analysis/fMRI/which_GLM.m
@@ -4560,6 +4560,34 @@ switch GLM
             % feedback
             GLMprm.model_onset.(Epm_nm).fbk = 'stick';
         end % physical/mental loop
+    case 170 % like GLM169 but orthogonalizing variables + RT at the beginning
+        % general parameters
+        GLMprm.gal.orth_vars = 1;
+        GLMprm.gal.zPerRun = 1;
+        GLMprm.gal.grey_mask = 7;
+        GLMprm.gal.mask_probaThreshold = 5;
+        % loop per task
+        for iEpm = 1:length(Epm)
+            Epm_nm = Epm{iEpm};
+            % choice
+            GLMprm.model_onset.(Epm_nm).choice = 'stick';
+            GLMprm.choice.(Epm_nm).RP.E.R_chosen = 1;
+            GLMprm.choice.(Epm_nm).RP.E.P_chosen = 1;
+            GLMprm.choice.(Epm_nm).RP.E.E_chosen = 1;
+            switch Epm_nm
+                case 'Ep'
+                    GLMprm.choice.(Epm_nm).RP.E.F_integral = 3;
+                case 'Em'
+                    GLMprm.choice.(Epm_nm).RP.E.efficacy = 1;
+            end
+            GLMprm.choice.(Epm_nm).RP.E.RT = 4;
+            % chosen option display
+            GLMprm.model_onset.(Epm_nm).chosen = 'stick';
+            % effort perf (effort execution)
+            GLMprm.model_onset.(Epm_nm).Eperf = 'stick';
+            % feedback
+            GLMprm.model_onset.(Epm_nm).fbk = 'stick';
+        end % physical/mental loop
 end % GLM number
 %% warnings: check compatibility of the GLM parameters entered
 isGLMokCheck(GLMprm);
