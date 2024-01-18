17268912a755883ea73501bff2596cac4824b1bb
NicolasClairis <nicolas.clairis@protonmail.com>
2024-01-18 18:22:42 +0100

    Update dmPFC_vs_aINS_Lac.m

--- LGC_Motiv_analysis/MRS/dmPFC_vs_aINS_Lac.m
+++ LGC_Motiv_analysis/MRS/dmPFC_vs_aINS_Lac.m
@@ -7,6 +7,9 @@
 dmPFC_Lac = metabolites.dmPFC.Lac;
 aIns_Lac = metabolites.aIns.Lac;
 
+%% check number of subjects
+NS_goodSubs = sum(~isnan(dmPFC_Lac.*aIns_Lac));
+
 %% extract and correlate dmPFC/dACC Lac vs aINS Lac
 [r_corr, betas, pval, y_fit,...
     aIns_Lac_sorted, dmPFC_Lac_fit_aInsSorted] = glm_package(aIns_Lac, dmPFC_Lac, 'normal', 'on');
