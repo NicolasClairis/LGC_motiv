82d92cb8f86be9b1fa506192ec083c9ebdcef08b
NicolasClairis <nicolas.clairis@protonmail.com>
2024-01-29 11:47:27 +0100

    Update rmv_outliers_3sd.m

--- Matlab_DIY_functions/rmv_outliers_3sd.m
+++ Matlab_DIY_functions/rmv_outliers_3sd.m
@@ -1,5 +1,5 @@
-function [cleaned_X, idx_badSubs, cleaned_X_bis] = rmv_outliers_3sd(variable_X)
-%[cleaned_X, idx_badSubs, cleaned_X_bis] = rmv_outliers_3sd(variable_X)
+function [cleaned_X, idx_badSubs, cleaned_X_bis, idx_goodSubs] = rmv_outliers_3sd(variable_X)
+%[cleaned_X, idx_badSubs, cleaned_X_bis, idx_goodSubs] = rmv_outliers_3sd(variable_X)
 % rmv_outliers_3sd will remove outliers (>mean+3SD or <mean-3SD) from
 % variable_X and give you the corresponding index. It will also remove any
 % NaN and include the index as well.
@@ -14,6 +14,9 @@ function [cleaned_X, idx_badSubs, cleaned_X_bis] = rmv_outliers_3sd(variable_X)
 % subjects
 %
 % cleaned_X_bis: 1*n1 vector where any outlier is replaced by a NaN
+%
+% idx_goodSubs: 1*n1 vector indicating the index, in variable_X, of the
+% good subjects
 
 %% check that input is ok in terms of lines/columns and adapt it if not
 if size(variable_X,1) > 1 && size(variable_X,2) == 1
@@ -27,6 +30,7 @@ end
 
 %% check if there are NaNs
 idx_badSubs = isnan(variable_X);
+idx_goodSubs = ~isnan(variable_X);
 
 %% extract mean and SD
 mu_X = median(variable_X, 2, 'omitnan');
