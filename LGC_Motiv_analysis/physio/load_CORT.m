dbd263d25e7d6587d16f000a56436513274b7477
NicolasClairis <nicolas.clairis@protonmail.com>
2024-02-16 17:31:56 +0100

    update to filter subs

--- LGC_Motiv_analysis/physio/load_CORT.m
+++ LGC_Motiv_analysis/physio/load_CORT.m
@@ -1,9 +1,11 @@
-function[CORT_data] = load_CORT(study_nm)
-% [CORT_data] = load_CORT(study_nm)
+function[CORT_data] = load_CORT(study_nm, subject_id)
+% [CORT_data] = load_CORT(study_nm, subject_id)
 % load_CORT will load the cortisol data.
 %
 % INPUTS
 % study_nm: string with study name
+%
+% subject_id: list of subjects to extract
 %
 % OUTPUTS
 % CORT_data: structure with cortisol data with the following subfields:
@@ -40,5 +42,24 @@ CORT_data.timings = [excelReadTable.SalivaA1Cortisol';...
     excelReadTable.SalivaBCortisol_post_MRS_';...
     excelReadTable.SalivaDCortisol_pr__IRMf_';...
     excelReadTable.SalivaECortisol_post_IRMf_'].*24; % store timings in hours
+
+%% filter based on subjects entered in input if subject_id is not empty
+if exist('subject_id','var') && ~isempty(subject_id)
+    NS = length(subject_id);
+    sub_to_include = false(1,NS);
+    for iS = 1:NS
+        sub_nm = ['CID',subject_id{iS}];
+        sub_idx = find(strcmp(sub_nm, CORT_data.CID));
+        if ~isempty(sub_idx)
+           sub_to_include(sub_idx) = true;
+        end
+    end % subject loop
+    
+    % apply filter on output
+    CORT_data.CID = CORT_data.CID(sub_to_include);
+    CORT_data.AUCg = CORT_data.AUCg(sub_to_include);
+    CORT_data.CORT = CORT_data.CORT(:,sub_to_include);
+    CORT_data.timings = CORT_data.timings(:,sub_to_include);
+end % subject filter
 
 end % function
