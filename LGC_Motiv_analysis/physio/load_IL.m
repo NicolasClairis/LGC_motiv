adfc88384baee7339ba5f003fcdce2992a1e5f00
NicolasClairis <nicolas.clairis@protonmail.com>
2024-02-16 17:31:50 +0100

    update to filter subs

--- LGC_Motiv_analysis/physio/load_IL.m
+++ LGC_Motiv_analysis/physio/load_IL.m
@@ -1,9 +1,11 @@
-function[IL_data] = load_IL(study_nm)
-% [IL_data] = load_IL(study_nm)
+function[IL_data] = load_IL(study_nm, subject_id)
+% [IL_data] = load_IL(study_nm, subject_id)
 % load_IL will load the interleukins data for the study defined in input.
 %
 % INPUTS
 % study_nm: study name
+%
+% subject_id: list of subjects to extract
 %
 % OUTPUTS
 % IL_data: structure with  interleukines data with the following subfields:
@@ -36,4 +38,22 @@ IL_data.IL1b = excelReadTable.Ilb_pg_mL_;
 IL_data.IL6 = excelReadTable.IL6_pg_mL_;
 IL_data.IL18 = excelReadTable.IL18_pg_mL_;
 
+%% filter based on subjects entered in input if subject_id is not empty
+if exist('subject_id','var') && ~isempty(subject_id)
+    NS = length(subject_id);
+    sub_to_include = false(1,NS);
+    for iS = 1:NS
+        sub_nm = ['CID',subject_id{iS}];
+        sub_idx = find(strcmp(sub_nm, excelReadTable.CID));
+        if ~isempty(sub_idx)
+           sub_to_include(sub_idx) = true;
+        end
+    end % subject loop
+    
+    % apply filter on output
+    IL_data.CID = IL_data.CID(sub_to_include);
+    IL_data.IL1b = IL_data.IL1b(sub_to_include);
+    IL_data.IL6 = IL_data.IL6(:,sub_to_include);
+    IL_data.IL18 = IL_data.IL18(:,sub_to_include);
+end % subject filter
 end % function
