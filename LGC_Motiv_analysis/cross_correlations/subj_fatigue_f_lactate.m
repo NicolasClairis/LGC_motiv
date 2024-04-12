308359faae910b1c7b8402e0fb9feed14f1367b4
NicolasClairis <nicolas.clairis@protonmail.com>
2024-04-12 15:56:56 +0200

    adding outlier removal + figure

--- LGC_Motiv_analysis/cross_correlations/subj_fatigue_f_lactate.m
+++ LGC_Motiv_analysis/cross_correlations/subj_fatigue_f_lactate.m
@@ -1,7 +1,13 @@
-function[betas, pval, r_corr] = subj_fatigue_f_lactate()
-% [betas, pval, rho] = subj_fatigue_f_lactate()
+function[betas, pval, r_corr] = subj_fatigue_f_lactate(fig_disp, rmv_outliers_yn)
+% [betas, pval, rho] = subj_fatigue_f_lactate(fig_disp, rmv_outliers_yn)
 % subj_fatigue_f_lactate will compare plasma and brain levels of lactate to
 % subjective ratings of fatigue across the experiment.
+%
+% INPUTS
+% fig_disp: display figure (1) or not (0)? By default will display it
+%
+% rmv_outliers_yn: remove median +/- 3*SD outliers yes (1) or no (0)? Yes by
+% default
 %
 % OUTPUTS
 % betas: regression estimates for correlations
@@ -9,6 +15,15 @@ function[betas, pval, r_corr] = subj_fatigue_f_lactate()
 % pval: structure with p.value for correlations
 %
 % rho: correlation coefficient for correlations
+
+%% default inputs
+if ~exist('fig_disp','var') || isempty(fig_disp) || ~ismember(fig_disp,[0,1])
+    fig_disp = 1;
+end
+% remove outliers by default
+if ~exist('rmv_outliers_yn','var') || isempty(rmv_outliers_yn) || ~ismember(rmv_outliers_yn,[0,1])
+    rmv_outliers_yn = 1;
+end
 
 %% subject selection
 [study_nm, ~, ~, subject_id, NS] = sub_id;
@@ -35,6 +50,19 @@ aIns_Lac(:) = metabolites.aIns.Lac;
 [plasma_Lac_struct] = load_plasma_Lac(subject_id);
 plasma_Lac(:) = plasma_Lac_struct.Lac;
 
+%% remove outliers
+if rmv_outliers_yn == 1
+    [~,~,subj_fatigue(1,:)] = rmv_outliers_3sd(subj_fatigue(1,:));
+    [~,~,subj_fatigue(2,:)] = rmv_outliers_3sd(subj_fatigue(2,:));
+    [~,~,subj_fatigue(3,:)] = rmv_outliers_3sd(subj_fatigue(3,:));
+    [~,~,subj_fatigue(4,:)] = rmv_outliers_3sd(subj_fatigue(4,:));
+    [~,~,subj_fatigue(5,:)] = rmv_outliers_3sd(subj_fatigue(5,:));
+    [~,~,subj_fatigue(6,:)] = rmv_outliers_3sd(subj_fatigue(6,:));
+    [~,~,dmPFC_Lac] = rmv_outliers_3sd(dmPFC_Lac);
+    [~,~,aIns_Lac] = rmv_outliers_3sd(aIns_Lac);
+    [~,~,plasma_Lac] = rmv_outliers_3sd(plasma_Lac);
+end % remove outliers
+
 %% test correlations
 for iF = 1:n_fatigue_rtgs
     fatigue_nm = fatigue_rating_names{iF};
@@ -55,4 +83,97 @@ for iF = 1:n_fatigue_rtgs
         ~, aIns_Lac_sorted,...
         subj_fatigue_fit.(fatigue_aIns_nm)] = glm_package(aIns_Lac, subj_fatigue(iF,:), 'normal');
 end % loop over fatigue periods
+
+%% figure
+if fig_disp == 1
+    %% aggregate data in one big matrix for the heatmap
+    dmPFC_r_vector = [r_corr.preMRS_fatigue_f_dmPFC_Lac;...
+        r_corr.postMRS_fatigue_f_dmPFC_Lac;...
+        r_corr.prefMRI_fatigue_f_dmPFC_Lac;...
+        r_corr.postfMRI_fatigue_f_dmPFC_Lac;...
+        r_corr.postfMRI_min_preMRS_fatigue_f_dmPFC_Lac;...
+        r_corr.postfMRI_min_prefMRI_fatigue_f_dmPFC_Lac];
+    aIns_r_vector = [r_corr.preMRS_fatigue_f_aIns_Lac;...
+        r_corr.postMRS_fatigue_f_aIns_Lac;...
+        r_corr.prefMRI_fatigue_f_aIns_Lac;...
+        r_corr.postfMRI_fatigue_f_aIns_Lac;...
+        r_corr.postfMRI_min_preMRS_fatigue_f_aIns_Lac;...
+        r_corr.postfMRI_min_prefMRI_fatigue_f_aIns_Lac];
+    plasma_r_vector = [r_corr.preMRS_fatigue_f_plasma_Lac;...
+        r_corr.postMRS_fatigue_f_plasma_Lac;...
+        r_corr.prefMRI_fatigue_f_plasma_Lac;...
+        r_corr.postfMRI_fatigue_f_plasma_Lac;...
+        r_corr.postfMRI_min_preMRS_fatigue_f_plasma_Lac;...
+        r_corr.postfMRI_min_prefMRI_fatigue_f_plasma_Lac];
+    corr_mtrx = [dmPFC_r_vector, aIns_r_vector, plasma_r_vector];
+    
+    % same with p.value
+    dmPFC_pval_vector = [pval.preMRS_fatigue_f_dmPFC_Lac(2);...
+        pval.postMRS_fatigue_f_dmPFC_Lac(2);...
+        pval.prefMRI_fatigue_f_dmPFC_Lac(2);...
+        pval.postfMRI_fatigue_f_dmPFC_Lac(2);...
+        pval.postfMRI_min_preMRS_fatigue_f_dmPFC_Lac(2);...
+        pval.postfMRI_min_prefMRI_fatigue_f_dmPFC_Lac(2)];
+    aIns_pval_vector = [pval.preMRS_fatigue_f_aIns_Lac(2);...
+        pval.postMRS_fatigue_f_aIns_Lac(2);...
+        pval.prefMRI_fatigue_f_aIns_Lac(2);...
+        pval.postfMRI_fatigue_f_aIns_Lac(2);...
+        pval.postfMRI_min_preMRS_fatigue_f_aIns_Lac(2);...
+        pval.postfMRI_min_prefMRI_fatigue_f_aIns_Lac(2)];
+    plasma_pval_vector = [pval.preMRS_fatigue_f_plasma_Lac(2);...
+        pval.postMRS_fatigue_f_plasma_Lac(2);...
+        pval.prefMRI_fatigue_f_plasma_Lac(2);...
+        pval.postfMRI_fatigue_f_plasma_Lac(2);...
+        pval.postfMRI_min_preMRS_fatigue_f_plasma_Lac(2);...
+        pval.postfMRI_min_prefMRI_fatigue_f_plasma_Lac(2)];
+    pval_mtrx = [dmPFC_pval_vector, aIns_pval_vector, plasma_pval_vector];
+    
+    %% general figure parameters
+    [pSize, ~, col] = general_fig_prm;
+    % define which colormap you want to use (see full list here if you are not
+    % happy with the selection:
+    % https://ch.mathworks.com/help/matlab/ref/colormap.html)
+    % color_range_choices = 'hot';
+    % color_range_choices = 'turbo';
+    % color_range_choices = 'jet';
+    color_range_choices = redblue(45);
+    
+    % correlation range
+    corr_range = [-0.65 0.65];
+    
+    % x/y-axis ratio
+    ax_ratio = [1.5 1 1];
+    
+    %% heatmap with correlation coefficients
+    fig_heatmap = fig;
+    subP = subplot(1,2,1);
+    imagesc(corr_mtrx, corr_range);
+    colormap(subP, color_range_choices);
+    cbar = colorbar;
+    cbar.Label.String = 'r';
+    xticks(1:3);
+    xticklabels({'dmPFC/dACC','aIns','plasma'});
+    yticks(1:size(corr_mtrx,1));
+    yticklabels({'F1','F2','F3','F4','dF1','dF2'});
+    % add stars in the graph if some correlations are significant
+    for iLac_measure = 1:size(corr_mtrx,2)
+        for iFatigueRtg = 1:size(corr_mtrx,1)
+            if pval_mtrx(iFatigueRtg, iLac_measure) <= 0.05
+                if pval_mtrx(iFatigueRtg, iLac_measure) > 0.01 && pval_mtrx(iFatigueRtg, iLac_measure) <= 0.05
+                    pval_hdl = text(iLac_measure, iFatigueRtg, '*');
+                elseif pval_mtrx(iFatigueRtg, iLac_measure) > 0.001 && pval_mtrx(iFatigueRtg, iLac_measure) <= 0.01
+                    pval_hdl = text(iLac_measure, iFatigueRtg, '**');
+                elseif pval_mtrx(iFatigueRtg, iLac_measure) <= 0.001
+                    pval_hdl = text(iLac_measure, iFatigueRtg, '***');
+                end % p.value
+                % adjust p.value parameters
+                pval_hdl.Color = col.white;
+                pval_hdl.FontSize = 70;
+                pval_hdl.FontWeight = 'bold';
+                pval_hdl.HorizontalAlignment = 'center'; % center text on x-axis
+                pval_hdl.VerticalAlignment = 'middle'; % center text on y-axis
+            end % when p.value is significant
+        end % loop over Y variables
+    end % loop over X variables
+end % figure display
 end % function
