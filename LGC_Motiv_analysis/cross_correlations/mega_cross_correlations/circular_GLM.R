085ebc976545cfe5fcfaf8ab8ef7b462253315b8
NicolasClairis <nicolas.clairis@protonmail.com>
2024-06-19 18:29:37 +0200

    Create circular_GLM.R
    work in progress

---
+++ LGC_Motiv_analysis/cross_correlations/mega_cross_correlations/circular_GLM.R
@@ -0,0 +1,3 @@
+# adaptation of circular_plot.R and circular_rplot_bis.R but instead of looking at correlations like these scripts,
+# circular_GLM.R will look at the betas after correcting for age and sex and, instead of filtering on r, 
+#circular_GLM.R will filter on p.values.
