b2b4a3ff258d73a32ad7db66f1236bbf404bd26a
NicolasClairis <nicolas.clairis@protonmail.com>
2024-06-26 11:50:45 +0200

    Update place_r_and_pval.m
    improve position of x and y text + replace text by "p<0.001" when pvalue is very low

--- Matlab_DIY_functions/place_r_and_pval.m
+++ Matlab_DIY_functions/place_r_and_pval.m
@@ -40,25 +40,31 @@ end
 r_corr_str = [num2str(r_corr),pval_stars];
 
 % for p.value need to track how small p.value is if below 0.001
-if pval > 0.001
-    pval_str = num2str(round(pval, 3));
+if pval >= 0.001
+    pval_str = ['p = ',num2str(round(pval, 3))];
 else
-    pval_str = num2str(round(pval,1,'significant'));
+    % pval_str = num2str(round(pval,1,'significant')); % to show actual
+    % value
+    pval_str = 'p < 0.001'; % just mention this if pvalue is smaller than 0.001
 end
 
 %% define x coordinate for the text
 xlim_vals = xlim();
-xlim_dims = xlim_vals(2) - xlim_vals(1);
-x_val_txt = xlim_vals(2) - xlim_dims/7;
+% xlim_dims = xlim_vals(2) - xlim_vals(1);
+% x_val_txt = xlim_vals(2) - xlim_dims/7; % manual centering
+x_val_txt = xlim_vals(2);
+x_alignment = 'center'; % use HorizontalAlignment property to align
 
 %% define y coordinate depending on if correlation is negative or positive (to avoid overlapping relevant values)
+ylim_vals = ylim();
 if r_corr < 0 % negative correlation (top right of the screen)
-    ylim_vals = ylim();
     y_val_txt = ylim_vals(2);
+    y_alignment = 'cap'; % start text on top of plot
 elseif r_corr >= 0 % positive correlation (bottom right of the screen)
-    ylim_vals = ylim();
-    ylim_dims = ylim_vals(2) - ylim_vals(1);
-    y_val_txt = ylim_vals(1) + ylim_dims/7;
+    % ylim_dims = ylim_vals(2) - ylim_vals(1);
+    % y_val_txt = ylim_vals(1) + ylim_dims/7; % manual placement
+    y_val_txt = ylim_vals(1); % use VerticalAlignment to place text
+    y_alignment = 'bottom'; % start text on bottom of plot
 end
 
 %% text size
@@ -66,8 +72,9 @@ txtSize = 30;
 
 %% add the text
 txt_hdl = text(x_val_txt,y_val_txt,...
-    {['r = ',r_corr_str],...
-    ['p = ',pval_str]},...
-    'FontSize',txtSize);
+    {['r = ',r_corr_str], pval_str},...
+    'FontSize',txtSize,...
+    'HorizontalAlignment',x_alignment,...
+    'VerticalAlignment',y_alignment);
 
 end % function
