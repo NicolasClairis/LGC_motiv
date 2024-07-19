f996a236d58efcbd27f9640853bb8dba31f85df3
NicolasClairis <nicolas.clairis@protonmail.com>
2024-07-19 17:55:46 +0200

    Update DCM_timeseries_extraction.m

--- LGC_Motiv_analysis/fMRI/DCM/DCM_timeseries_extraction.m
+++ LGC_Motiv_analysis/fMRI/DCM/DCM_timeseries_extraction.m
@@ -1,12 +1,25 @@
-function[] = DCM_timeseries_extraction()
-% DCM_timeseries_extraction
+function[] = DCM_timeseries_extraction(GLM, checking)
+% DCM_timeseries_extraction(GLM, checking)
 % DCM_timeseries_extraction serves to extract the timeseries in the ROI of
 % interest in order to perform the DCM.
+%
+% INPUTS
+% GLM: GLM number
+%
+% checking: display batch before launching it or not
+% (0) launch batch directly
+% (1) display batch before launching it
 
 %% subject selection
 study_nm = 'study1';
 condition = subject_condition;
 [subject_id, NS] = LGCM_subject_selection(study_nm, condition);
+
+%% GLM
+if ~exist('GLM','var') || isempty(GLM)
+   GLM_info = inputdlg({'GLM number?'});
+   GLM = str2double(GLM_info{1});
+end
 
 %% which DCM mode to use? will define how sessions are grouped
 [DCM_mode] = which_DCM_mode_for_GLM;
@@ -25,7 +38,8 @@ Fcon_to_adjust = 2;
 VOI_names = {'dmPFCdACC','aIns'};
 n_VOIs_per_sub = length(VOI_names);
 matlabbatch = cell(NS*n_VOIs_per_sub,1);
-for iS = 1:NS
+matlabbatch = cell(1,1);
+for iS = 1%:NS
     sub_nm = subject_id{iS};
     subFolder = [fullfile('E:',study_nm,['CID',sub_nm],...
         'fMRI_analysis','functional',...
@@ -36,27 +50,35 @@ for iS = 1:NS
         'MRS','MRS_voxels');
     
     %% loop over volumes of interest
-    for iVOI = 1:n_VOIs_per_sub
+    for iVOI = 1%:n_VOIs_per_sub
         VOI_nm = VOI_names{iVOI};
         batch_idx = iVOI + n_VOIs_per_sub.*(iS - 1);
         
-        matlabbatch{batch_idx}.spm.util.voi.spmmat = [subFolder,'SPM.mat'];
+        matlabbatch{batch_idx}.spm.util.voi.spmmat = {[subFolder,'SPM.mat']};
         matlabbatch{batch_idx}.spm.util.voi.adjust = Fcon_to_adjust; % index of F-contrast used to adjust data
         matlabbatch{batch_idx}.spm.util.voi.session = 1; % all sessions pooled for DCM
         matlabbatch{batch_idx}.spm.util.voi.name = VOI_nm;
         switch VOI_nm
             case 'dmPFCdACC'
                 matlabbatch{batch_idx}.spm.util.voi.roi{1}.mask.image = {[fullfile(MRS_ROI_folder,...
-                    'bwdmpfc.nii'),',1']};
+                    'wdmpfc.nii'),',1']};
             case 'aIns'
                 matlabbatch{batch_idx}.spm.util.voi.roi{1}.mask.image = {[fullfile(MRS_ROI_folder,...
-                    'bwai.nii'),',1']};
+                    'wai.nii'),',1']};
             otherwise
                 error(['ROI = ',VOI_nm,' not ready yet']);
         end
-        matlabbatch{batch_idx}.spm.util.voi.roi{1}.mask.threshold = 0;
-        matlabbatch{batch_idx}.spm.util.voi.expression = 'i1>0';
+        matlabbatch{batch_idx}.spm.util.voi.roi{1}.mask.threshold = 0.1;
+        matlabbatch{batch_idx}.spm.util.voi.expression = 'i1';
     end % VOI loop
 end % subject loop
+
+%% display spm batch before running it
+if exist('checking','var') && checking == 1
+    spm_jobman('interactive',matlabbatch);
+%     spm_jobman('run',matlabbatch);
+elseif ~exist('checking','var') || isempty(checking) || checking == 0
+    spm_jobman('run',matlabbatch);
+end
 
 end % function
