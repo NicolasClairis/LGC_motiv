e677dde772dadb1b79372bf8260fb808c00c5c1f
NicolasClairis <nicolas.clairis@protonmail.com>
2024-10-24 16:42:51 +0200

    Update sub_id.m
    replaced gender by sex which is more acurrate

--- LGC_Motiv_analysis/sub_id.m
+++ LGC_Motiv_analysis/sub_id.m
@@ -1,5 +1,5 @@
-function[study_nm, condition, gender, subject_id, NS] = sub_id()
-% [study_nm, condition, gender, subject_id, NS] = sub_id
+function[study_nm, condition, sex, subject_id, NS] = sub_id()
+% [study_nm, condition, sex, subject_id, NS] = sub_id
 % sub_id will provide you the list of subjects to look for.
 %
 % OUTPUTS
@@ -7,7 +7,7 @@ function[study_nm, condition, gender, subject_id, NS] = sub_id()
 %
 % condition: string with condition selected for this analysis
 %
-% gender: string with gender selected for this analysis
+% sex: string with sex selected for this analysis
 %
 % subject_id: list of subjects to be included
 %
@@ -22,14 +22,14 @@ study_nm = 'study1';
 %% selection of the condition (which subjects to include
 condition = subject_condition;
 
-%% selection of the gender
-% gender_names = {'all','males','females'};
-% which_gender = listdlg('PromptString','Which gender?',...
-%     'SelectionMode','single','ListString',gender_names);
-% gender = gender_names{which_gender};
-gender = 'all';
+%% selection of the sex
+% sex_names = {'all','males','females'};
+% which_sex = listdlg('PromptString','Which sex?',...
+%     'SelectionMode','single','ListString',sex_names);
+% sex = sex_names{which_sex};
+sex = 'all';
 
 %% extract list of subjects
-[subject_id, NS] = LGCM_subject_selection(study_nm, condition, gender);
+[subject_id, NS] = LGCM_subject_selection(study_nm, condition, sex);
 
 end % function
